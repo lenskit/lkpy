c52dad3dad4be0f4ee9f537a4843ec1251819b01
Michael Ekstrand <md@ekstrandom.net>
2018-06-08 15:04:33 -0600

    Project metadata

--- setup.py
+++ setup.py
@@ -1,7 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    requires = ['pandas'],
+    name="lenskit",
+    version="0.0.1",
+    author="Michael Ekstrand",
+    author_email="michaelekstrand@boisestate.edu",
+    description="Run recommender algorithms and experiments",
+    url="https://lenskit.github.io/lkpy",
+    packages=find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+
+    requires = ['pandas', 'numpy'],
     setup_requires = ['pytest-runner'],
     tests_require = ['pytest', 'dask']
 )
