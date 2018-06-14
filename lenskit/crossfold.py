adfd02240798bf2c9ec9c9f7b0bc1689cbce95c4
Michael Ekstrand <md@ekstrandom.net>
2018-06-14 15:56:23 -0600

    Add support for splitting and partitioning rows

---
+++ lenskit/crossfold.py
@@ -0,0 +1,80 @@
+"""
+Data set cross-folding.
+"""
+
+from collections import namedtuple
+import logging
+
+import numpy as np
+
+TTPair = namedtuple('TTPair', ['train', 'test'])
+
+_logger = logging.getLogger(__package__)
+
+def partition_rows(data, partitions):
+    """
+    Partition a frame of ratings or other datainto train-test partitions.  This function does not
+    care what kind of data is in `data`, so long as it is a Pandas DataFrame (or equivalent).
+
+    :param data: a data frame containing ratings or other data you wish to partition.
+    :type data: `pd.DataFrame` or equivalent
+    :param partitions: the number of partitions to produce
+    :type partitions: integer
+    :rtype: iterator
+    :returns: an iterator of train-test pairs
+    """
+    _logger.info('partitioning %d ratings into %d partitions', len(data), partitions)
+
+    # create an array of indexes
+    rows = np.arange(len(data))
+    # shuffle the indices & split into partitions
+    np.random.shuffle(rows)
+    test_sets = np.array_split(rows, partitions)
+
+    # convert each partition into a split
+    for i, ts in enumerate(test_sets):
+        test = data.iloc[ts,:]
+        trains = test_sets[:i] + test_sets[(i+1):]
+        train_idx = np.concatenate(trains)
+        train = data.iloc[train_idx,:]
+        yield TTPair(train, test)
+
+def sample_rows(data, partitions, size, disjoint=True):
+    """
+    Sample train-test a frame of ratings into train-test partitions.  This function does not care what kind
+    of data is in `data`, so long as it is a Pandas DataFrame (or equivalent).
+
+    :param data: a data frame containing ratings or other data you wish to partition.
+    :type data: `pd.DataFrame` or equivalent
+    :param partitions: the number of partitions to produce
+    :type partitions: integer
+    :rtype: iterator
+    :returns: an iterator of train-test pairs
+    """
+
+    if disjoint and partitions * size >= len(data):
+        _logger.warn('wanted %d disjoint splits of %d each, but only have %d rows; partitioning',
+                     partitions, size, len(data))
+        return partition_rows(data, partitions)
+
+    # create an array of indexes
+    rows = np.arange(len(data))
+
+    if disjoint:
+        _logger.info('creating %d disjoint samples of size %d', partitions, size)
+        # shuffle the indices & split into partitions
+        np.random.shuffle(rows)
+
+        # convert each partition into a split
+        for i in range(partitions):
+            start = i * size
+            test = rows[start:start+size]
+            train = np.concatenate((rows[:start], rows[start+size:]))
+            yield TTPair(data.iloc[train,:], data.iloc[test,:])
+
+    else:
+        _logger.info('taking %d samples of size %d', partitions, size)
+        for i in range(partitions):
+            test = np.random.choice(rows, size, False)
+            train = np.setdiff1d(rows, test, assume_unique=True)
+            yield TTPair(data.iloc[train,:], data.iloc[test,:])
---
+++ tests/test_crossfold.py
@@ -0,0 +1,86 @@
+import itertools as it
+import pytest
+
+import lk_test_utils as lktu
+
+import lenskit.crossfold as xf
+
+def test_partition_rows():
+    ratings = lktu.ml_pandas.renamed.ratings
+    splits = xf.partition_rows(ratings, 5)
+    splits = list(splits)
+    assert len(splits) == 5
+    assert isinstance(splits, list)
+
+    for s in splits:
+        assert len(s.test) + len(s.train) == len(ratings)
+        assert all(s.test.index.union(s.train.index) == ratings.index)
+        test_idx = s.test.set_index(['user', 'item']).index
+        train_idx = s.train.set_index(['user', 'item']).index
+        assert len(test_idx.intersection(train_idx)) == 0
+    
+    # we should partition!
+
+def test_sample_rows():
+    ratings = lktu.ml_pandas.renamed.ratings
+    splits = xf.sample_rows(ratings, partitions=5, size=1000)
+    splits = list(splits)
+    assert len(splits) == 5
+    assert isinstance(splits, list)
+
+    for s in splits:
+        assert len(s.test) == 1000
+        assert len(s.test) + len(s.train) == len(ratings)
+        test_idx = s.test.set_index(['user', 'item']).index
+        train_idx = s.train.set_index(['user', 'item']).index
+        assert len(test_idx.intersection(train_idx)) == 0
+    
+    for s1, s2 in it.product(splits, splits):
+        if s1 is s2: continue
+        
+        i1 = s1.test.set_index(['user', 'item']).index
+        i2 = s2.test.set_index(['user', 'item']).index
+        inter = i1.intersection(i2)
+        assert len(inter) == 0
+
+def test_sample_rows_more_smaller_parts():
+    ratings = lktu.ml_pandas.renamed.ratings
+    splits = xf.sample_rows(ratings, partitions=10, size=500)
+    splits = list(splits)
+    assert len(splits) == 10
+    assert isinstance(splits, list)
+
+    for s in splits:
+        assert len(s.test) == 500
+        assert len(s.test) + len(s.train) == len(ratings)
+        test_idx = s.test.set_index(['user', 'item']).index
+        train_idx = s.train.set_index(['user', 'item']).index
+        assert len(test_idx.intersection(train_idx)) == 0
+    
+    for s1, s2 in it.product(splits, splits):
+        if s1 is s2: continue
+        
+        i1 = s1.test.set_index(['user', 'item']).index
+        i2 = s2.test.set_index(['user', 'item']).index
+        inter = i1.intersection(i2)
+        assert len(inter) == 0
+
+def test_sample_non_disjoint():
+    ratings = lktu.ml_pandas.renamed.ratings
+    splits = xf.sample_rows(ratings, partitions=10, size=1000, disjoint=False)
+    splits = list(splits)
+    assert len(splits) == 10
+    assert isinstance(splits, list)
+
+    for s in splits:
+        assert len(s.test) == 1000
+        assert len(s.test) + len(s.train) == len(ratings)
+        test_idx = s.test.set_index(['user', 'item']).index
+        train_idx = s.train.set_index(['user', 'item']).index
+        assert len(test_idx.intersection(train_idx)) == 0
+
+    # There are enough splits & items we should pick at least one duplicate
+    ipairs = ((s1.test.set_index('user', 'item').index, s2.test.set_index('user', 'item').index)
+              for (s1, s2) in it.product(splits, splits))
+    isizes = [len(i1.intersection(i2)) for (i1, i2) in ipairs]
+    assert any(n > 0 for n in isizes)
