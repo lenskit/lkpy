a032a2617ad2e3bac7f0f85dd00abae308314d76
Michael Ekstrand <md@ekstrandom.net>
2018-06-15 09:01:51 -0600

    Cache Miniconda

--- build-tools/install-conda.sh
+++ build-tools/install-conda.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
 set -e
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-bash miniconda.sh -b -p "$HOME/miniconda"
+if [ ! -d "$HOME/miniconda" ]; then
+    wget --no-verbose https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+    bash miniconda.sh -b -p "$HOME/miniconda"
+fi
 export PATH="$HOME/miniconda/bin:$PATH"
 conda config --set always_yes yes --set changeps1 no
 conda update -q conda
-conda create -q -n lkpy-test python="$TRAVIS_PYTHON_VERSION" pandas dask pytest
+if [ ! -d "$HOME/miniconda/envs/lkpy-test" ]; then
+    conda create -q -n lkpy-test python="$TRAVIS_PYTHON_VERSION" pandas dask pytest
+fi
 conda install -q -n lkpy-test coverage pylint
+conda update -q -n lkpy-test --all
