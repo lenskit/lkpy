9132bdd889f238f014ea050e0ff9b671d9a0ec57
Michael Ekstrand <md@ekstrandom.net>
2018-06-27 08:59:52 -0600

    Implement and test top-N precision

---
+++ lenskit/metrics/topn.py
@@ -0,0 +1,59 @@
+"""
+Top-N evaluation metrics.
+"""
+
+
+import pandas as pd
+import numpy as np
+
+
+def precision(recs, relevant):
+    """
+    Compute the precision of a set of recommendations.
+
+    Args:
+        recs(array-like): a sequence of recommended items
+        relevant(set-like): the set of relevant items
+
+    Returns:
+        double: the fraction of recommended items that are relevant
+    """
+    if len(recs) == 0:
+        return None
+
+    recs = pd.Series(recs)
+    ngood = recs.isin(relevant).sum()
+    return ngood / len(recs)
+
+
+def recall(recs, relevant):
+    """
+    Compute the recall of a set of recommendations.
+
+    Args:
+        recs(array-like): a sequence of recommended items
+        relevant(set-like): the set of relevant items
+    """
+    pass
+
+
+def recip_rank(recs, relevant):
+    """
+    Compute the reciprocal rank of the first relevant item in a recommendation list.
+
+    Args:
+        recs(array-like): a sequence of recommended items
+        relevant(set-like): the set of relevant items
+    """
+    pass
+
+
+def avg_precision(recs, relevant):
+    """
+    Compute the average precision of a list of recommendations.
+
+    Args:
+        recs(array-like): a sequence of recommended items
+        relevant(set-like): the set of relevant items
+    """
+    pass
---
+++ tests/test_topn_precision.py
@@ -0,0 +1,83 @@
+import numpy as np
+import pandas as pd
+
+from pytest import approx
+
+import lenskit.metrics.topn as lm
+
+
+def test_precision_empty_none():
+    prec = lm.precision([], set([1, 3]))
+    assert prec is None
+
+
+def test_precision_simple_cases():
+    prec = lm.precision([1, 3], set([1, 3]))
+    assert prec == approx(1.0)
+
+    prec = lm.precision([1], set([1, 3]))
+    assert prec == approx(1.0)
+
+    prec = lm.precision([1, 2, 3, 4], set([1, 3]))
+    assert prec == approx(0.5)
+
+    prec = lm.precision([1, 2, 3, 4], set([1, 3, 5]))
+    assert prec == approx(0.5)
+
+    prec = lm.precision([1, 2, 3, 4], range(5, 10))
+    assert prec == approx(0.0)
+
+    prec = lm.precision([1, 2, 3, 4], range(4, 10))
+    assert prec == approx(0.25)
+
+
+def test_precision_series():
+    prec = lm.precision(pd.Series([1, 3]), pd.Series([1, 3]))
+    assert prec == approx(1.0)
+
+    prec = lm.precision(pd.Series([1, 2, 3, 4]), pd.Series([1, 3, 5]))
+    assert prec == approx(0.5)
+
+    prec = lm.precision(pd.Series([1, 2, 3, 4]), pd.Series(range(4, 10)))
+    assert prec == approx(0.25)
+
+
+def test_precision_series_set():
+    prec = lm.precision(pd.Series([1, 2, 3, 4]), set([1, 3, 5]))
+    assert prec == approx(0.5)
+
+    prec = lm.precision(pd.Series([1, 2, 3, 4]), range(4, 10))
+    assert prec == approx(0.25)
+
+
+def test_precision_series_index():
+    prec = lm.precision(pd.Series([1, 3]), pd.Index([1, 3]))
+    assert prec == approx(1.0)
+
+    prec = lm.precision(pd.Series([1, 2, 3, 4]), pd.Index([1, 3, 5]))
+    assert prec == approx(0.5)
+
+    prec = lm.precision(pd.Series([1, 2, 3, 4]), pd.Index(range(4, 10)))
+    assert prec == approx(0.25)
+
+
+def test_precision_series_array():
+    prec = lm.precision(pd.Series([1, 3]), np.array([1, 3]))
+    assert prec == approx(1.0)
+
+    prec = lm.precision(pd.Series([1, 2, 3, 4]), np.array([1, 3, 5]))
+    assert prec == approx(0.5)
+
+    prec = lm.precision(pd.Series([1, 2, 3, 4]), np.arange(4, 10, 1, 'u4'))
+    assert prec == approx(0.25)
+
+
+def test_precision_array():
+    prec = lm.precision(np.array([1, 3]), np.array([1, 3]))
+    assert prec == approx(1.0)
+
+    prec = lm.precision(np.array([1, 2, 3, 4]), np.array([1, 3, 5]))
+    assert prec == approx(0.5)
+
+    prec = lm.precision(np.array([1, 2, 3, 4]), np.arange(4, 10, 1, 'u4'))
+    assert prec == approx(0.25)
