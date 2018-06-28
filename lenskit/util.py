3406dd361aaf0c98211f380d0560e74f95982c2b
Michael Ekstrand <md@ekstrandom.net>
2018-06-27 18:18:29 -0600

    Add a stopwatch utility

--- lenskit/util.py
+++ lenskit/util.py
@@ -2,6 +2,8 @@
 Miscellaneous utility functions.
 """
 
+import time
+
 
 def compute(x):
     """
@@ -17,3 +19,32 @@ def compute(x):
         return x.compute()
     else:
         return x
+
+
+class Stopwatch():
+    start_time = None
+    stop_time = None
+
+    def __init__(self, start=True):
+        if start:
+            self.start()
+
+    def start(self):
+        self.start_time = time.perf_counter()
+
+    def stop(self):
+        self.stop_time = time.perf_counter()
+
+    def elapsed(self):
+        stop = self.stop_time
+        if stop is None:
+            stop = time.perf_counter()
+
+        return stop - self.start_time
+
+    def __str__(self):
+        elapsed = self.elapsed()
+        if elapsed < 1:
+            return "{: 0.0f}ms".format(elapsed * 1000)
+        else:
+            return "{: 0.2f}s".format(elapsed)
---
+++ tests/test_util.py
@@ -0,0 +1,23 @@
+import time
+
+from pytest import approx
+
+from lenskit import util as lku
+
+
+def test_stopwatch_instant():
+    w = lku.Stopwatch()
+    assert w.elapsed() > 0
+
+
+def test_stopwatch_sleep():
+    w = lku.Stopwatch()
+    time.sleep(0.5)
+    assert w.elapsed() == approx(0.5, 0.05)
+
+
+def test_stopwatch_str():
+    w = lku.Stopwatch()
+    time.sleep(0.5)
+    s = str(w)
+    assert s.endswith('ms')
