75ca25ce0622a0bee68a1b481d69d7b23cb716dc
Michael Ekstrand <md@ekstrandom.net>
2018-07-28 11:05:48 -0600

    Use Distutils built-in Cythonization

--- setup.py
+++ setup.py
@@ -2,13 +2,17 @@ import sys
 import os
 from setuptools import setup, find_packages
 from distutils.extension import Extension
-import numpy
 
-try:
-    from Cython.Build import cythonize
-except ImportError:
-    print('Please install Cython before building', file=sys.stderr)
-    sys.exit(1)
+
+class LazyNPPath(os.PathLike):
+    "NumPy include path, but evaluated lazily"
+    def __fspath__(self):
+        import numpy
+        return numpy.get_include()
+
+    def __str__(self):
+        return self.__fspath__()
+
 
 if sys.platform == 'win32':
     # MSVC - we do not yet support mingw or clang
@@ -25,12 +29,21 @@ if 'COVERAGE' in os.environ:
     debug_cflags += ['-DCYTHON_TRACE=1', '-DCYTHON_TRACE_NOGIL=1']
     cython_opts['linetrace'] = True
 
-extensions = [
-    Extension('*', ['lenskit/**/*.pyx'],
-              include_dirs=[numpy.get_include()],
-              extra_compile_args=[] + debug_cflags + openmp_cflags,
-              extra_link_args=[] + openmp_lflags)
-]
+
+def extmod(name, openmp=False, cflags=[], ldflags=[]):
+    "Create an extension object for one of our extension modules."
+    comp_args = cflags + debug_cflags
+    link_args = list(ldflags)
+    if openmp:
+        comp_args += openmp_cflags
+        link_args += openmp_lflags
+    parts = name.split('.')
+    parts[-1] += '.pyx'
+    path = os.path.join(*parts)
+    return Extension(name, [path], include_dirs=[LazyNPPath()],
+                     extra_compile_args=comp_args,
+                     extra_link_args=link_args)
+
 
 setup(
     name="lenskit",
@@ -39,33 +52,33 @@ setup(
     author_email="michaelekstrand@boisestate.edu",
     description="Run recommender algorithms and experiments",
     url="https://lenskit.github.io/lkpy",
-    packages=find_packages(),
-    ext_modules=cythonize(extensions, compiler_directives={
-        'language_level': 3,
-        'warn.undeclared': True,
-        'warn.unused': True,
-        'warn.maybe_uninitialized': True,
-        **cython_opts
-    }),
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ),
 
+    setup_requires=[
+        'pytest-runner',
+        'sphinx',
+        'cython',
+        'numpy',
+        'scipy'
+    ],
     install_requires=[
         'pandas',
         'numpy',
         'scipy',
         'tables >= 3.2.0'
     ],
-    setup_requires=[
-        'pytest-runner',
-        'sphinx',
-        'Cython'
-    ],
     tests_require=[
         'pytest >= 3.5.1',
         'pytest-arraydiff'
+    ],
+
+    packages=find_packages(),
+    ext_modules=[
+        extmod('lenskit.algorithms._item_knn', openmp=True),
+        extmod('lenskit.algorithms._funksvd')
     ]
 )
