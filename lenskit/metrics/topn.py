fe0257e989d29e3e3b7c36db1caa05b34d8ac035
Michael Ekstrand <md@ekstrandom.net>
2018-07-27 21:09:10 -0600

    implement nDCCG

--- lenskit/metrics/topn.py
+++ lenskit/metrics/topn.py
@@ -73,3 +73,44 @@ def _dcg(scores, discount=np.log2):
     ranks = np.arange(1, len(scores) + 1)
     disc = np.maximum(discount(ranks), 1)
     return np.dot(scores, 1.0/disc)
+
+
+def ndcg(scores, items=None, discount=np.log2):
+    """
+    Compute the Normalized Discounted Cumulative Gain of a series of scores.  These should be
+    relevance scores; they can be :math:`{0,1}` for binary relevance data.
+
+    Args:
+        scores(pd.Series or array-like):
+            relevance scores for items. If ``items`` is ``None``, these should be in order
+            of recommendation; if ``items`` is not ``None``, then this must be a
+            :py:class:`pandas.Series` indexed by item ID.
+        items(array-like):
+            the list of item IDs, if the item list and score list is to be provided separately.
+        discount(ufunc):
+            the rank discount function.  Each item's score will be divided the discount of its rank,
+            if the discount is greater than 1.
+
+    Returns:
+        The nDCG of the scored items.
+    """
+
+    if not isinstance(scores, pd.Series):
+        check.check_value(items is None, "scores must be Series when items provided")
+        scores = pd.Series(scores)
+
+    if items is None:
+        actual = _dcg(scores, discount)
+        iscores = scores.sort_values(ascending=False)
+        ideal = _dcg(iscores, discount)
+    else:
+        ascores = scores.reindex(items, fill_value=0)
+        actual = _dcg(ascores, discount)
+        iscores = scores.sort_values(ascending=True)
+        iscores = iscores[:len(items)]
+        ideal = _dcg(iscores, discount)
+
+    if ideal > 0:
+        return actual / ideal
+    else:
+        return 0
--- tests/test_topn_ndcg.py
+++ tests/test_topn_ndcg.py
@@ -27,3 +27,39 @@ def test_dcg_mult():
     "multiple elements should score correctly"
     assert lm._dcg(np.array([np.e, np.pi])) == approx(np.e + np.pi)
     assert lm._dcg(np.array([np.e, 0, 0, np.pi])) == approx(np.e + np.pi / np.log2(4))
+
+
+def test_ndcg_empty():
+    "empty should be zero"
+    assert lm.ndcg(np.array([])) == approx(0)
+
+
+def test_ndcg_zeros():
+    assert lm.ndcg(np.zeros(10)) == approx(0)
+
+
+def test_ndcg_single():
+    "a single element should be scored at the right place"
+    assert lm.ndcg(np.array([0.5])) == approx(1)
+    assert lm.ndcg(np.array([0, 0.5])) == approx(1)
+    assert lm.ndcg(np.array([0, 0, 0.5])) == approx(1 / np.log2(3))
+    assert lm.ndcg(np.array([0, 0, 0.5, 0])) == approx(1 / np.log2(3))
+
+
+def test_ndcg_mult():
+    "multiple elements should score correctly"
+    assert lm.ndcg(np.array([np.e, np.pi])) == approx(1)
+    assert lm.ndcg(np.array([np.e, 0, 0, np.pi])) == \
+        approx((np.e + np.pi / np.log2(4)) / (np.e + np.pi))
+
+
+def test_ndcg_score_items():
+    "looked-up items should score correctly"
+    scores = pd.Series([1, 1, 1, 1], index=['a', 'b', 'c', 'd'])
+
+    assert lm.ndcg(scores, []) == approx(0)
+    assert lm.ndcg(scores, ['a', 'b']) == approx(1)
+    assert lm.ndcg(scores, ['a', 'b', 'c', 'd']) == approx(1)
+    assert lm.ndcg(scores, ['d', 'b', 'a', 'c']) == approx(1)
+    assert lm.ndcg(scores, ['d', 'b', 'z']) == approx(2 / (2 + 1/np.log2(3)))
+    assert lm.ndcg(scores, ['d', 'z', 'b']) == approx((1 + 1/np.log2(3)) / (2 + 1/np.log2(3)))
