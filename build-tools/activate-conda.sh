2e0778792b0aa03310dc0ea202e6d213d2a008ce
Michael Ekstrand <md@ekstrandom.net>
2018-09-30 10:50:17 -0700

    renmove old build tools

--- build-tools/activate-conda.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# INTENDED TO BE SOURCED
-if [[ "$PYTHON_TYPE" = conda ]]; then
-    export PATH="$HOME/miniconda/bin:$PATH"
-    source activate lkpy-test
-else
-    echo >&2 "Conda not requested, skipping"
-fi
--- build-tools/fetch-ml100k.sh
+++
@@ -1,9 +0,0 @@
-
-
-if [ -r ml-100k/u.data ]; then
-    echo "ML-100K already downloaded"
-    exit 0
-fi
-
-wget --no-verbose -O ml-100k.zip http://files.grouplens.org/datasets/movielens/ml-100k.zip
-unzip ml-100k.zip
