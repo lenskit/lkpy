fb8f784cdd0fbff1b85d8f8581bebbc62f3ad360
Michael Ekstrand <md@ekstrandom.net>
2018-10-13 13:48:18 -0600

    Add check_dimension

--- tests/test_check.py
+++ tests/test_check.py
@@ -1,6 +1,8 @@
-import pytest
+from pytest import raises
 
-from lenskit.check import check_value
+import numpy as np
+
+from lenskit.check import check_value, check_dimension
 
 
 def test_check_value_passes():
@@ -9,15 +11,34 @@ def test_check_value_passes():
 
 
 def test_check_value_fails():
-    with pytest.raises(ValueError):
+    with raises(ValueError):
         check_value(False, "value should be true")
 
 
 def test_check_meaningful_value_fails():
-    with pytest.raises(ValueError):
+    with raises(ValueError):
         check_value(5 < 4, "five should be less than four")
 
 
 def test_check_meaningful_value_succeeds():
     check_value(3 < 4, "three should be less than four")
     # it should complete successfully
+
+
+def test_check_dim_len():
+    check_dimension([], [])
+    with raises(ValueError):
+        check_dimension([], [3])
+    with raises(ValueError):
+        check_dimension([1], [])
+    check_dimension(range(10), range(10, 20))
+
+
+def test_check_dim_shape():
+    check_dimension(np.arange(5), np.arange(5), d1=1, d2=1)
+    with raises(ValueError):
+        check_dimension(np.arange(5), np.arange(6), d1=1, d2=1)
+    with raises(ValueError):
+        check_dimension(np.random.randn(8, 10), np.arange(8, 9), d1=2, d2=2)
+
+    check_dimension(np.random.randn(8, 10), np.arange(23, 8), d1=1, d2=2)
