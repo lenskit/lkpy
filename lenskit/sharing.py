e8131d25b17a282341960afe25b4428cf000dca3
Michael Ekstrand <md@ekstrandom.net>
2018-11-17 10:27:32 -0700

    Rip out Plasma

--- lenskit/sharing.py
+++ lenskit/sharing.py
@@ -3,26 +3,15 @@ Support for sharing data between processes to enable multi-process
 evaluation operations more easily.
 """
 
-import os
 from abc import ABCMeta, abstractmethod, abstractclassmethod
 import tempfile
-import subprocess
 from pathlib import Path
 import uuid
 import logging
-import weakref
 from multiprocessing import sharedctypes as mpctypes
-import ctypes
 
 import numpy as np
 import pandas as pd
-
-import pyarrow as pa
-try:
-    from pyarrow import plasma
-    have_plasma = True
-except ImportError:
-    have_plasma = False
 
 _logger = logging.getLogger(__name__)
 
@@ -145,135 +134,17 @@ class SHMShareContext(ShareContext):
         _pop_context()
 
 
-class PlasmaShareContext(ShareContext):
-    process = None
-    disconnect = False
-
-    def __new__(cls, *args, **kwargs):
-        obj = super().__new__(cls)
-        obj.__obj_map = {}
-        obj.__added_ids = []
-        return obj
-
-    def __init__(self, path=None, size=None, _client=None):
-        if path is None:
-            path = os.environ.get('PLASMA_SOCKET', None)
-            self.socket = path
-
-        if _client is not None:
-            self.client = _client
-            self.disconnect = False
-            self.process = None
-            return
-
-        if path is None:
-            if size is None:
-                size = os.environ.get('PLASMA_SIZE', 4*1024*1024*1024)
-                size = str(size)
-
-            self._dir = tempfile.TemporaryDirectory(prefix='lkpy-plasma')
-            self.socket = Path(self._dir.name) / 'plasma-socket'
-            _logger.info('launching Plasma store with %s bytes', size)
-            self.process = subprocess.Popen(['plasma_store', '-m', size, '-s', self.socket],
-                                            stdin=subprocess.DEVNULL)
-        else:
-            self.process = None
-
-        self.disconnect = True
-        self.client = plasma.connect(str(self.socket), "", 0)
-
-    def _rand_id(self):
-        return plasma.ObjectID(np.random.bytes(20))
-
-    def _clear_ref(self, id):
-        try:
-            del self.__obj_map[id]
-        except KeyError:
-            pass
-
-    def child(self):
-        return PlasmaShareContext(self.socket, _client=self.client)
-
-    def put_array(self, array):
-        key = self._rand_id()
-        _logger.debug('storing array of shape %s in %s', array.shape, key)
-        tensor = pa.Tensor.from_numpy(array)
-        size = pa.get_tensor_size(tensor)
-        _logger.debug('array data is %d bytes', size)
-        buf = self.client.create(key, size)
-
-        sink = pa.FixedSizeBufferWriter(buf)
-        pa.write_tensor(tensor, sink)
-
-        self.client.seal(key)
-        self.__added_ids.append(key)
-
-        return key
-
-    def get_array(self, key):
-        [data] = self.client.get_buffers([key])
-        _logger.debug('loading data of size %d from %s', data.size, key)
-        buffer = pa.BufferReader(data)
-        tensor = pa.read_tensor(buffer)
-        result = tensor.to_numpy()
-        rid = id(result)
-
-        self.__obj_map[rid] = data
-        weakref.finalize(result, self._clear_ref, rid)
-
-        _logger.debug('loaded array of shape %s', result.shape)
-        return result
-
-    def __getstate__(self):
-        return self.socket
-
-    def __setstate__(self, state):
-        self.socket = state
-        self.client = plasma.connect(self.socket, "", 0)
-        self.disconnect = True
-
-    def __enter__(self):
-        _push_context(self)
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        try:
-            self._cleanup()
-        finally:
-            _pop_context()
-
-    def __del__(self):
-        self._cleanup()
-
-    def _cleanup(self):
-        if self.client is not None:
-            _logger.debug('deleting %d objects', len(self.__added_ids))
-            self.client.delete(self.__added_ids)
-            _logger.debug('disconnecting from Plasma')
-            self.client.disconnect()
-            self.client = None
-
-        if self.process is not None:
-            _logger.info('shutting down Plasma')
-            self.process.terminate()
-            self.process.wait()
-            self.process = None
-            self._dir.cleanup()
-
-
 class Shareable(metaclass=ABCMeta):
     @abstractmethod
     def share_publish(self, context):
-        raise NotImplemented()
+        raise NotImplementedError()
 
     @abstractclassmethod
     def share_resolve(self, key, context):
-        raise NotImplemented()
+        raise NotImplementedError()
 
 
 share_impls = [SHMShareContext, DiskShareContext]
-if have_plasma:
-    share_impls.append(PlasmaShareContext)
 
 
 def context():
