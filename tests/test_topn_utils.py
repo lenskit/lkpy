266c3ae9f336c64a78b6fcb4b70cab1cfe81c8fc
Michael Ekstrand <md@ekstrandom.net>
2019-01-21 10:02:49 -0700

    Add CandidateSelector interface

--- tests/test_topn_utils.py
+++ tests/test_topn_utils.py
@@ -1,5 +1,7 @@
 from lenskit import topn
+from lenskit.algorithms import CandidateSelector
 
+import pandas as pd
 import numpy as np
 
 import lk_test_utils as lktu
@@ -14,3 +16,33 @@ def test_unrated():
     rated = ratings[ratings.user == 100].item.unique()
     assert len(cs) == len(items) - len(rated)
     assert len(np.intersect1d(cs, rated)) == 0
+
+
+def test_cs_rated_items_series():
+    "rated_items should de-index series"
+    items = ['a', 'b', 'wombat']
+    series = pd.Series(np.random.randn(3), index=items)
+
+    i2 = CandidateSelector.rated_items(series)
+    assert isinstance(i2, np.ndarray)
+    assert all(i2 == items)
+
+
+def test_cs_rated_items():
+    "rated_items should return list as array"
+    items = ['a', 'b', 'wombat']
+
+    i2 = CandidateSelector.rated_items(items)
+    assert isinstance(i2, np.ndarray)
+    assert all(i2 == items)
+
+
+def test_cs_rated_items_array():
+    "rated_items should return array as itself"
+    items = ['a', 'b', 'wombat']
+    items = np.array(items)
+
+    i2 = CandidateSelector.rated_items(items)
+    assert isinstance(i2, np.ndarray)
+    assert all(i2 == items)
+    assert i2 is items
