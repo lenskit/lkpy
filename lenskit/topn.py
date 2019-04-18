bba8c387c7ea7c42af8263259d30c264fac63872
Michael Ekstrand <michaelekstrand@boisestate.edu>
2019-04-18 10:02:03 -0600

    Update topn.py

--- lenskit/topn.py
+++ lenskit/topn.py
@@ -28,6 +28,11 @@ class RecListAnalysis:
     grouping columns, and contain at least an ``item`` column.  If it also
     contains a ``rating`` column, that is used as the users' rating for
     metrics that require it; otherwise, a rating value of 1 is assumed.
+    
+    .. warning::
+       Currently, RecListAnalysis will silently drop users who received
+       no recommendations.  We are working on an ergonomic API for fixing
+       this problem.
 
     Args:
         group_cols(list):
