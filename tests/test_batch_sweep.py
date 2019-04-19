807e8e38ba1db0bd1b56a141df9b1c7f3f17e23a
Michael Ekstrand <md@ekstrandom.net>
2019-04-19 12:29:52 -0600

    Fix incorrect import

--- tests/test_batch_sweep.py
+++ tests/test_batch_sweep.py
@@ -5,7 +5,7 @@ import pickle
 import pandas as pd
 import numpy as np
 
-from lenskit.util import norm_path
+from lenskit.util import norm_path, ml_test
 from lenskit import batch, crossfold as xf
 from lenskit.algorithms import Predictor
 from lenskit.algorithms.basic import Bias, Popular
