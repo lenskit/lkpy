6514695db8d4514825d38885f43ccf1fab905041
Michael Ekstrand <md@ekstrandom.net>
2019-05-18 17:37:02 -0600

    Bump version

--- doc/conf.py
+++ doc/conf.py
@@ -23,9 +23,9 @@ copyright = '2018–2019 Boise State University'
 author = 'Michael D. Ekstrand'
 
 # The short X.Y version
-version = '0.6.1'
+version = '0.7.0'
 # The full version, including alpha/beta/rc tags
-release = '0.6.1'
+release = '0.7.0'
 
 
 # -- General configuration ---------------------------------------------------
