77ab1837d750d53662de1250bde5639298bb6323
Michael Ekstrand <md@ekstrandom.net>
2020-04-17 17:09:22 -0600

    Fix variable parsing

--- .github/actions/conda-env/main.js
+++ .github/actions/conda-env/main.js
@@ -39,17 +39,25 @@ async function initialize(cfg) {
     await run(conda_bin, ['env', 'create', '-q', '-n', cfg.name, '-f', cfg.file]);
 }
 
+function parseVar(s) {
+    let m = /(\w+)=(.*)/.match(s);
+    return {
+        name: m.group(1),
+        value: m.group(2)
+    }
+}
+
 async function exportUnix(cfg) {
     let before = await exec('env', {shell: '/bin/bash'});
     let after = await exec(`_c=$(${conda_bin} shell.posix activate ${cfg.name}); eval "$_c"; env`, {shell: '/bin/bash'});
     let vars = {};
     for (let line of before.stdout.split(/\r?\n/)) {
-        let [name, val] = line.split(/=/, 2);
-        vars[name] = val;
+        let {name, value} = parseVar(line);
+        vars[name] = value;
     }
     for (let line of after.stdout.split(/\r?\n/)) {
-        let [name, val] = line.split(/=/, 2);
-        if (vars[name] != val) {
+        let {name, value} = parseVar(line);
+        if (vars[name] != value) {
             core.info('exporting variable ' + name);
             core.exportVariable(name, val);
         }
