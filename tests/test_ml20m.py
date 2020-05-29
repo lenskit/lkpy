87842a5bdbcfee87f88316214ad1532275be1ac8
Michael Ekstrand <michaelekstrand@boisestate.edu>
2020-05-29 14:58:09 -0600

    reduce ML20M test load

--- tests/test_ml20m.py
+++ tests/test_ml20m.py
@@ -12,6 +12,7 @@ from lenskit import crossfold as xf
 from lenskit import batch
 from lenskit.algorithms import Recommender
 from lenskit.algorithms.basic import Popular
+from lenskit.util.test import rng
 
 import pytest
 
@@ -33,17 +34,12 @@ def ml20m():
 
 
 @pytest.mark.parametrize('n_jobs', [1, 2])
-def test_pop_recommend(ml20m, n_jobs):
-    all_test = []
-    all_recs = []
-    for train, test in xf.sample_users(ml20m, 5, 5000, xf.SampleN(5)):
-        algo = Popular()
-        _log.info('training %s', algo)
-        algo.fit(train)
-        _log.info('recommending with %s', algo)
-        recs = batch.recommend(algo, test['user'].unique(), 10, n_jobs=n_jobs)
-        all_recs.append(recs)
-        all_test.append(test)
+def test_pop_recommend(ml20m, rng, n_jobs):
+    users = rng.choose(ml20m['user'].unique(), 10000)
+    algo = Popular()
+    _log.info('training %s', algo)
+    algo.fit(ml20m)
+    _log.info('recommending with %s', algo)
+    recs = batch.recommend(algo, users, 10, n_jobs=n_jobs)
 
-    all_recs = pd.concat(all_recs)
-    assert all_recs['user'].nunique() == 25000
+    assert recs['user'].nunique() == 10000
