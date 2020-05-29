e9db1067f03fabb9a710300e9972cfb37bd992b4
Michael Ekstrand <michaelekstrand@boisestate.edu>
2020-05-29 12:11:38 -0600

    Add custom context to parallelism

--- lenskit/util/parallel.py
+++ lenskit/util/parallel.py
@@ -9,16 +9,70 @@ import logging
 import inspect
 from concurrent.futures import ProcessPoolExecutor
 from abc import ABC, abstractmethod
+import pickle
 
 from ..sharing import persist
+
+if pickle.HIGHEST_PROTOCOL < 5:
+    import pickle5 as pickle
 
 _log = logging.getLogger(__name__)
 __work_model = None
 __work_func = None
 
 
+def _p5_recv(self):
+    buf = self.recv_bytes()
+    return pickle.loads(buf)
+
+
+def _p5_send(self, obj):
+    buf = pickle.dumps(obj, protocol=pickle.HIGHEST_PROTOCOL)
+    self._send_bytes(buf)
+
+
+class FastQ(mp.queues.SimpleQueue):
+    """
+    SimpleQueue subclass that uses Pickle5 instead of default pickling.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__patch()
+
+    def __patch(self):
+        # monkey-patch the sockets to use pickle5
+        self._reader.recv = _p5_recv.__get__(self._reader)
+        self._writer.send = _p5_send.__get__(self._writer)
+
+    def get(self):
+        with self._rlock:
+            res = self._reader.recv_bytes()
+        return pickle.loads(res)
+
+    def put(self, obj):
+        bytes = pickle.dumps(obj, protocol=pickle.HIGHEST_PROTOCOL)
+        # follow SimpleQueue, need to deal with _wlock being None
+        if self._wlock is None:
+            self._writer.send_bytes(bytes)
+        else:
+            with self._wlock:
+                self._writer.send_bytes(bytes)
+
+    def __setstate__(self, state):
+        super().__setstate__(state)
+        self.__patch()
+
+
+class LKContext(mp.context.SpawnContext):
+    def SimpleQueue(self):
+        return FastQ(ctx=self.get_context())
+
+
+LKContext.INSTANCE = LKContext()
+
+
 def _initialize_worker(mkey, func):
-    global __work_model, __work_func
+    global __work_model, __work_func, __profile
     __work_model = mkey
     __work_func = func
 
@@ -134,7 +188,7 @@ class InProcessOpInvoker(ModelOpInvoker):
 class ProcessPoolOpInvoker(ModelOpInvoker):
     def __init__(self, model, func, n_jobs):
         key = persist(model)
-        ctx = mp.get_context('spawn')
+        ctx = LKContext.INSTANCE
         _log.info('setting up ProcessPoolExecutor w/ %d workers', n_jobs)
         self.executor = ProcessPoolExecutor(n_jobs, ctx, _initialize_worker, (key, func))
 
@@ -148,7 +202,7 @@ class ProcessPoolOpInvoker(ModelOpInvoker):
 class MPOpInvoker(ModelOpInvoker):
     def __init__(self, model, func, n_jobs):
         key = persist(model)
-        ctx = mp.get_context('spawn')
+        ctx = LKContext.INSTANCE
         _log.info('setting up multiprocessing.Pool w/ %d workers', n_jobs)
         self.pool = ctx.Pool(n_jobs, _initialize_worker, (key, func))
 
