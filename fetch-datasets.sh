bc6550d28890e916954c02938f15ee40f9698ff9
Michael Ekstrand <michaelekstrand@boisestate.edu>
2020-06-04 12:06:02 -0600

    Fix shells

--- fetch-datasets.sh
+++ fetch-datasets.sh
@@ -3,8 +3,8 @@ set -e
 
 fetch_movielens()
 {
-    zipfile="$1"; shift
-    testfile="$1"; shift
+    zipfile="$1"; shift 1
+    testfile="$1"; shift 1
 
     echo "checking for $testfile"
     if [ ! -r "$testfile" ]; then
@@ -19,10 +19,10 @@ cd data
 
 while [ -n "$1" ]; do
     case "$1" in
-        ml-100k) fetch_movielens "ml-100k/u.data" "ml-100k.zip"; shift;;
-        ml-1m) fetch_movielens "ml-1m/ratings.dat" "ml-1m.zip"; shift;;
-        ml-10m) fetch_movielens "ml-10M100K/ratings.dat" "ml-10m.zip"; shift;;
-        ml-20m) fetch_movielens "ml-20m/ratings.csv" "ml-20m.zip"; shift;;
+        ml-100k) fetch_movielens "ml-100k/u.data" "ml-100k.zip"; shift 1;;
+        ml-1m) fetch_movielens "ml-1m/ratings.dat" "ml-1m.zip"; shift 1;;
+        ml-10m) fetch_movielens "ml-10M100K/ratings.dat" "ml-10m.zip"; shift 1;;
+        ml-20m) fetch_movielens "ml-20m/ratings.csv" "ml-20m.zip"; shift 1;;
         *) echo "Unknown data set $1" >&2; exit 2;;
     esac
 done
