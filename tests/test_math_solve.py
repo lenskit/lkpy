5530ded17546d9f02a952785eff6a8b5c54432a5
Michael Ekstrand <michaelekstrand@boisestate.edu>
2020-06-12 18:02:03 -0600

    Use Hypothesis for solver tests

--- tests/test_math_solve.py
+++ tests/test_math_solve.py
@@ -1,18 +1,27 @@
-import os
 import numpy as np
 import scipy.linalg as sla
 
-from pytest import approx, mark
+from pytest import approx
 
 from lenskit.math.solve import dposv
-from lenskit.util.test import repeated
+
+from hypothesis import given
+import hypothesis.strategies as st
 
 
-@repeated
-def test_solve_dposv():
-    size = np.random.randint(5, 50)
-    A = np.random.randn(size, size)
-    b = np.random.randn(size)
+@st.composite
+def square_problem(draw, scale=10):
+    size = draw(st.integers(1, 1000))
+    # Hypothesis doesn't do well at generating problem data, so go with this
+    rng = np.random.default_rng(42)
+    A = rng.standard_normal((size, size)) * scale
+    b = rng.standard_normal(size) * scale
+    return A, b, size
+
+
+@given(square_problem())
+def test_solve_dposv(problem):
+    A, b, size = problem
 
     # square values of A
     A = A * A
