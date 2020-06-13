e201d7926fe3021a8f076a8d4a67bcbff136e597
Michael Ekstrand <michaelekstrand@boisestate.edu>
2020-06-12 18:04:00 -0600

    Remove unused tri solver

--- lenskit/math/solve.py
+++ lenskit/math/solve.py
@@ -26,44 +26,6 @@ __dposv = __ffi.cast("void (*) (char*, int*, int*, double*, int*, double*, int*,
                      get_cython_function_address("scipy.linalg.cython_lapack", "dposv"))
 
 
-@n.njit(n.void(n.boolean, n.boolean, n.double[:, ::1], n.double[::1]), nogil=True)
-def _dtrsv(lower, trans, a, x):
-    inc1 = __ffi.from_buffer(__inc_1)
-
-    # dtrsv uses Fortran-layout arrays. Because we use C-layout arrays, we will
-    # invert the meaning of 'lower' and 'trans', and the function will work fine.
-    # We also need to swap index orders
-    uplo = __uplo_U if lower else __uplo_L
-    tspec = __trans_N if trans else __trans_T
-
-    n_p = np.array([a.shape[0]], dtype=np.intc)
-    n_p = __ffi.from_buffer(n_p)
-    lda_p = np.array([a.shape[1]], dtype=np.intc)
-    lda_p = __ffi.from_buffer(lda_p)
-
-    __dtrsv(__ffi.from_buffer(uplo), __ffi.from_buffer(tspec), __ffi.from_buffer(__diag_N),
-            n_p, __ffi.from_buffer(a), lda_p,
-            __ffi.from_buffer(x), inc1)
-
-
-def solve_tri(A, b, transpose=False, lower=True):
-    """
-    Solve the system :math:`Ax = b`, where :math:`A` is triangular.
-    This is equivalent to :py:func:`scipy.linalg.solve_triangular`, but does *not*
-    check for non-singularity.  It is a thin wrapper around the BLAS ``dtrsv``
-    function.
-
-    Args:
-        A(ndarray): the matrix.
-        b(ndarray): the taget vector.
-        transpose(bool): whether to solve :math:`Ax = b` or :math:`A^T x = b`.
-        lower(bool): whether :math:`A` is lower- or upper-triangular.
-    """
-    x = b.copy()
-    _dtrsv(lower, transpose, A, x)
-    return x
-
-
 @n.njit(n.intc(n.float64[:, ::1], n.float64[::1], n.boolean), nogil=True)
 def _dposv(A, b, lower):
     if A.shape[0] != A.shape[1]:
--- tests/test_math_solve.py
+++ tests/test_math_solve.py
@@ -4,84 +4,8 @@ import scipy.linalg as sla
 
 from pytest import approx, mark
 
-from lenskit.math.solve import solve_tri, dposv
+from lenskit.math.solve import dposv
 from lenskit.util.test import repeated
-
-
-@repeated
-def test_solve_ltri():
-    size = np.random.randint(5, 50)
-    Af = np.random.randn(size, size)
-    b = np.random.randn(size)
-    A = np.tril(Af)
-
-    x = solve_tri(A, b)
-    assert len(x) == size
-
-    xexp = sla.solve_triangular(A, b, lower=True)
-    assert x == approx(xexp, rel=1.0e-6)
-
-
-@repeated
-def test_solve_ltri_transpose():
-    size = np.random.randint(5, 50)
-    Af = np.random.randn(size, size)
-    b = np.random.randn(size)
-    A = np.tril(Af)
-
-    x = solve_tri(A, b, True)
-    assert len(x) == size
-
-    xexp = sla.solve_triangular(A.T, b, lower=False)
-    assert x == approx(xexp, rel=1.0e-6)
-
-
-@repeated
-def test_solve_utri():
-    size = np.random.randint(5, 50)
-    Af = np.random.randn(size, size)
-    b = np.random.randn(size)
-    A = np.triu(Af)
-
-    x = solve_tri(A, b, lower=False)
-    assert len(x) == size
-    xexp = sla.solve_triangular(A, b, lower=False)
-    assert x == approx(xexp, rel=1.0e-6)
-
-
-@repeated
-def test_solve_utri_transpose():
-    size = np.random.randint(5, 50)
-    Af = np.random.randn(size, size)
-    b = np.random.randn(size)
-    A = np.triu(Af)
-
-    x = solve_tri(A, b, True, lower=False)
-    assert len(x) == size
-    xexp = sla.solve_triangular(A.T, b, lower=True)
-    assert x == approx(xexp, rel=1.0e-6)
-
-
-@mark.skip(reason='we no longer use Cholesky decomposition anywhere')
-@repeated
-def test_solve_cholesky():
-    size = np.random.randint(5, 50)
-    A = np.random.randn(size, size)
-    b = np.random.randn(size)
-
-    # square values of A
-    A = A * A
-
-    # and solve
-    xexp, resid, rank, s = np.linalg.lstsq(A, b, rcond=None)
-
-    # chol solve
-    L = np.linalg.cholesky(A.T @ A)
-
-    w = solve_tri(L, A.T @ b)
-    x = solve_tri(L, w, transpose=True)
-
-    assert x == approx(xexp, abs=1.0e-3)
 
 
 @repeated
