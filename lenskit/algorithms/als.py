599991bd1ad5e7e2829120fd6ce99e88c3df8a04
Carlos <carlos@carloss-mbp-2.boisestate.edu>
2020-09-18 14:27:56 -0600

    undo numba change

--- lenskit/algorithms/als.py
+++ lenskit/algorithms/als.py
@@ -126,7 +126,7 @@ def _train_matrix_lu(mat, this: np.ndarray, other: np.ndarray, reg: float):
     return np.sqrt(frob)
 
 
-@njit
+@njit(nogil=True)
 def _train_bias_row_lu(items, ratings, other, reg):
     """
     Args:
