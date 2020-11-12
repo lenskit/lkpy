f57f6c72be79224a4c66ec041ccc5252f87ffc26
Michael Ekstrand <michaelekstrand@boisestate.edu>
2020-11-12 16:22:44 -0700

    Conda build update for Mac

--- conda/bld.bat
+++
@@ -1,8 +0,0 @@
-"%PYTHON%" setup.py build_helper || goto :fail
-"%PYTHON%" -m pip install --no-deps . || goto :fail
-
-goto :EOF
-
-:fail
-echo "Failed with code %errorlevel%"
-exit 1
--- conda/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-$PYTHON setup.py build_helper
-$PYTHON -m pip install --no-deps .
