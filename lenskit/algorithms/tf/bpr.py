4de5d93576842e597363e028f83a9c6b2de272e3
Michael Ekstrand <michaelekstrand@boisestate.edu>
2021-02-05 18:28:39 -0700

    Fix BPR compilation

--- lenskit/algorithms/tf/bpr.py
+++ lenskit/algorithms/tf/bpr.py
@@ -4,6 +4,7 @@ import math
 import pandas as pd
 import numpy as np
 from numba import njit
+import csr.native_ops as csrn
 
 try:
     import tensorflow as tf
@@ -35,7 +36,7 @@ def _neg_sample(mat, uv):
 
     for i in range(n):
         u = uv[i]
-        used = mat.row_cs(u)
+        used = csrn.row_cs(mat, u)
         j = np.random.randint(0, ni)
         while np.any(used == j):
             j = np.random.randint(0, ni)
