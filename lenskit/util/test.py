0aa492fd44a16a913fc9333b5f87038b038607ee
Michael Ekstrand <michaelekstrand@boisestate.edu>
2021-02-06 17:54:39 -0700

    Remove CSR test utilities

--- lenskit/util/test.py
+++ lenskit/util/test.py
@@ -7,13 +7,7 @@ import os.path
 import logging
 from contextlib import contextmanager
 
-import numpy as np
-from .. import matrix
-
 import pytest
-from hypothesis import given, assume
-import hypothesis.strategies as st
-import hypothesis.extra.numpy as nph
 
 from lenskit.datasets import MovieLens, ML100K
 
@@ -21,36 +15,6 @@ _log = logging.getLogger(__name__)
 
 ml_test = MovieLens('data/ml-latest-small')
 ml100k = ML100K('data/ml-100k')
-
-
-@st.composite
-def csrs(draw, nrows=None, ncols=None, nnz=None, values=None):
-    if ncols is None:
-        ncols = draw(st.integers(5, 100))
-    elif not isinstance(ncols, int):
-        ncols = draw(ncols)
-
-    if nrows is None:
-        nrows = draw(st.integers(5, 100))
-    elif not isinstance(nrows, int):
-        nrows = draw(nrows)
-
-    if nnz is None:
-        nnz = draw(st.integers(10, nrows * ncols // 2))
-    elif not isinstance(nnz, int):
-        nnz = draw(nnz)
-
-    coords = draw(nph.arrays(np.int32, nnz, elements=st.integers(0, nrows*ncols - 1), unique=True))
-    rows = np.mod(coords, nrows, dtype=np.int32)
-    cols = np.floor_divide(coords, nrows, dtype=np.int32)
-    if values is None:
-        values = draw(st.booleans())
-    if values:
-        rng = draw(st.randoms())
-        vals = np.array([rng.normalvariate(0, 1) for i in range(nnz)])
-    else:
-        vals = None
-    return matrix.CSR.from_coo(rows, cols, vals, (nrows, ncols))
 
 
 @contextmanager
