f3fdf6fe81e20decc33e3af93e0542c9c782ee08
Michael Ekstrand <michaelekstrand@boisestate.edu>
2021-04-01 11:50:29 -0600

    Implement PopScore agorithm

--- lenskit/algorithms/basic.py
+++ lenskit/algorithms/basic.py
@@ -22,6 +22,8 @@ class Popular(Recommender):
     """
     Recommend the most popular items.
 
+    The :py:class:`PopScore` class is more flexible, and recommended for new code.
+
     Args:
         selector(CandidateSelector):
             The candidate selector to use. If ``None``, uses a new
@@ -62,6 +64,56 @@ class Popular(Recommender):
 
     def __str__(self):
         return 'Popular'
+
+
+class PopScore(Predictor):
+    """
+    Score items by their popularity.  Use with :py:class:`TopN` to get a
+    most-popular-items recommender.
+
+    Args:
+        score_type(str):
+            The method for computing popularity scores.  Can be one of the following:
+
+            - ``'quantile'`` (the default)
+            - ``'rank'``
+            - ``'count'``
+
+    Attributes:
+        item_pop_(pandas.Series):
+            Item popularity scores.
+    """
+
+    def __init__(self, score_method='quantile'):
+        self.score_method = score_method
+
+    def fit(self, ratings, **kwargs):
+        _logger.info('counting item popularity')
+        scores = ratings['item'].value_counts()
+        if self.score_method == 'rank':
+            _logger.info('ranking %d items', len(scores))
+            scores = scores.rank().sort_index()
+        elif self.score_method == 'quantile':
+            _logger.info('computing quantiles for %d items', len(scores))
+            cmass = scores.sort_values()
+            cmass = cmass.cumsum()
+            cdens = cmass / scores.sum()
+            scores = cdens.sort_index()
+        elif self.score_method == 'count':
+            _logger.info('scoring items with their rating counts')
+            scores = scores.sort_index()
+        else:
+            raise ValueError('invalid scoring method ' + repr(self.score_method))
+
+        self.item_scores_ = scores
+
+        return self
+
+    def predict_for_user(self, user, items, ratings=None):
+        return self.item_scores_.reindex(items)
+
+    def __str__(self):
+        return 'PopScore({})'.format(self.score_method)
 
 
 class Memorized(Predictor):
--- tests/test_popular.py
+++ tests/test_popular.py
@@ -87,3 +87,53 @@ def test_pop_save_load():
     assert recs.score.iloc[0] == counts.max()
     # the 10 most popular should be the same
     assert all(counts.index[:10] == recs.item[:10])
+
+
+def test_popscore_quantile(rng):
+    algo = basic.PopScore()
+    algo.fit(lktu.ml_test.ratings)
+
+    assert algo.item_scores_.max() == 1.0
+
+    counts = lktu.ml_test.ratings.groupby('item').user.count()
+    counts = counts.sort_values()
+
+    winner = counts.index[-1]
+    assert algo.item_scores_.loc[winner] == 1.0
+
+
+def test_popscore_rank(rng):
+    algo = basic.PopScore('rank')
+    algo.fit(lktu.ml_test.ratings)
+
+    counts = lktu.ml_test.ratings.groupby('item').user.count()
+    counts = counts.sort_values()
+
+    assert algo.item_scores_.max() == len(counts)
+
+    winner = counts.index[-1]
+    assert algo.item_scores_.loc[winner] == len(counts)
+
+
+def test_popscore_counts(rng):
+    algo = basic.PopScore('count')
+    algo.fit(lktu.ml_test.ratings)
+
+    counts = lktu.ml_test.ratings.groupby('item').user.count()
+
+    scores, counts = algo.item_scores_.align(counts)
+    assert all(scores == counts)
+
+    items = rng.choice(counts.index, 100)
+    scores = algo.predict_for_user(20, items)
+    assert all(scores == counts.loc[items])
+
+
+def test_popscore_save_load():
+    original = basic.PopScore()
+    original.fit(lktu.ml_test.ratings)
+
+    mod = pickle.dumps(original)
+    algo = pickle.loads(mod)
+
+    assert all(algo.item_scores_ == original.item_scores_)
