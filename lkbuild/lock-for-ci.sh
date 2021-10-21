d28c6d1a030c0b34098618faa01ce622727f7e5e
Michael Ekstrand <michaelekstrand@boisestate.edu>
2021-10-21 13:29:16 -0600

    use dir

--- lkbuild/lock-for-ci.sh
+++ lkbuild/lock-for-ci.sh
@@ -2,7 +2,9 @@
 
 # Create a lock file for CI builds, and register with GitHub outputs.
 
-. ./lkbuild/bootstrap-env.sh
+SDIR=$(dirname $0)
+
+. "$SDIR/bootstrap-env.sh"
 
 vr invoke dev-lock "$@"
 echo "::set-output name=environment-file::conda-$CONDA_PLATFORM.lock"
