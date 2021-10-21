e0bd2b905e4ac131dab7d3585e8f04b6a6b0d8e4
Michael Ekstrand <michaelekstrand@boisestate.edu>
2021-10-21 15:37:53 -0600

    Remove MultiEval (#254)

--- lenskit/batch/__init__.py
+++ lenskit/batch/__init__.py
@@ -4,5 +4,4 @@ Batch-run predictors and recommenders for evaluation.
 
 from ._predict import predict  # noqa: F401
 from ._recommend import recommend  # noqa: F401
-from ._multi import MultiEval  # noqa: F401
 from ._train import train_isolated  # noqa: F401
