c1a53b9d57d0cb7827b031492120e63dd4fc934d
Michael Ekstrand <michaelekstrand@boisestate.edu>
2021-11-01 11:52:41 -0600

    Remove bibtex update from project-specific file

--- tasks.py
+++ tasks.py
@@ -1,16 +1,1 @@
-from pathlib import Path
-import requests
-from invoke import task
-
 from lkbuild.tasks import *
-
-BIBTEX_URL = 'https://paperpile.com/eb/YdOlWmnlit'
-BIBTEX_FILE = Path('doc/lenskit.bib')
-
-
-@task
-def update_bibtex(c):
-    "Update the BibTeX file"
-    res = requests.get(BIBTEX_URL)
-    print('updating file', BIBTEX_FILE)
-    BIBTEX_FILE.write_text(res.text, encoding='utf-8')
