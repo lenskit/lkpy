12d57cae77949ad2d1bb0c447d53a4bf4d37fef7
Michael Ekstrand <michaelekstrand@boisestate.edu>
2022-02-11 16:51:01 -0700

    Add documentation task

--- tasks.py
+++ tasks.py
@@ -1,1 +1,10 @@
 from lkbuild.tasks import *
+
+
+@task
+def docs(c, watch=False, rebuild=False):
+    rb = '-a' if rebuild else ''
+    if watch:
+        c.run(f'sphinx-autobuild {rb} --watch lenskit docs build/doc')
+    else:
+        c.run(f'sphinx-build {rb} docs build/doc')
