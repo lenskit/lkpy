1f279eb85c28e9f5a4d2629af229958daf540b75
Michael Ekstrand <michaelekstrand@boisestate.edu>
2022-07-27 16:33:41 -0600

    Increase 0.14 version number

--- lenskit/__init__.py
+++ lenskit/__init__.py
@@ -5,7 +5,7 @@ The LensKit toolkit for recommender systems research.
 
 from lenskit.algorithms import *  # noqa: F401,F403
 
-__version__ = '0.14.1'
+__version__ = '0.14.2'
 
 
 class DataWarning(UserWarning):
