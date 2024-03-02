b430741d11511bede9bb4d5787b3a992bd59c8e2
Michael Ekstrand <md@ekstrandom.net>
2024-03-02 18:39:28 -0500

    Add env-spec update task

--- tasks.py
+++ tasks.py
@@ -2,9 +2,15 @@ from lkbuild.tasks import *
 
 
 @task
+def update_env_specs(c):
+    "Update environment files."
+    c.run("pyproject2conda project")
+
+
+@task
 def docs(c, watch=False, rebuild=False):
-    rb = '-a' if rebuild else ''
+    rb = "-a" if rebuild else ""
     if watch:
-        c.run(f'sphinx-autobuild {rb} --watch lenskit docs build/doc')
+        c.run(f"sphinx-autobuild {rb} --watch lenskit docs build/doc")
     else:
-        c.run(f'sphinx-build {rb} docs build/doc')
+        c.run(f"sphinx-build {rb} docs build/doc")
