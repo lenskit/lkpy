11811fd7442c0aba73f19fff5918bb2fa75dbd91
Michael Ekstrand <md@ekstrandom.net>
2024-03-03 18:40:55 -0500

    fix setup.py build problems

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
-from Cython.Build import cythonize
 from setuptools import Extension, setup
+
+try:
+    from Cython.Build import cythonize
+
+    USE_CYTHON = True
+except ImportError:
+    USE_CYTHON = False
 
 EXT_SPECS = {"lenskit.util.kvp": None}
 
 
 def _make_extension(name: str, opts: None) -> Extension:
-    path = name.replace(".", "/") + ".pyx"
+    path = name.replace(".", "/")
+    if USE_CYTHON:
+        path += ".pyx"
+    else:
+        path += ".c"
     return Extension(name, [path])
 
 
 EXTENSIONS = [_make_extension(ext, opts) for (ext, opts) in EXT_SPECS.items()]
-setup(ext_modules=cythonize(EXTENSIONS))
+if USE_CYTHON:
+    EXTENSIONS = cythonize(EXTENSIONS)
+setup(ext_modules=EXTENSIONS)
