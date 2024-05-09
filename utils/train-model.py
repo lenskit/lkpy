08fb41a13536f5ffb85dadd3cf796af2c385b433
Michael Ekstrand <md@ekstrandom.net>
2024-05-09 14:00:20 -0400

    add missing license headers to utility scripts

--- utils/train-model.py
+++ utils/train-model.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+# This file is part of LensKit.
+# Copyright (C) 2018-2023 Boise State University
+# Copyright (C) 2023-2024 Drexel University
+# Licensed under the MIT license, see LICENSE.md for details.
+# SPDX-License-Identifier: MIT
+
 """
 Train a recommendation model and save it to disk.
 
