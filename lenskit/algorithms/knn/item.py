951294ffc6c6d67996bb9e36aa913e63ad7fe627
Michael Ekstrand <md@ekstrandom.net>
2024-05-25 15:15:14 +0200

    reorder item-item code

--- lenskit/algorithms/knn/item.py
+++ lenskit/algorithms/knn/item.py
@@ -8,6 +8,8 @@
 Item-based k-NN collaborative filtering.
 """
 
+from __future__ import annotations
+
 import logging
 import warnings
 from sys import intern
@@ -24,285 +26,8 @@ from lenskit.util.logging import pbh_update, progress_handle
 
 from .. import Predictor
 
-AggFun: TypeAlias = Callable[
-    [
-        torch.Tensor,
-        tuple[int, int],
-        torch.Tensor,
-        torch.Tensor,
-    ],
-    torch.Tensor,
-]
-
 _logger = logging.getLogger(__name__)
 MAX_BLOCKS = 1024
-
-
-@torch.jit.ignore  # type: ignore
-def _msg(level, msg):
-    # type: (int, str) -> None
-    _logger.log(level, msg)
-
-
-@torch.jit.ignore  # type: ignore
-def _item_dbg(item, msg):
-    # type: (int, str) -> None
-    _logger.debug("item %d: %s", item, msg)
-
-
-@torch.jit.script
-def _sim_row(
-    item: int, matrix: torch.Tensor, row: torch.Tensor, min_sim: float, max_nbrs: Optional[int]
-) -> tuple[int, torch.Tensor, torch.Tensor]:
-    nitems, nusers = matrix.shape
-    if len(row.indices()) == 0:
-        return 0, torch.zeros((0,), dtype=torch.int32), torch.zeros((0,), dtype=torch.float64)
-
-    # _item_dbg(item, f"comparing item with {row.indices().shape[1]} users")
-    # _item_dbg(item, f"row norm {torch.linalg.vector_norm(row.values()).item()}")
-    row = row.to_dense()
-    sim = torch.mv(matrix, row.to(torch.float64))
-    sim[item] = 0
-
-    mask = sim >= min_sim
-    cols = torch.nonzero(mask)[:, 0].to(torch.int32)
-    vals = sim[mask]
-    # _item_dbg(item, f"found {len(vals)} acceptable similarities")
-    assert len(cols) == torch.sum(mask)
-
-    if max_nbrs is not None and max_nbrs > 0 and max_nbrs < vals.shape[0]:
-        # _item_dbg(item, "truncating similarities")
-        vals, cis = torch.topk(vals, max_nbrs, sorted=False)
-        cols = cols[cis]
-        order = torch.argsort(cols)
-        cols = cols[order]
-        vals = vals[order]
-
-    return len(cols), cols, torch.clamp(vals, -1, 1)
-
-
-@torch.jit.script
-def _sim_block(
-    matrix: torch.Tensor, start: int, end: int, min_sim: float, max_nbrs: Optional[int], pbh: str
-) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-    "Compute a single block of the similarity matrix"
-    bsize = end - start
-
-    counts = torch.zeros(bsize, dtype=torch.int32)
-    columns = []
-    values = []
-
-    for i in range(start, end):
-        c, cs, vs = _sim_row(i, matrix, matrix[i], min_sim, max_nbrs)
-        counts[i - start] = c
-        columns.append(cs)
-        values.append(vs)
-        pbh_update(pbh, 1)
-
-    return counts, torch.cat(columns), torch.cat(values).to(torch.float32)
-
-
-@torch.jit.script
-def _sim_blocks(
-    matrix: torch.Tensor, min_sim: float, max_nbrs: Optional[int], block_size: int, pbh: str
-) -> torch.Tensor:
-    "Compute the similarity matrix with blocked matrix-matrix multiplies"
-    nitems, nusers = matrix.shape
-
-    jobs: list[torch.jit.Future[tuple[torch.Tensor, torch.Tensor, torch.Tensor]]] = []
-
-    for start in range(0, nitems, block_size):
-        end = min(start + block_size, nitems)
-        jobs.append(torch.jit.fork(_sim_block, matrix, start, end, min_sim, max_nbrs, pbh))  # type: ignore
-
-    counts = [torch.tensor([0], dtype=torch.int32)]
-    columns = []
-    values = []
-
-    for job in jobs:
-        cts, cis, vs = job.wait()
-        counts.append(cts)
-        columns.append(cis)
-        values.append(vs)
-
-    c_cat = torch.cat(counts)
-    crow_indices = torch.cumsum(c_cat, 0, dtype=torch.int32)
-    assert len(crow_indices) == nitems + 1
-    col_indices = torch.cat(columns)
-    c_values = torch.cat(values)
-    assert crow_indices[nitems] == len(col_indices)
-    assert crow_indices[nitems] == len(c_values)
-
-    return torch.sparse_csr_tensor(
-        crow_indices=crow_indices,
-        col_indices=col_indices,
-        values=c_values,
-        size=(nitems, nitems),
-    )
-
-
-def _predict_weighted_average(
-    model: torch.Tensor,
-    nrange: tuple[int, int],
-    rate_v: torch.Tensor,
-    rated: torch.Tensor,
-) -> torch.Tensor:
-    "Weighted average prediction function"
-    nitems, _ni = model.shape
-    assert nitems == _ni
-    min_nbrs, max_nbrs = nrange
-
-    # we proceed rating-by-rating, and accumulate results
-    scores = torch.zeros(nitems)
-    t_sims = torch.zeros(nitems)
-    counts = torch.zeros(nitems, dtype=torch.int32)
-    # these store the similarities and values for neighbors, so we can un-count
-    nbr_sims = torch.empty((nitems, max_nbrs))
-    nbr_vals = torch.empty((nitems, max_nbrs))
-    # and this stores the smallest similarity so far for each item
-    nbr_min = torch.full((nitems,), torch.finfo().max)
-
-    for i, iidx in enumerate(rated):
-        row = model[int(iidx)]
-        row_is = row.indices()[0]
-        row_vs = row.values()
-        assert row_is.shape == row_vs.shape
-
-        row_avs = torch.abs(row_vs)
-        fast = counts[row_is] < max_nbrs
-
-        # save the fast-path items
-        if torch.any(fast):
-            ris_fast = row_is[fast]
-            vs_fast = row_vs[fast]
-            avs_fast = row_avs[fast]
-            vals_fast = vs_fast * rate_v[i]
-            nbr_sims[ris_fast, counts[ris_fast]] = vs_fast
-            nbr_vals[ris_fast, counts[ris_fast]] = vals_fast
-            counts[ris_fast] += 1
-            t_sims[ris_fast] += avs_fast
-            scores[ris_fast] += vals_fast
-            nbr_min[ris_fast] = torch.minimum(nbr_min[ris_fast], vs_fast)
-
-        # skip early if we're done
-        if torch.all(fast):
-            continue
-
-        # now we have the slow-path items
-        slow = torch.logical_not(fast)
-        ris_slow = row_is[slow]
-        rvs_slow = row_vs[slow]
-        # which slow items might actually need an update?
-        exc = rvs_slow > nbr_min[ris_slow]
-        if not torch.any(exc):
-            continue
-
-        ris_slow = ris_slow[exc]
-        rvs_slow = rvs_slow[exc]
-
-        # this is brute-force linear search for simplicity right now
-        # for each, find the neighbor that's the smallest:
-        min_sims, mins = torch.min(nbr_sims[ris_slow], dim=1)
-        assert torch.all(min_sims < rvs_slow)
-
-        # now we need to update values: add in new and remove old
-        min_vals = nbr_vals[ris_slow, mins]
-        ravs_slow = row_avs[slow][exc]
-        slow_vals = rvs_slow * rate_v[i]
-        t_sims[ris_slow] += ravs_slow - min_sims.abs()
-        scores[ris_slow] += slow_vals - min_vals
-        # and save
-        nbr_sims[ris_slow, mins] = ravs_slow
-        nbr_vals[ris_slow, mins] = slow_vals
-        # and now we need to update the saved minimums
-        nm_sims, _nm_is = torch.min(nbr_sims[ris_slow], dim=1)
-        nbr_min[ris_slow] = nm_sims
-
-    # compute averages for items that pass match the threshold
-    mask = counts >= min_nbrs
-    scores[mask] /= t_sims[mask]
-    scores[torch.logical_not(mask)] = torch.nan
-
-    return scores
-
-
-def _predict_sum(
-    model: torch.Tensor,
-    nrange: tuple[int, int],
-    rate_v: torch.Tensor,
-    rated: torch.Tensor,
-) -> torch.Tensor:
-    "Sum-of-similarities prediction function"
-    nitems, _ni = model.shape
-    assert nitems == _ni
-    min_nbrs, max_nbrs = nrange
-    _msg(logging.DEBUG, f"sum-scoring with {len(rated)} items")
-
-    # we proceed rating-by-rating, and accumulate results
-    t_sims = torch.zeros(nitems)
-    counts = torch.zeros(nitems, dtype=torch.int32)
-    nbr_sims = torch.zeros((nitems, max_nbrs))
-    # and this stores the smallest similarity so far for each item
-    nbr_min = torch.full((nitems,), torch.finfo().max)
-
-    for i, iidx in enumerate(rated):
-        iidx = int(iidx)
-        row = model[iidx]
-        row_is = row.indices()[0]
-        row_vs = row.values()
-        assert row_is.shape == row_vs.shape
-
-        fast = counts[row_is] < max_nbrs
-
-        # save the fast-path items
-        if torch.any(fast):
-            ris_fast = row_is[fast]
-            vs_fast = row_vs[fast]
-            nbr_sims[ris_fast, counts[ris_fast]] = vs_fast
-            counts[ris_fast] += 1
-            t_sims[ris_fast] += vs_fast
-            nbr_min[ris_fast] = torch.minimum(nbr_min[ris_fast], vs_fast)
-
-        # skip early if we're done
-        if torch.all(fast):
-            continue
-
-        # now we have the slow-path items
-        slow = torch.logical_not(fast)
-        ris_slow = row_is[slow]
-        rvs_slow = row_vs[slow]
-        # which slow items might actually need an update?
-        exc = rvs_slow > nbr_min[ris_slow]
-        if not torch.any(exc):
-            continue
-
-        ris_slow = ris_slow[exc]
-        rvs_slow = rvs_slow[exc]
-
-        # this is brute-force linear search for simplicity right now
-        # for each, find the neighbor that's the smallest:
-        min_sims, mins = torch.min(nbr_sims[ris_slow], dim=1)
-
-        # now we need to update values: add in new and remove old
-        # anywhere our new neighbor is grater than smallest, replace smallest
-        t_sims[ris_slow] -= min_sims
-        t_sims[ris_slow] += rvs_slow
-        # and save
-        nbr_sims[ris_slow, mins] = rvs_slow
-        # save the minimums
-        nm_sims, _nm_is = torch.min(nbr_sims[ris_slow], dim=1)
-        nbr_min[ris_slow] = nm_sims
-
-    # compute averages for items that pass match the threshold
-    t_sims[counts < min_nbrs] = torch.nan
-
-    return t_sims
-
-
-_predictors: dict[str, AggFun] = {
-    "weighted-average": _predict_weighted_average,
-    "sum": _predict_sum,
-}
 
 
 class ItemItem(Predictor):
@@ -612,3 +337,274 @@ class ItemItem(Predictor):
 
     def __str__(self):
         return "ItemItem(nnbrs={}, msize={})".format(self.nnbrs, self.save_nbrs)
+
+
+@torch.jit.ignore  # type: ignore
+def _msg(level, msg):
+    # type: (int, str) -> None
+    _logger.log(level, msg)
+
+
+@torch.jit.script
+def _sim_row(
+    item: int, matrix: torch.Tensor, row: torch.Tensor, min_sim: float, max_nbrs: Optional[int]
+) -> tuple[int, torch.Tensor, torch.Tensor]:
+    nitems, nusers = matrix.shape
+    if len(row.indices()) == 0:
+        return 0, torch.zeros((0,), dtype=torch.int32), torch.zeros((0,), dtype=torch.float64)
+
+    # _item_dbg(item, f"comparing item with {row.indices().shape[1]} users")
+    # _item_dbg(item, f"row norm {torch.linalg.vector_norm(row.values()).item()}")
+    row = row.to_dense()
+    sim = torch.mv(matrix, row.to(torch.float64))
+    sim[item] = 0
+
+    mask = sim >= min_sim
+    cols = torch.nonzero(mask)[:, 0].to(torch.int32)
+    vals = sim[mask]
+    # _item_dbg(item, f"found {len(vals)} acceptable similarities")
+    assert len(cols) == torch.sum(mask)
+
+    if max_nbrs is not None and max_nbrs > 0 and max_nbrs < vals.shape[0]:
+        # _item_dbg(item, "truncating similarities")
+        vals, cis = torch.topk(vals, max_nbrs, sorted=False)
+        cols = cols[cis]
+        order = torch.argsort(cols)
+        cols = cols[order]
+        vals = vals[order]
+
+    return len(cols), cols, torch.clamp(vals, -1, 1)
+
+
+@torch.jit.script
+def _sim_block(
+    matrix: torch.Tensor, start: int, end: int, min_sim: float, max_nbrs: Optional[int], pbh: str
+) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    "Compute a single block of the similarity matrix"
+    bsize = end - start
+
+    counts = torch.zeros(bsize, dtype=torch.int32)
+    columns = []
+    values = []
+
+    for i in range(start, end):
+        c, cs, vs = _sim_row(i, matrix, matrix[i], min_sim, max_nbrs)
+        counts[i - start] = c
+        columns.append(cs)
+        values.append(vs)
+        pbh_update(pbh, 1)
+
+    return counts, torch.cat(columns), torch.cat(values).to(torch.float32)
+
+
+@torch.jit.script
+def _sim_blocks(
+    matrix: torch.Tensor, min_sim: float, max_nbrs: Optional[int], block_size: int, pbh: str
+) -> torch.Tensor:
+    "Compute the similarity matrix with blocked matrix-matrix multiplies"
+    nitems, nusers = matrix.shape
+
+    jobs: list[torch.jit.Future[tuple[torch.Tensor, torch.Tensor, torch.Tensor]]] = []
+
+    for start in range(0, nitems, block_size):
+        end = min(start + block_size, nitems)
+        jobs.append(torch.jit.fork(_sim_block, matrix, start, end, min_sim, max_nbrs, pbh))  # type: ignore
+
+    counts = [torch.tensor([0], dtype=torch.int32)]
+    columns = []
+    values = []
+
+    for job in jobs:
+        cts, cis, vs = job.wait()
+        counts.append(cts)
+        columns.append(cis)
+        values.append(vs)
+
+    c_cat = torch.cat(counts)
+    crow_indices = torch.cumsum(c_cat, 0, dtype=torch.int32)
+    assert len(crow_indices) == nitems + 1
+    col_indices = torch.cat(columns)
+    c_values = torch.cat(values)
+    assert crow_indices[nitems] == len(col_indices)
+    assert crow_indices[nitems] == len(c_values)
+
+    return torch.sparse_csr_tensor(
+        crow_indices=crow_indices,
+        col_indices=col_indices,
+        values=c_values,
+        size=(nitems, nitems),
+    )
+
+
+def _predict_weighted_average(
+    model: torch.Tensor,
+    nrange: tuple[int, int],
+    rate_v: torch.Tensor,
+    rated: torch.Tensor,
+) -> torch.Tensor:
+    "Weighted average prediction function"
+    nitems, _ni = model.shape
+    assert nitems == _ni
+    min_nbrs, max_nbrs = nrange
+
+    # we proceed rating-by-rating, and accumulate results
+    scores = torch.zeros(nitems)
+    t_sims = torch.zeros(nitems)
+    counts = torch.zeros(nitems, dtype=torch.int32)
+    # these store the similarities and values for neighbors, so we can un-count
+    nbr_sims = torch.empty((nitems, max_nbrs))
+    nbr_vals = torch.empty((nitems, max_nbrs))
+    # and this stores the smallest similarity so far for each item
+    nbr_min = torch.full((nitems,), torch.finfo().max)
+
+    for i, iidx in enumerate(rated):
+        row = model[int(iidx)]
+        row_is = row.indices()[0]
+        row_vs = row.values()
+        assert row_is.shape == row_vs.shape
+
+        row_avs = torch.abs(row_vs)
+        fast = counts[row_is] < max_nbrs
+
+        # save the fast-path items
+        if torch.any(fast):
+            ris_fast = row_is[fast]
+            vs_fast = row_vs[fast]
+            avs_fast = row_avs[fast]
+            vals_fast = vs_fast * rate_v[i]
+            nbr_sims[ris_fast, counts[ris_fast]] = vs_fast
+            nbr_vals[ris_fast, counts[ris_fast]] = vals_fast
+            counts[ris_fast] += 1
+            t_sims[ris_fast] += avs_fast
+            scores[ris_fast] += vals_fast
+            nbr_min[ris_fast] = torch.minimum(nbr_min[ris_fast], vs_fast)
+
+        # skip early if we're done
+        if torch.all(fast):
+            continue
+
+        # now we have the slow-path items
+        slow = torch.logical_not(fast)
+        ris_slow = row_is[slow]
+        rvs_slow = row_vs[slow]
+        # which slow items might actually need an update?
+        exc = rvs_slow > nbr_min[ris_slow]
+        if not torch.any(exc):
+            continue
+
+        ris_slow = ris_slow[exc]
+        rvs_slow = rvs_slow[exc]
+
+        # this is brute-force linear search for simplicity right now
+        # for each, find the neighbor that's the smallest:
+        min_sims, mins = torch.min(nbr_sims[ris_slow], dim=1)
+        assert torch.all(min_sims < rvs_slow)
+
+        # now we need to update values: add in new and remove old
+        min_vals = nbr_vals[ris_slow, mins]
+        ravs_slow = row_avs[slow][exc]
+        slow_vals = rvs_slow * rate_v[i]
+        t_sims[ris_slow] += ravs_slow - min_sims.abs()
+        scores[ris_slow] += slow_vals - min_vals
+        # and save
+        nbr_sims[ris_slow, mins] = ravs_slow
+        nbr_vals[ris_slow, mins] = slow_vals
+        # and now we need to update the saved minimums
+        nm_sims, _nm_is = torch.min(nbr_sims[ris_slow], dim=1)
+        nbr_min[ris_slow] = nm_sims
+
+    # compute averages for items that pass match the threshold
+    mask = counts >= min_nbrs
+    scores[mask] /= t_sims[mask]
+    scores[torch.logical_not(mask)] = torch.nan
+
+    return scores
+
+
+def _predict_sum(
+    model: torch.Tensor,
+    nrange: tuple[int, int],
+    rate_v: torch.Tensor,
+    rated: torch.Tensor,
+) -> torch.Tensor:
+    "Sum-of-similarities prediction function"
+    nitems, _ni = model.shape
+    assert nitems == _ni
+    min_nbrs, max_nbrs = nrange
+    _msg(logging.DEBUG, f"sum-scoring with {len(rated)} items")
+
+    # we proceed rating-by-rating, and accumulate results
+    t_sims = torch.zeros(nitems)
+    counts = torch.zeros(nitems, dtype=torch.int32)
+    nbr_sims = torch.zeros((nitems, max_nbrs))
+    # and this stores the smallest similarity so far for each item
+    nbr_min = torch.full((nitems,), torch.finfo().max)
+
+    for i, iidx in enumerate(rated):
+        iidx = int(iidx)
+        row = model[iidx]
+        row_is = row.indices()[0]
+        row_vs = row.values()
+        assert row_is.shape == row_vs.shape
+
+        fast = counts[row_is] < max_nbrs
+
+        # save the fast-path items
+        if torch.any(fast):
+            ris_fast = row_is[fast]
+            vs_fast = row_vs[fast]
+            nbr_sims[ris_fast, counts[ris_fast]] = vs_fast
+            counts[ris_fast] += 1
+            t_sims[ris_fast] += vs_fast
+            nbr_min[ris_fast] = torch.minimum(nbr_min[ris_fast], vs_fast)
+
+        # skip early if we're done
+        if torch.all(fast):
+            continue
+
+        # now we have the slow-path items
+        slow = torch.logical_not(fast)
+        ris_slow = row_is[slow]
+        rvs_slow = row_vs[slow]
+        # which slow items might actually need an update?
+        exc = rvs_slow > nbr_min[ris_slow]
+        if not torch.any(exc):
+            continue
+
+        ris_slow = ris_slow[exc]
+        rvs_slow = rvs_slow[exc]
+
+        # this is brute-force linear search for simplicity right now
+        # for each, find the neighbor that's the smallest:
+        min_sims, mins = torch.min(nbr_sims[ris_slow], dim=1)
+
+        # now we need to update values: add in new and remove old
+        # anywhere our new neighbor is grater than smallest, replace smallest
+        t_sims[ris_slow] -= min_sims
+        t_sims[ris_slow] += rvs_slow
+        # and save
+        nbr_sims[ris_slow, mins] = rvs_slow
+        # save the minimums
+        nm_sims, _nm_is = torch.min(nbr_sims[ris_slow], dim=1)
+        nbr_min[ris_slow] = nm_sims
+
+    # compute averages for items that pass match the threshold
+    t_sims[counts < min_nbrs] = torch.nan
+
+    return t_sims
+
+
+AggFun: TypeAlias = Callable[
+    [
+        torch.Tensor,
+        tuple[int, int],
+        torch.Tensor,
+        torch.Tensor,
+    ],
+    torch.Tensor,
+]
+
+_predictors: dict[str, AggFun] = {
+    "weighted-average": _predict_weighted_average,
+    "sum": _predict_sum,
+}
