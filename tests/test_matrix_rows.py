0dac656706a1baad132646cc99203896e8d1b198
Michael Ekstrand <md@ekstrandom.net>
2024-05-26 12:46:08 -0400

    add tests for row stats

---
+++ tests/test_matrix_rows.py
@@ -0,0 +1,38 @@
+"""
+Tests for matrix row utilities.
+"""
+
+import logging
+
+import numpy as np
+import pandas as pd
+import scipy.sparse as sps
+import torch
+
+import hypothesis.extra.numpy as nph
+import hypothesis.strategies as st
+from hypothesis import assume, given
+from pytest import approx, mark
+
+from lenskit.data.matrix import sparse_row_stats
+from lenskit.util.test import sparse_tensors
+
+_log = logging.getLogger(__name__)
+
+
+@given(sparse_tensors())
+def test_sparse_stats(tensor):
+    nr, nc = tensor.shape
+    _log.debug("tensor: %d x %d", nr, nc)
+
+    stats = sparse_row_stats(tensor)
+    assert stats.means.shape == (nr,)
+    assert stats.counts.shape == (nr,)
+
+    assert np.sum(stats.counts.numpy()) == tensor.values().shape[0]
+
+    sums = tensor.sum(dim=1, keepdim=True)
+    sums = sums.to_dense().reshape(-1)
+    tots = stats.means * stats.counts
+    mask = stats.counts.numpy() > 0
+    assert tots.numpy()[mask] == approx(sums.numpy()[mask])
