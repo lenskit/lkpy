b9fd7a396be17fec36cd672b4141fa8b20daa8df
Michael Ekstrand <md@ekstrandom.net>
2024-05-29 17:23:28 -0400

    relax dense tests slightly

--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -156,8 +156,10 @@ def test_torch_spmv(torch_device, data, M: sps.coo_array, layout):
     tv = torch.from_numpy(v).to(torch_device)
 
     # quick make sure that dense works
-    assert M.todense() @ v == approx(res)
-    assert torch.mv(torch.from_numpy(M.todense()).to(torch_device), tv).cpu().numpy() == approx(res)
+    assert M.todense() @ v == approx(res, rel=1.0e-5)
+    assert torch.mv(torch.from_numpy(M.todense()).to(torch_device), tv).cpu().numpy() == approx(
+        res, rel=1.0e-5
+    )
 
     tres = torch.mv(TM, tv)
     tres = tres.nan_to_num()
