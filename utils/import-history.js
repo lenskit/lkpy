66beda5cf73fc0e97d3c9cabbf48f9cf93052014
Michael Ekstrand <mdekstrand@drexel.edu>
2024-07-15 16:24:22 -0400

    add coverage history script

---
+++ utils/import-history.js
@@ -0,0 +1,28 @@
+let logs = JSON.parse(await Deno.readTextFile('build/historical-coverage.json'));
+console.log('read %d records', logs.count);
+
+for (let c of logs.results) {
+    if (!c.totals) continue;
+
+    let blob = {
+        meta: {
+            source: 'codecov',
+        },
+        totals: {
+            covered_lines: c.totals.hits,
+            percent_covered: c.totals.coverage,
+            missing_lines: c.totals.misses,
+        }
+    }
+    console.log('commit %s: %o', c.commitid, blob);
+    let cmd = new Deno.Command('git', {
+        args: ['notes', '--ref=coverage','add',  '-m', JSON.stringify(blob) + '\n', c.commitid],
+        stdout: 'inherit',
+        stderr: 'inherit',
+    });
+    let out = await cmd.output();
+    if (out.code != 0) {
+        console.error('git notes failed with code %s', out.code);
+        Deno.exit(5)
+    }
+}
