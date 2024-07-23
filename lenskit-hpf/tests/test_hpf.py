612df6dcc2dde62baa49d1247623987ea3fed8e2
Michael Ekstrand <mdekstrand@drexel.edu>
2024-07-23 15:56:38 -0400

    use dataset for hpf

--- lenskit-hpf/tests/test_hpf.py
+++ lenskit-hpf/tests/test_hpf.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 from pytest import importorskip, mark
 
+from lenskit.data.dataset import from_interactions_df
 import lenskit.util.test as lktu
 from lenskit.algorithms import basic
 
@@ -23,7 +24,8 @@ def test_hpf_train_large(tmp_path):
     algo = hpf.HPF(20)
     ratings = lktu.ml_test.ratings
     ratings = ratings.assign(rating=ratings.rating + 0.5)
-    algo.fit(ratings)
+    ds = from_interactions_df(ratings)
+    algo.fit(ds)
 
     assert algo.n_users == ratings.user.nunique()
     assert algo.n_items == ratings.item.nunique()
@@ -39,7 +41,7 @@ def test_hpf_train_large(tmp_path):
     assert a2.n_items == algo.n_items
 
     csel = basic.UnratedItemCandidateSelector()
-    csel.fit(ratings)
+    csel.fit(ds)
     rec = basic.TopN(algo, csel)
 
     for u in np.random.choice(ratings.user.unique(), size=50, replace=False):
@@ -52,7 +54,8 @@ def test_hpf_train_large(tmp_path):
 def test_hpf_train_binary(tmp_path):
     algo = hpf.HPF(20)
     ratings = lktu.ml_test.ratings.drop(columns=["timestamp", "rating"])
-    algo.fit(ratings)
+    ds = from_interactions_df(ratings)
+    algo.fit(ds)
 
     assert algo.n_users == ratings.user.nunique()
     assert algo.n_items == ratings.item.nunique()
@@ -68,7 +71,7 @@ def test_hpf_train_binary(tmp_path):
     assert a2.n_items == algo.n_items
 
     csel = basic.UnratedItemCandidateSelector()
-    csel.fit(ratings)
+    csel.fit(ds)
     rec = basic.TopN(algo, csel)
 
     for u in np.random.choice(ratings.user.unique(), size=50, replace=False):
