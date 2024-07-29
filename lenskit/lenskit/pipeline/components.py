c1b1b3d3e0a49b1e7905311bbcc9b61580652bb1
Michael Ekstrand <mdekstrand@drexel.edu>
2024-07-29 12:49:34 -0400

    add missing headers

--- lenskit/lenskit/pipeline/components.py
+++ lenskit/lenskit/pipeline/components.py
@@ -1,3 +1,9 @@
+# This file is part of LensKit.
+# Copyright (C) 2018-2023 Boise State University
+# Copyright (C) 2023-2024 Drexel University
+# Licensed under the MIT license, see LICENSE.md for details.
+# SPDX-License-Identifier: MIT
+
 "Definition of the component interfaces."
 
 from typing_extensions import Any, Generic, Protocol, Self, TypeVar, runtime_checkable
