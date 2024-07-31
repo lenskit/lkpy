a84f8d80752ddb757a10ab01486d9b94c033da44
Michael Ekstrand <mdekstrand@drexel.edu>
2024-07-31 10:48:17 -0400

    document item list fields and get them working

--- lenskit/tests/test_itemlist.py
+++ lenskit/tests/test_itemlist.py
@@ -1,3 +1,9 @@
+# This file is part of LensKit.
+# Copyright (C) 2018-2023 Boise State University
+# Copyright (C) 2023-2024 Drexel University
+# Licensed under the MIT license, see LICENSE.md for details.
+# SPDX-License-Identifier: MIT
+
 import numpy as np
 import torch
 
@@ -29,7 +35,27 @@ def test_item_list():
         il.numbers()
 
 
-def test_item_num_list():
+def test_item_list_alias():
+    il = ItemList(item_id=["one", "two"])
+
+    assert len(il) == 2
+    assert il.ids().shape == (2,)
+
+    with raises(RuntimeError, match="item numbers not available"):
+        il.numbers()
+
+
+def test_item_list_bad_type():
+    with raises(TypeError):
+        ItemList(item_id=[3.4, 7.2])
+
+
+def test_item_list_bad_dimension():
+    with raises(TypeError):
+        ItemList(item_id=[["one", "two"]])
+
+
+def test_item_num_array():
     il = ItemList(item_nums=np.arange(5))
 
     assert len(il) == 5
@@ -37,6 +63,31 @@ def test_item_num_list():
 
     with raises(RuntimeError, match="item IDs not available"):
         il.ids()
+
+
+def test_item_num_alias():
+    il = ItemList(item_num=np.arange(5))
+
+    assert len(il) == 5
+    assert il.numbers().shape == (5,)
+
+    with raises(RuntimeError, match="item IDs not available"):
+        il.ids()
+
+
+def test_item_num_bad_type():
+    with raises(TypeError):
+        ItemList(item_num=np.random.randn(5))
+
+
+def test_item_num_bad_dims():
+    with raises(TypeError):
+        ItemList(item_num=[[1, 3, 8, 4]])
+
+
+def test_item_ids_num_mismatch_sizes():
+    with raises(TypeError, match="has incorrect shape"):
+        ItemList(item_ids=ITEMS, item_num=np.arange(4))
 
 
 def test_item_num_list_vocab():
