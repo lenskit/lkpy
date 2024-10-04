ac419a9d1c5e05e6a47fae14d410a39540d626ae
Michael Ekstrand <mdekstrand@drexel.edu>
2024-10-04 13:55:21 -0400

    add and test a TopN component

--- lenskit/lenskit/basic/topn.py
+++ lenskit/lenskit/basic/topn.py
@@ -1,3 +1,64 @@
 """
 Basic Top-*N* ranking.
 """
+
+import numpy as np
+
+from lenskit.data import ItemList
+from lenskit.pipeline import Component
+
+
+class TopN(Component):
+    """
+    Rank scored items by their score and take the top *N*.  The ranking length
+    can be passed either at runtime or at component instantiation time, with the
+    latter taking precedence.
+
+    Args:
+        n:
+            The desired ranking length.  If negative, then scored items are
+            ranked but the ranking is not truncated.
+    """
+
+    n: int
+
+    def __init__(self, n: int = -1):
+        self.n = n
+
+    def __call__(self, *, items: ItemList, n: int | None = None) -> ItemList:
+        """
+        Rank the items.
+
+        Args:
+            items:
+                The items to rank, with scores.  Items with missing scores are
+                not included in the final ranking.
+            n:
+                The number of items to return, or -1 to return all scored items.
+                If ``None``, the length configured at construction time is used.
+
+        Returns:
+            An ordered list of items, with scores and all other attributes
+            preserved.
+        """
+        if n is None:
+            n = self.n
+
+        scores = items.scores("numpy")
+        if scores is None:
+            raise RuntimeError("input item list has no scores")
+
+        # find and filter out invalid scores
+        v_mask = ~np.isnan(scores)
+        items = items[v_mask]
+
+        # order remaining scores
+        order = np.argsort(scores[v_mask])
+        if n >= 0 and n < len(order):
+            order = order[: -(n + 1) : -1]
+        else:
+            order = order[::-1]
+
+        # now we need to return in expected order
+        result = items[order]
+        return result
---
+++ lenskit/tests/basic/test_topn.py
@@ -0,0 +1,142 @@
+# This file is part of LensKit.
+# Copyright (C) 2018-2023 Boise State University
+# Copyright (C) 2023-2024 Drexel University
+# Licensed under the MIT license, see LICENSE.md for details.
+# SPDX-License-Identifier: MIT
+
+import logging
+import pickle
+
+import numpy as np
+import pandas as pd
+from numpy.typing import NDArray
+
+import hypothesis.extra.numpy as nph
+import hypothesis.strategies as st
+from hypothesis import given, settings
+
+import lenskit.util.test as lktu
+from lenskit.basic import PopScore
+from lenskit.basic.topn import TopN
+from lenskit.data.items import ItemList
+from lenskit.util.test import ml_ds, ml_ratings  # noqa: F401
+
+_log = logging.getLogger(__name__)
+
+
+@st.composite
+def scored_lists(draw: st.DrawFn) -> ItemList:
+    n = draw(st.integers(0, 1000))
+    ids = np.arange(1, n + 1, dtype=np.int32)
+    scores = draw(nph.arrays(nph.floating_dtypes(endianness="=", sizes=[32, 64]), n))
+    return ItemList(item_ids=ids, scores=scores)
+
+
+@given(scored_lists())
+def test_unlimited_ranking(items: ItemList):
+    topn = TopN()
+    ranked = topn(items=items)
+
+    ids = items.ids()
+    scores = items.scores("numpy")
+    assert scores is not None
+    invalid = np.isnan(scores)
+    _log.info("ranking %d items, %d invalid", len(ids), np.sum(invalid))
+
+    assert isinstance(ranked, ItemList)
+    assert len(ranked) <= len(items)
+    # all valid items are included
+    assert len(ranked) == np.sum(~invalid)
+
+    # the set of valid items matches
+    assert set(ids[~invalid]) == set(ranked.ids())
+
+    # the scores match
+    rank_s = ranked.scores("pandas", index="ids")
+    assert rank_s is not None
+    src_s = items.scores("pandas", index="ids")
+    assert src_s is not None
+
+    # make sure the scores were preserved properly
+    rank_s, src_s = rank_s.align(src_s, "left")
+    assert not np.any(np.isnan(src_s))
+    assert np.all(rank_s == src_s)
+
+    # make sure it's sorted
+    assert np.all(rank_s.diff()) >= 0
+
+
+@given(st.integers(min_value=1, max_value=100), scored_lists())
+def test_configured_truncation(n, items: ItemList):
+    topn = TopN(n)
+    ranked = topn(items=items)
+
+    ids = items.ids()
+    scores = items.scores("numpy")
+    assert scores is not None
+    invalid = np.isnan(scores)
+    _log.info("top %d of %d items, %d invalid", n, len(ids), np.sum(invalid))
+
+    val_items = items[~invalid]
+
+    assert isinstance(ranked, ItemList)
+    assert len(ranked) == min(n, len(val_items))
+
+    # the scores match
+    rank_s = ranked.scores("pandas", index="ids")
+    assert rank_s is not None
+    src_s = items.scores("pandas", index="ids")
+    assert src_s is not None
+    src_s = src_s[src_s.notna()]
+
+    rem_s = src_s[src_s.index.difference(rank_s.index)]
+
+    # make sure the scores were preserved properly
+    rank_s, src_s = rank_s.align(src_s, "left")
+    assert not np.any(np.isnan(src_s))
+    assert np.all(rank_s == src_s)
+
+    # make sure it's sorted
+    assert np.all(rank_s.diff()) >= 0
+
+    # make sure it's the largest
+    if len(rem_s) > 0:
+        assert np.all(rank_s >= np.max(rem_s))
+
+
+@given(st.integers(min_value=1, max_value=100), scored_lists())
+def test_runtime_truncation(n, items: ItemList):
+    topn = TopN()
+    ranked = topn(items=items, n=n)
+
+    ids = items.ids()
+    scores = items.scores("numpy")
+    assert scores is not None
+    invalid = np.isnan(scores)
+    _log.info("top %d of %d items, %d invalid", n, len(ids), np.sum(invalid))
+
+    val_items = items[~invalid]
+
+    assert isinstance(ranked, ItemList)
+    assert len(ranked) == min(n, len(val_items))
+
+    # the scores match
+    rank_s = ranked.scores("pandas", index="ids")
+    assert rank_s is not None
+    src_s = items.scores("pandas", index="ids")
+    assert src_s is not None
+    src_s = src_s[src_s.notna()]
+
+    rem_s = src_s[src_s.index.difference(rank_s.index)]
+
+    # make sure the scores were preserved properly
+    rank_s, src_s = rank_s.align(src_s, "left")
+    assert not np.any(np.isnan(src_s))
+    assert np.all(rank_s == src_s)
+
+    # make sure it's sorted
+    assert np.all(rank_s.diff()) >= 0
+
+    # make sure it's the largest
+    if len(rem_s) > 0:
+        assert np.all(rank_s >= np.max(rem_s))
