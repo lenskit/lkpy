37f71f2411bbdcbd6db7ad98eba436fd854ba7c6
Michael Ekstrand <mdekstrand@drexel.edu>
2024-10-04 18:43:25 -0400

    initial pass at a composite component

---
+++ lenskit/lenskit/basic/composite.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+import logging
+
+import numpy as np
+
+from lenskit.data.items import ItemList
+from lenskit.pipeline import Component
+
+_logger = logging.getLogger(__name__)
+
+
+class FallbackScorer(Component):
+    """
+    Scoring component that fills in missing scores using a fallback.
+    """
+
+    def __call__(self, scores: ItemList, backup: ItemList) -> ItemList:
+        s = scores.scores()
+        if s is None:
+            return backup
+
+        s = s.copy()
+        missing = np.isnan(s)
+        if not np.any(missing):
+            return scores
+
+        bs = backup.scores("pandas", index="ids")
+        if bs is None:
+            return scores
+
+        s[missing] = bs.reindex(scores.ids()[missing]).values
+        return ItemList(scores, scores=s)
---
+++ lenskit/tests/basic/test_composite.py
@@ -0,0 +1,53 @@
+# This file is part of LensKit.
+# Copyright (C) 2018-2023 Boise State University
+# Copyright (C) 2023-2024 Drexel University
+# Licensed under the MIT license, see LICENSE.md for details.
+# SPDX-License-Identifier: MIT
+
+import pickle
+from typing import Any
+
+import numpy as np
+import pandas as pd
+
+from pytest import approx
+
+import lenskit.util.test as lktu
+from lenskit import util as lku
+from lenskit.basic import BiasScorer
+from lenskit.basic.composite import FallbackScorer
+from lenskit.basic.history import KnownRatingScorer
+from lenskit.data import Dataset
+from lenskit.data.items import ItemList
+from lenskit.data.types import EntityId
+from lenskit.pipeline import Pipeline
+from lenskit.util.test import ml_ds, ml_ratings  # noqa: F401
+
+
+def test_fallback_fill_missing(ml_ds: Dataset):
+    pipe = Pipeline()
+    user = pipe.create_input("user", int)
+    items = pipe.create_input("items")
+
+    known = KnownRatingScorer()
+    s1 = pipe.add_component("known", known, query=user, items=items)
+    bias = BiasScorer()
+    s2 = pipe.add_component("bias", bias, query=user, items=items)
+
+    fallback = FallbackScorer()
+    score = pipe.add_component("mix", fallback, scores=s1, backup=s2)
+
+    pipe.train(ml_ds)
+
+    # the first 2 of these are rated, the 3rd does not exist, and the other 2 are not rated
+    items = [50, 17, 210, 1172, 2455]
+    scored = pipe.run(score, user=2, items=ItemList(item_ids=items))
+
+    assert len(scored) == len(items)
+    assert np.all(scored.ids() == items)
+    scores = scored.scores()
+    assert scores is not None
+    assert not np.any(np.isnan(scored.scores()))
+
+    assert np.all(scores[:2] == known(2, ItemList(item_ids=items[:2])))
+    assert np.all(scores[2:] == bias(2, ItemList(item_ids=items[2:])))
