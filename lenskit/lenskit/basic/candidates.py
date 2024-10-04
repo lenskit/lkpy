b1ebc5395764a587a47c5250032878d0377f257e
Michael Ekstrand <mdekstrand@drexel.edu>
2024-10-04 14:44:07 -0400

    add candidate selector components

---
+++ lenskit/lenskit/basic/candidates.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+
+import logging
+
+import numpy as np
+from typing_extensions import override
+
+from lenskit.data import Dataset, ItemList, QueryInput, RecQuery
+from lenskit.pipeline import Component, Trainable
+
+_logger = logging.getLogger(__name__)
+
+
+class TrainingCandidateSelectorBase(Component, Trainable):
+    """
+    Base class for candidate selectors using the training data.
+    """
+
+    @override
+    def train(self, data: Dataset):
+        self.items_ = data.items.copy()
+
+
+class AllTrainingItemsCandidateSelector(TrainingCandidateSelectorBase):
+    """
+    Candidate selector that selects all known items from the training data.
+    """
+
+    def __call__(self) -> ItemList:
+        return ItemList.from_vocabulary(self.items_)
+
+
+class UnratedTrainingItemsCandidateSelector(TrainingCandidateSelectorBase):
+    """
+    Candidate selector that selects all known items from the training data that
+    do not appear in the request user's history (:attr:`RecQuery.user_items`).
+    If no item history is available, then all training items are returned.
+
+    In order to look up the user's history in the training data, this needs to
+    be combined with a component like
+    :class:`~.history.UserTrainingHistoryLookup`.
+    """
+
+    def __call__(self, query: QueryInput) -> ItemList:
+        query = RecQuery.create(query)
+        items = ItemList.from_vocabulary(self.items_)
+
+        if query.user_items is not None:
+            mask = np.full(len(self.items_), True, np.bool_)
+            qis = query.user_items.numbers(vocabulary=self.items_)
+            qis = qis[qis >= 0]
+            mask[qis] = False
+            items = items[mask]
+
+        return items
---
+++ lenskit/tests/basic/test_candidates.py
@@ -0,0 +1,38 @@
+# This file is part of LensKit.
+# Copyright (C) 2018-2023 Boise State University
+# Copyright (C) 2023-2024 Drexel University
+# Licensed under the MIT license, see LICENSE.md for details.
+# SPDX-License-Identifier: MIT
+
+import numpy as np
+import pandas as pd
+
+import lenskit.util.test as lktu
+from lenskit.algorithms import basic
+from lenskit.basic.candidates import (
+    AllTrainingItemsCandidateSelector,
+    UnratedTrainingItemsCandidateSelector,
+)
+from lenskit.data import Dataset, from_interactions_df
+from lenskit.util.test import ml_ds, ml_ratings  # noqa: F401
+
+
+def test_all(ml_ds: Dataset):
+    sel = AllTrainingItemsCandidateSelector()
+    sel.train(ml_ds)
+
+    cands = sel()
+    assert len(cands) == ml_ds.item_count
+    assert np.all(cands.ids() == ml_ds.items.ids())
+
+
+def test_unrated_selector(ml_ds: Dataset):
+    sel = UnratedTrainingItemsCandidateSelector()
+    sel.train(ml_ds)
+
+    row = ml_ds.user_row(100)
+    assert row is not None
+    cands = sel(query=row)
+
+    assert len(cands) <= ml_ds.item_count
+    assert len(cands) == len(set(ml_ds.items.ids()) - set(row.ids()))
