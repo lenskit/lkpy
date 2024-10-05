108a71a6a5c045278efc3ca489e52703d52187ce
Michael Ekstrand <mdekstrand@drexel.edu>
2024-10-05 16:18:20 -0400

    more basic imports

--- lenskit/lenskit/basic/__init__.py
+++ lenskit/lenskit/basic/__init__.py
@@ -3,8 +3,16 @@ Various basic pipeline components.
 """
 
 from .bias import BiasScorer
+from .candidates import AllTrainingItemsCandidateSelector, UnratedTrainingItemsCandidateSelector
 from .history import UserTrainingHistoryLookup
 from .popularity import PopScorer
 from .topn import TopNRanker
 
-__all__ = ["BiasScorer", "PopScorer", "TopNRanker", "UserTrainingHistoryLookup"]
+__all__ = [
+    "BiasScorer",
+    "PopScorer",
+    "TopNRanker",
+    "UserTrainingHistoryLookup",
+    "UnratedTrainingItemsCandidateSelector",
+    "AllTrainingItemsCandidateSelector",
+]
