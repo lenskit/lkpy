8a2953ca2094ee124357552560fe61868dbb3e80
Michael Ekstrand <mdekstrand@drexel.edu>
2024-10-10 11:56:40 -0400

    remove lkdev code

--- lkdev/workflows/__init__.py
+++
@@ -1,9 +0,0 @@
-# This file is part of LensKit.
-# Copyright (C) 2018-2023 Boise State University
-# Copyright (C) 2023-2024 Drexel University
-# Licensed under the MIT license, see LICENSE.md for details.
-# SPDX-License-Identifier: MIT
-
-"""
-Workflow definitions for LensKit build.
-"""
