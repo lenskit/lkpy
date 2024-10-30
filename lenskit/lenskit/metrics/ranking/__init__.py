70ca0bbe871df6d4ce0ba93e305c3ee3033ca4b5
Michael Ekstrand <mdekstrand@drexel.edu>
2024-10-30 17:41:02 -0400

    add top-N accuracy metric docs

--- lenskit/lenskit/metrics/ranking/__init__.py
+++ lenskit/lenskit/metrics/ranking/__init__.py
@@ -2,10 +2,20 @@
 LensKit ranking (and list) metrics.
 """
 
+from ._base import RankingMetric, RankingMetricBase
 from ._dcg import NDCG
 from ._hit import hit
 from ._pr import precision, recall
 from ._rbp import RBP
 from ._recip import recip_rank
 
-__all__ = ["hit", "precision", "recall", "recip_rank", "NDCG", "RBP"]
+__all__ = [
+    "RankingMetric",
+    "RankingMetricBase",
+    "hit",
+    "precision",
+    "recall",
+    "recip_rank",
+    "NDCG",
+    "RBP",
+]
