48271a984857c9f0f6069b78a755a0cf34d1b4fa
Michael Ekstrand <mdekstrand@drexel.edu>
2024-11-10 18:01:19 -0500

    add bulk iteration capabilities

--- lenskit/lenskit/data/bulk.py
+++ lenskit/lenskit/data/bulk.py
@@ -13,7 +13,7 @@ from typing import Iterator, cast, overload
 import pandas as pd
 
 from .items import ItemList
-from .schemas import USER_COMPAT_COLUMN, column_name, normalize_columns
+from .schemas import ITEM_COMPAT_COLUMN, USER_COMPAT_COLUMN, column_name, normalize_columns
 from .types import Column, EntityId
 
 
@@ -50,23 +50,43 @@ def dict_from_df(df: pd.DataFrame, *, column: str = "user_id") -> dict[EntityId,
     return {u: ItemList.from_df(udf) for (u, udf) in df.groupby(column)}  # type: ignore
 
 
-def group_df(df: pd.DataFrame, *, column: Column = USER_COMPAT_COLUMN):
-    df = normalize_columns(df, column)
+def group_df(
+    df: pd.DataFrame, *, column: Column = USER_COMPAT_COLUMN, require_cols=[ITEM_COMPAT_COLUMN]
+):
+    """
+    Group a data frame by a specified column, possibly checking for and
+    normalizing the names of other columns as well.  The default options group
+    by ``user_id`` and require an ``item_id`` column, allowing the compatibility
+    aliases ``user`` and ``item``.
+    """
+    df = normalize_columns(df, column, *require_cols)
     col = column_name(column)
     return df.groupby(col)
 
 
 @overload
-def iter_groups(data: dict[EntityId, ItemList]) -> Iterator[tuple[EntityId, ItemList]]: ...
+def iter_item_lists(data: dict[EntityId, ItemList]) -> Iterator[tuple[EntityId, ItemList]]: ...
 @overload
-def iter_groups(
-    data: pd.DataFrame | dict[EntityId, ItemList], *, column: Column = USER_COMPAT_COLUMN
+def iter_item_lists(
+    data: pd.DataFrame | dict[EntityId, ItemList],
+    *,
+    column: Column = USER_COMPAT_COLUMN,
+    required_cols=[ITEM_COMPAT_COLUMN],
 ) -> Iterator[tuple[EntityId, ItemList]]: ...
-def iter_groups(
-    data: pd.DataFrame | dict[EntityId, ItemList], *, column: Column = USER_COMPAT_COLUMN
+def iter_item_lists(
+    data: pd.DataFrame | dict[EntityId, ItemList],
+    *,
+    column: Column = USER_COMPAT_COLUMN,
+    required_cols=[ITEM_COMPAT_COLUMN],
 ) -> Iterator[tuple[EntityId, ItemList]]:
+    """
+    Iterate over item lists identified by keys.  When the input is a data frame,
+    the column names may be specified; the default options group by ``user_id``
+    and require an ``item_id`` column, allowing the compatibility aliases
+    ``user`` and ``item``.
+    """
     if isinstance(data, pd.DataFrame):
-        for key, df in group_df(data, column=column):
+        for key, df in group_df(data, column=column, require_cols=required_cols):
             yield cast(EntityId, key), ItemList.from_df(df)
 
     elif isinstance(data, dict):
--- lenskit/tests/data/test_bulk.py
+++ lenskit/tests/data/test_bulk.py
@@ -11,7 +11,7 @@ Test the data type utilities in splits.
 import numpy as np
 import pandas as pd
 
-from lenskit.data.bulk import dict_from_df
+from lenskit.data.bulk import dict_from_df, iter_item_lists
 
 
 def test_dict_from_df(rng, ml_ratings: pd.DataFrame):
@@ -28,3 +28,15 @@ def test_dict_from_df(rng, ml_ratings: pd.DataFrame):
 
     tot = sum(len(il) for il in users.values())
     assert tot == len(ml_ratings)
+
+
+def test_iter_df(ml_ratings: pd.DataFrame):
+    counts = {}
+    for key, il in iter_item_lists(ml_ratings):
+        counts[key] = len(il)
+
+    counts = pd.Series(counts)
+    in_counts = ml_ratings.value_counts("user")
+    assert len(counts) == len(in_counts)
+    counts, in_counts = counts.align(in_counts, join="outer")
+    assert np.all(counts == in_counts)
