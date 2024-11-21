a7574310f2e05c8396244da9ecff929cf9bd25f1
Michael Ekstrand <mdekstrand@drexel.edu>
2024-11-20 23:27:25 -0500

    move implicit to pipeline

--- lenskit-implicit/tests/test_implicit.py
+++ lenskit-implicit/tests/test_implicit.py
@@ -8,20 +8,16 @@ import logging
 import pickle
 
 import numpy as np
-import pandas as pd
 
-from pytest import mark
+from pytest import approx, mark
 
 import lenskit.util.test as lktu
 from lenskit import util
-from lenskit.data import from_interactions_df
+from lenskit.data import ItemList, from_interactions_df
 from lenskit.implicit import ALS, BPR
+from lenskit.metrics import quick_measure_model
 
 _log = logging.getLogger(__name__)
-
-simple_df = pd.DataFrame(
-    {"item": [1, 1, 2, 3], "user": [10, 12, 10, 13], "rating": [4.0, 3.0, 5.0, 2.0]}
-)
 
 
 @mark.slow
@@ -29,56 +25,33 @@ def test_implicit_als_train_rec(ml_ds):
     algo = ALS(25)
     assert algo.factors == 25
 
-    ret = algo.fit(ml_ds)
+    ret = algo.train(ml_ds)
     assert ret is algo
 
-    recs = algo.recommend(100, n=20)
-    assert len(recs) == 20
-
-    preds = algo.predict_for_user(100, items=[1, 377])
-    assert np.all(preds.notnull())
+    preds = algo(100, items=ItemList([1, 377]))
+    preds = preds.scores("pandas", index="ids")
+    assert preds is not None
+    assert np.all(preds.notna())
 
     _log.info("serializing implicit model")
     mod = pickle.dumps(algo)
     _log.info("serialized to %d bytes")
     a2 = pickle.loads(mod)
 
-    r2 = a2.recommend(100, n=20)
-    assert len(r2) == 20
-    assert all(r2 == recs)
+    p2 = a2(100, items=ItemList([1, 377])).scores("pandas", index="ids")
+    assert np.all(p2 == preds)
 
 
 @mark.slow
 @mark.eval
 @mark.parametrize("n_jobs", [1, None])
 def test_implicit_als_batch_accuracy(ml_100k, n_jobs):
-    import lenskit.crossfold as xf
-    from lenskit import batch, topn
+    ds = from_interactions_df(ml_100k)
+    results = quick_measure_model(ALS(25), ds, n_jobs=n_jobs)
 
-    algo_t = ALS(25)
-
-    def eval(train, test):
-        _log.info("running training")
-        train["rating"] = train.rating.astype(np.float32)
-        algo = util.clone(algo_t)
-        algo.fit(from_interactions_df(train))
-        users = test.user.unique()
-        _log.info("testing %d users", len(users))
-        recs = batch.recommend(algo, users, 100, n_jobs=n_jobs)
-        return recs
-
-    folds = list(xf.partition_users(ml_100k, 5, xf.SampleFrac(0.2)))
-    test = pd.concat(f.test for f in folds)
-
-    recs = pd.concat(eval(train, test) for (train, test) in folds)
-
-    _log.info("analyzing recommendations")
-    rla = topn.RecListAnalysis()
-    rla.add_metric(topn.ndcg)
-    results = rla.compute(recs, test)
-    dcg = results.ndcg
-    _log.info("nDCG for %d users is %.4f", len(dcg), dcg.mean())
-    assert dcg.mean() > 0
+    ndcg = results.list_summary().loc["NDCG", "mean"]
+    _log.info("nDCG for %d users is %.4f", len(results.list_metrics()), ndcg)
+    assert ndcg > 0
 
 
 @mark.slow
@@ -86,12 +59,11 @@ def test_implicit_bpr_train_rec(ml_ds):
     algo = BPR(25, use_gpu=False)
     assert algo.factors == 25
 
-    algo.fit(ml_ds)
+    algo.train(ml_ds)
 
-    recs = algo.recommend(100, n=20)
-    assert len(recs) == 20
-
-    preds = algo.predict_for_user(100, [20, 30, 23148010])
+    preds = algo(100, ItemList([20, 30, 23148010]))
+    preds = preds.scores("pandas", index="ids")
+    assert preds is not None
     assert all(preds.index == [20, 30, 23148010])
     assert all(preds.isna() == [False, False, True])
 
@@ -100,9 +72,20 @@ def test_implicit_bpr_train_rec(ml_ds):
     _log.info("serialized to %d bytes")
     a2 = pickle.loads(mod)
 
-    r2 = a2.recommend(100, n=20)
-    assert len(r2) == 20
-    assert all(r2 == recs)
+    p2 = a2(100, items=ItemList([20, 30, 23148010])).scores("pandas", index="ids")
+    assert p2.values == approx(preds.values, nan_ok=True)
+
+
+@mark.slow
+@mark.eval
+@mark.parametrize("n_jobs", [1, None])
+def test_implicit_bpr_batch_accuracy(ml_100k, n_jobs):
+    ds = from_interactions_df(ml_100k)
+    results = quick_measure_model(BPR(25), ds, n_jobs=n_jobs)
+
+    ndcg = results.list_summary().loc["NDCG", "mean"]
+    _log.info("nDCG for %d users is %.4f", len(results.list_metrics()), ndcg)
+    assert ndcg > 0
 
 
 def test_implicit_pickle_untrained(tmp_path):
