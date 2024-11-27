aeef09f33e3d43cc0c893a4392d0a2d50b10064f
Michael Ekstrand <mdekstrand@drexel.edu>
2024-11-26 19:21:21 -0500

    remove legacy predict/recommend

--- lenskit/lenskit/batch/_predict.py
+++ lenskit/lenskit/batch/_predict.py
@@ -6,14 +6,9 @@
 from __future__ import annotations
 
 import logging
-import warnings
 from typing import Mapping
 
-import pandas as pd
-
-from lenskit import util
 from lenskit.data import ID, GenericKey, ItemList, ItemListCollection
-from lenskit.parallel import invoke_progress, invoker
 from lenskit.pipeline import Pipeline
 
 from ._runner import BatchPipelineRunner
@@ -32,76 +27,8 @@ def predict(
     Convenience function to batch-generate rating predictions (or other per-item
     scores) from a pipeline.
     """
-    if isinstance(pipeline, Algorithm):
-        return legacy_predict(pipeline, test, n_jobs=n_jobs, **kwargs)  # type: ignore
 
     runner = BatchPipelineRunner(n_jobs=n_jobs)
     runner.predict()
     outs = runner.run(pipeline, test)
     return outs.output("predictions")  # type: ignore
-
-
-def _predict_user(model, req):
-    user, udf = req
-    watch = util.Stopwatch()
-    res = model.predict_for_user(user, udf["item"])
-    res = pd.DataFrame({"user": user, "item": res.index, "prediction": res.values})
-    _logger.debug(
-        "%s produced %d/%d predictions for %s in %s",
-        model,
-        res.prediction.notna().sum(),
-        len(udf),
-        user,
-        watch,
-    )
-    return res
-
-
-def legacy_predict(algo, pairs, *, n_jobs=None, **kwargs):
-    """
-    Generate predictions for user-item pairs.  The provided algorithm should be a
-    :py:class:`algorithms.Predictor` or a function of two arguments: the user ID and
-    a list of item IDs. It should return a dictionary or a :py:class:`pandas.Series`
-    mapping item IDs to predictions.
-
-    Args:
-        algo(lenskit.algorithms.Predictor):
-            A rating predictor function or algorithm.
-        pairs(pandas.DataFrame):
-            A data frame of (``user``, ``item``) pairs to predict for. If this frame also
-            contains a ``rating`` column, it will be included in the result.
-        n_jobs(int):
-            The number of processes to use for parallel batch prediction.  Passed to
-            :func:`lenskit.util.parallel.invoker`.
-
-    Returns:
-        pandas.DataFrame:
-            a frame with columns ``user``, ``item``, and ``prediction`` containing
-            the prediction results. If ``pairs`` contains a `rating` column, this
-            result will also contain a `rating` column.
-    """
-    if n_jobs is None and "nprocs" in kwargs:
-        n_jobs = kwargs["nprocs"]
-        warnings.warn("nprocs is deprecated, use n_jobs", DeprecationWarning)
-
-    nusers = pairs["user"].nunique()
-
-    timer = util.Stopwatch()
-    nusers = pairs["user"].nunique()
-    with (
-        invoke_progress(_logger, "predictions", nusers, unit="user") as progress,
-        invoker(algo, _predict_user, n_jobs=n_jobs, progress=progress) as worker,
-    ):
-        del algo  # maybe free some memory
-
-        _logger.info(
-            "generating %d predictions for %d users (setup took %s)", len(pairs), nusers, timer
-        )
-        timer = util.Stopwatch()
-        results = worker.map((user, udf.copy()) for (user, udf) in pairs.groupby("user"))
-        results = pd.concat(results)
-        _logger.info("generated %d predictions for %d users in %s", len(pairs), nusers, timer)
-
-    if "rating" in pairs:
-        return pairs.join(results.set_index(["user", "item"]), on=("user", "item"))
-    return results
--- lenskit/lenskit/batch/_recommend.py
+++ lenskit/lenskit/batch/_recommend.py
@@ -6,15 +6,9 @@
 from __future__ import annotations
 
 import logging
-import warnings
 from typing import Sequence
 
-import numpy as np
-import pandas as pd
-
-from lenskit import util
 from lenskit.data import ID, ItemListCollection, UserIDKey
-from lenskit.parallel import invoke_progress, invoker
 from lenskit.pipeline import Pipeline
 
 from ._runner import BatchPipelineRunner
@@ -34,95 +28,8 @@ def recommend(
     """
     Convenience function to batch-generate recommendations from a pipeline.
     """
-    if isinstance(pipeline, Algorithm):
-        return legacy_recommend(pipeline, users, n, candidates, n_jobs=n_jobs, **kwargs)  # type: ignore
 
     runner = BatchPipelineRunner(n_jobs=n_jobs)
     runner.recommend(n=n)
     outs = runner.run(pipeline, users)
     return outs.output("recommendations")
-
-
-def _recommend_user(algo, req):
-    user, n, candidates = req
-
-    _logger.debug("generating recommendations for %s", user)
-    watch = util.Stopwatch()
-    res = algo.recommend(user, n, candidates)
-    _logger.debug("%s recommended %d/%s items for %s in %s", str(algo), len(res), n, user, watch)
-
-    res["user"] = user
-    res["rank"] = np.arange(1, len(res) + 1)
-
-    return res.reset_index(drop=True)
-
-
-def __standard_cand_fun(candidates):
-    """
-    Convert candidates from the forms accepted by :py:fun:`recommend` into
-    a standard form, a function that takes a user and returns a candidate
-    list.
-    """
-    if isinstance(candidates, dict):
-        return candidates.get
-    elif candidates is None:
-        return lambda u: None
-    else:
-        return candidates
-
-
-def legacy_recommend(algo, users, n, candidates=None, *, n_jobs=None, **kwargs):
-    """
-    Batch-recommend for multiple users.  The provided algorithm should be a
-    :py:class:`algorithms.Recommender`.
-
-    Args:
-        algo: the algorithm
-        users(array-like): the users to recommend for
-        n(int): the number of recommendations to generate (None for unlimited)
-        candidates:
-            the users' candidate sets. This can be a function, in which case it will
-            be passed each user ID; it can also be a dictionary, in which case user
-            IDs will be looked up in it.  Pass ``None`` to use the recommender's
-            built-in candidate selector (usually recommended).
-        n_jobs(int):
-            The number of processes to use for parallel recommendations.  Passed to
-            :func:`lenskit.util.parallel.invoker`.
-
-    Returns:
-        A frame with at least the columns ``user``, ``rank``, and ``item``; possibly also
-        ``score``, and any other columns returned by the recommender.
-    """
-
-    if n_jobs is None and "nprocs" in kwargs:
-        n_jobs = kwargs["nprocs"]
-        warnings.warn("nprocs is deprecated, use n_jobs", DeprecationWarning)
-
-    rec_algo = Recommender.adapt(algo)
-    if candidates is None and rec_algo is not algo:
-        warnings.warn("no candidates provided and algo is not a recommender, unlikely to work")
-    algo = rec_algo
-    del rec_algo
-
-    if "ratings" in kwargs:
-        warnings.warn("Providing ratings to recommend is not supported", DeprecationWarning)
-
-    candidates = __standard_cand_fun(candidates)
-
-    _logger.info("recommending with %s for %d users (n_jobs=%s)", str(algo), len(users), n_jobs)
-    with (
-        invoke_progress(_logger, "recommending", len(users), unit="user") as progress,
-        invoker(algo, _recommend_user, n_jobs=n_jobs, progress=progress) as worker,
-    ):
-        del algo
-        timer = util.Stopwatch()
-        results = worker.map((user, n, candidates(user)) for user in users)
-        results = pd.concat(results, ignore_index=True, copy=False)
-        timer.stop()
-        time = timer.elapsed()
-        rate = time / len(users)
-        _logger.info(
-            "recommended for %d users in %s (%.1fms per user)", len(users), timer, rate * 1000
-        )
-
-    return results
