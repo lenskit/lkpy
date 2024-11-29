123cbf393e2b0c2e7e7df5d60029dd62a8d89d62
Michael Ekstrand <mdekstrand@drexel.edu>
2024-11-29 18:56:51 -0500

    test randomness

--- lenskit/tests/basic/test_random.py
+++ lenskit/tests/basic/test_random.py
@@ -6,6 +6,7 @@
 
 import logging
 import pickle
+from itertools import groupby
 
 import numpy as np
 import pandas as pd
@@ -13,24 +14,13 @@ from numpy.typing import NDArray
 
 import hypothesis.extra.numpy as nph
 import hypothesis.strategies as st
-from hypothesis import given, settings
+from hypothesis import assume, given
 
-import lenskit.util.test as lktu
-from lenskit.basic import PopScorer
 from lenskit.basic.random import RandomSelector
-from lenskit.basic.topn import TopNRanker
 from lenskit.data.items import ItemList
-from lenskit.util.test import ml_ds, ml_ratings  # noqa: F401
+from lenskit.util.test import scored_lists
 
 _log = logging.getLogger(__name__)
-
-
-@st.composite
-def scored_lists(draw: st.DrawFn) -> ItemList:
-    n = draw(st.integers(0, 1000))
-    ids = np.arange(1, n + 1, dtype=np.int32)
-    scores = draw(nph.arrays(nph.floating_dtypes(endianness="=", sizes=[32, 64]), n))
-    return ItemList(item_ids=ids, scores=scores)
 
 
 @given(scored_lists())
@@ -62,3 +52,31 @@ def test_runtime_truncation(n, items: ItemList):
     assert len(ranked) == min(n, len(items))
     # all items are unique
     assert len(set(ranked.ids())) == len(ranked)
+
+
+@given(
+    st.integers(min_value=5, max_value=100),
+    st.lists(st.emails(), min_size=5, unique=True),
+    scored_lists(n=(5, 500)),
+)
+def test_random_seeding(n: int, users: list[str], items: ItemList):
+    assume(n < len(items))
+    rsel = RandomSelector(rng=(42, "user"))
+
+    distinct = set()
+
+    for user in users:
+        ranked = rsel(items=items, n=n, query=user)
+
+        assert len(ranked) == n
+        # all items are unique
+        assert len(set(ranked.ids())) == len(ranked)
+
+        # ranking again gets the same list
+        r2 = rsel(items=items, n=n, query=user)
+        assert np.all(r2.ids() == ranked.ids())
+
+        distinct |= set(ranked.ids())
+
+    # we should have more than n distinct values (randomness works)
+    assert len(distinct) > n
