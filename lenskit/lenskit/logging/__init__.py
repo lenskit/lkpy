9bc64353b8ff6587e86dab80e2a48bc749a58f59
Michael Ekstrand <mdekstrand@drexel.edu>
2024-11-30 15:43:08 -0500

    export item progress

--- lenskit/lenskit/logging/__init__.py
+++ lenskit/lenskit/logging/__init__.py
@@ -3,5 +3,10 @@ Logging, progress, and resource records.
 """
 
 from .config import LoggingConfig
+from .progress import Progress, item_progress
 
-__all__ = ["LoggingConfig"]
+__all__ = [
+    "LoggingConfig",
+    "Progress",
+    "item_progress",
+]
