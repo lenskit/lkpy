2452b28d7e420150aa86cdd7fc3e3df0d920751e
Michael Ekstrand <mdekstrand@drexel.edu>
2024-11-29 19:04:34 -0500

    add seeds for derivable RNGs

--- lenskit/tests/utils/test_random.py
+++ lenskit/tests/utils/test_random.py
@@ -1,9 +1,10 @@
-from numpy.random import SeedSequence
+from numpy.random import Generator, SeedSequence
 
-from hypothesis import given
+from hypothesis import assume, given
 from hypothesis import strategies as st
 
-from lenskit.util.random import make_seed
+from lenskit.data.query import RecQuery
+from lenskit.util.random import derivable_rng, make_seed
 
 
 @given(
@@ -21,3 +22,31 @@ def test_make_seed(seed):
     state = ss.generate_state(1)
     assert len(state) == 1
     assert state[0]
+
+
+@given(st.integers(min_value=0), st.emails(), st.emails())
+def test_derivable_fixed(seed: int, uid1: str, uid2: str):
+    assume(uid1 != uid2)
+
+    drng = derivable_rng(seed)
+
+    v1a = drng(RecQuery.create(uid1)).integers(1_000_000)
+    v1b = drng(RecQuery.create(uid1)).integers(1_000_000)
+    v2 = drng(RecQuery.create(uid2)).integers(1_000_000)
+
+    assert v1a != v2
+    assert v1a != v1b
+
+
+@given(st.integers(min_value=0), st.emails(), st.emails())
+def test_derivable_user(seed: int, uid1: str, uid2: str):
+    assume(uid1 != uid2)
+
+    drng = derivable_rng((seed, "user"))
+
+    v1a = drng(RecQuery.create(uid1)).integers(1_000_000)
+    v1b = drng(RecQuery.create(uid1)).integers(1_000_000)
+    v2 = drng(RecQuery.create(uid2)).integers(1_000_000)
+
+    assert v1a != v2
+    assert v1a == v1b
