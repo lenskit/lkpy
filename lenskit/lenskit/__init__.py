a9bf30de5585593f87a38223c20f880b09371364
Michael Ekstrand <mdekstrand@drexel.edu>
2024-11-30 10:50:26 -0500

    add version import

--- lenskit/lenskit/__init__.py
+++ lenskit/lenskit/__init__.py
@@ -1,5 +1,10 @@
 """
-Recommender toolkit.
+Recommender systems toolkit.
 """
 
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "UNSPECIFIED"
