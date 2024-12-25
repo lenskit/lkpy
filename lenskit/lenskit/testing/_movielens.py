cc1527128690fe4f1ad0aa5dbc97a23bac6325ba
Michael Ekstrand <md@ekstrandom.net>
2024-12-25 16:17:17 -0600

    cache fixture but retain check for modification

--- lenskit/lenskit/testing/_movielens.py
+++ lenskit/lenskit/testing/_movielens.py
@@ -25,9 +25,10 @@ ml_test_dir = here("data/ml-latest-small")
 ml_100k_zip = here("data/ml-100k.zip")
 
 ml_test: Dataset = LazyDataset(lambda: load_movielens(ml_test_dir))
+_ml_ds_cache: Dataset | None = None
 
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
 def ml_ratings() -> Generator[pd.DataFrame, None, None]:
     """
     Fixture to load the test MovieLens ratings as a data frame. To use this,
@@ -54,8 +55,12 @@ def ml_ds(ml_ratings: pd.DataFrame) -> Generator[Dataset, None, None]:
     .. note::
         This is imported in ``conftest.py`` so it is always available in LensKit tests.
     """
+    global _ml_ds_cache
     log = _log.bind()
-    ds = from_interactions_df(ml_ratings)
+    ds = _ml_ds_cache
+    if ds is None:
+        _ml_ds_cache = ds = from_interactions_df(ml_ratings)
+
     old_rates = ds.interaction_matrix("pandas", field="rating", original_ids=True).copy(deep=True)
     old_ustats = ds.user_stats().copy(deep=True)
     old_istats = ds.item_stats().copy(deep=True)
