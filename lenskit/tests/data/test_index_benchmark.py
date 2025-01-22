0effdcd7a935922599af6ea00c79b5fa1cf674ef
Michael Ekstrand <mdekstrand@drexel.edu>
2025-01-22 14:50:16 -0500

    benchmark pandas and arrow for index lookup

---
+++ lenskit/tests/data/test_index_benchmark.py
@@ -0,0 +1,114 @@
+from uuid import UUID, uuid4
+
+import numpy as np
+import pandas as pd
+import pyarrow as pa
+import pyarrow.compute as pc
+
+from pytest import mark
+
+VOCAB_SIZE = 50000
+
+
+@mark.benchmark(group="single")
+def test_pandas_integer_single(rng: np.random.Generator, benchmark):
+    ids = rng.choice(VOCAB_SIZE * 1000, VOCAB_SIZE, replace=False)
+    index = pd.Index(ids)
+
+    query = rng.choice(ids, 1).item()
+
+    def search():
+        _val = index.get_loc(query)
+
+    benchmark(search)
+
+
+@mark.benchmark(group="array")
+def test_pandas_integer_500(rng: np.random.Generator, benchmark):
+    ids = rng.choice(VOCAB_SIZE * 1000, VOCAB_SIZE, replace=False)
+    index = pd.Index(ids)
+
+    query = rng.choice(ids, 500, replace=False)
+
+    def search():
+        _val = index.get_indexer_for(query)
+
+    benchmark(search)
+
+
+@mark.benchmark(group="single")
+def test_pandas_string_single(rng: np.random.Generator, benchmark):
+    ids = [str(uuid4()) for i in range(VOCAB_SIZE)]
+    index = pd.Index(ids)
+
+    query = rng.choice(ids, 1).item()
+
+    def search():
+        _val = index.get_loc(query)
+
+    benchmark(search)
+
+
+@mark.benchmark(group="array")
+def test_pandas_string_500(rng: np.random.Generator, benchmark):
+    ids = [str(uuid4()) for i in range(VOCAB_SIZE)]
+    index = pd.Index(ids)
+
+    query = rng.choice(ids, 500, replace=False)
+
+    def search():
+        _val = index.get_indexer_for(query)
+
+    benchmark(search)
+
+
+@mark.benchmark(group="single")
+def test_arrow_integer_single(rng: np.random.Generator, benchmark):
+    ids = rng.choice(VOCAB_SIZE * 1000, VOCAB_SIZE, replace=False)
+    index = pa.array(ids)
+
+    query = rng.choice(ids, 1).item()
+
+    def search():
+        _val = index.index(query)
+
+    benchmark(search)
+
+
+@mark.benchmark(group="array")
+def test_arrow_integer_500(rng: np.random.Generator, benchmark):
+    ids = rng.choice(VOCAB_SIZE * 1000, VOCAB_SIZE, replace=False)
+    index = pa.array(ids)
+
+    query = rng.choice(ids, 500)
+
+    def search():
+        _val = pc.index_in(query, index)
+
+    benchmark(search)
+
+
+@mark.benchmark(group="single")
+def test_arrow_string_single(rng: np.random.Generator, benchmark):
+    ids = [str(uuid4()) for i in range(VOCAB_SIZE)]
+    index = pa.array(ids)
+
+    query = rng.choice(ids, 1).item()
+
+    def search():
+        _val = index.index(query)
+
+    benchmark(search)
+
+
+@mark.benchmark(group="array")
+def test_arrow_string_500(rng: np.random.Generator, benchmark):
+    ids = [str(uuid4()) for i in range(VOCAB_SIZE)]
+    index = pa.array(ids)
+
+    query = rng.choice(ids, 500)
+
+    def search():
+        _val = pc.index_in(query, index)
+
+    benchmark(search)
