0002a964d72ee2ed7b932d92b522ca077927dfc6
Michael Ekstrand <mdekstrand@drexel.edu>
2025-01-28 11:57:13 -0500

    add DF tests + refactor loading into classes

--- lenskit/lenskit/data/movielens.py
+++ lenskit/lenskit/data/movielens.py
@@ -8,9 +8,11 @@
 Code to import MovieLens data sets into LensKit.
 """
 
+from __future__ import annotations
+
 import re
+from collections.abc import Callable
 from dataclasses import dataclass
-from enum import Enum
 from pathlib import Path
 from typing import TypeAlias
 from zipfile import ZipFile
@@ -28,18 +30,6 @@ _log = get_logger(__name__)
 LOC: TypeAlias = Path | tuple[ZipFile, str]
 
 
-class MLVersion(Enum):
-    ML_100K = "ml-100k"
-    ML_1M = "ml-1m"
-    ML_10M = "ml-10m"
-    ML_20M = "ml-20m"
-    ML_25M = "ml-25m"
-    ML_32M = "ml-32m"
-    ML_LATEST_SMALL = "ml-latest-small"
-    ML_LATEST = "ml-latest"
-    ML_MODERN = "ml-modern"
-
-
 @dataclass
 class MLData:
     """
@@ -48,9 +38,20 @@ class MLData:
     .. stability:: internal
     """
 
-    version: MLVersion
+    version: str
     source: Path | ZipFile
     prefix: str = ""
+
+    @staticmethod
+    def version_impl(version: str) -> Callable[..., MLData]:
+        if version == "ml-100k":
+            return ML100KLoader
+        elif re.match(r"^ml-10?m(100k)?$", version, re.IGNORECASE):
+            return MLMLoader
+        elif re.match(r"^ml-(\d+m|latest(-small)?)$", version, re.IGNORECASE):
+            return MLModernLoader
+        else:
+            raise ValueError(f"unknown ML version {version}")
 
     def __enter__(self):
         return self
@@ -64,6 +65,73 @@ class MLData:
             return open(self.source / (self.prefix + name), "r")
         else:
             return self.source.open(self.prefix + name)
+
+    def ratings_df(self) -> pd.DataFrame:
+        """
+        Load the ratings data frame.
+        """
+        raise NotImplementedError()
+
+
+class ML100KLoader(MLData):
+    """
+    Loader for the ML100K data set.
+    """
+
+    def ratings_df(self) -> pd.DataFrame:
+        with self.open_file("u.data") as data:
+            return pd.read_csv(
+                data,
+                sep="\t",
+                header=None,
+                names=["user_id", "item_id", "rating", "timestamp"],
+                dtype={
+                    "user_id": np.int32,
+                    "item_id": np.int32,
+                    "rating": np.float32,
+                    "timestamp": np.int32,
+                },
+            )
+
+
+class MLMLoader(MLData):
+    """
+    Loader for the ML 1M and 10M data sets.
+    """
+
+    def ratings_df(self):
+        with self.open_file("ratings.dat") as data:
+            return pd.read_csv(
+                data,
+                sep=":",
+                header=None,
+                names=["user_id", "_ui", "item_id", "_ir", "rating", "_rt", "timestamp"],
+                usecols=[0, 2, 4, 6],
+                dtype={
+                    "user_id": np.int32,
+                    "item_id": np.int32,
+                    "rating": np.float32,
+                    "timestamp": np.int32,
+                },
+            )
+
+
+class MLModernLoader(MLData):
+    """
+    Loader for modern MovieLens data sets (20M and later).
+    """
+
+    def ratings_df(self):
+        with self.open_file("ratings.csv") as data:
+            return pd.read_csv(
+                data,
+                dtype={
+                    "userId": np.int32,
+                    "movieId": np.int32,
+                    "rating": np.float32,
+                    "timestamp": np.int64,
+                },
+            ).rename(columns={"userId": "user_id", "movieId": "item_id"})
 
 
 def load_movielens(path: str | Path) -> Dataset:
@@ -104,18 +172,13 @@ def load_movielens_df(path: str | Path) -> pd.DataFrame:
         ``timestamp``.
     """
     with _ml_detect_and_open(path) as ml:
-        match ml.version:
-            case MLVersion.ML_100K:
-                return _load_ml_100k(ml)
-            case MLVersion.ML_1M | MLVersion.ML_10M:
-                return _load_ml_million(ml)
-            case _:
-                return _load_ml_modern(ml)
+        return ml.ratings_df()
 
 
 def _ml_detect_and_open(path: str | Path) -> MLData:
     loc = Path(path)
-    ds: MLVersion
+    version: str
+    ctor: Callable[..., MLData]
 
     if loc.is_file() and loc.suffix == ".zip":
         log = _log.bind(zipfile=str(loc))
@@ -134,9 +197,10 @@ def _ml_detect_and_open(path: str | Path) -> MLData:
                 log.error("invalid directory name %s", first.filename)
                 raise RuntimeError("invalid ML zip file")
 
-            ds = MLVersion(dsm.group(1).lower())
-            log.debug("found ML data set %s", ds)
-            return MLData(ds, zf, first.filename)
+            version = dsm.group(1).lower()
+            log.debug("found ML data set %s", version)
+            ctor = MLData.version_impl(version)
+            return ctor(version, zf, first.filename)
         except Exception as e:  # pragma nocover
             zf.close()
             raise e
@@ -145,71 +209,28 @@ def _ml_detect_and_open(path: str | Path) -> MLData:
         log.debug("loading from directory")
         dsm = re.match(r"^(ml-\d+[MmKk])", loc.name)
         if dsm:
-            ds = MLVersion(dsm.group(1))
-            _log.debug("inferred data set %s from dir name", ds)
+            version = dsm.group(1)
+            ctor = MLData.version_impl(dsm.group(1))
+            _log.debug("inferred data set %s from dir name", version)
         else:
             _log.debug("checking contents for data type")
             if (loc / "u.data").exists():
                 _log.debug("found u.data, interpreting as 100K")
-                ds = MLVersion.ML_100K
+                ctor = ML100KLoader
             elif (loc / "ratings.dat").exists():
                 if (loc / "tags.dat").exists():
                     _log.debug("found ratings.dat and tags.dat, interpreting as 10M")
-                    ds = MLVersion.ML_10M
+                    version = "ml-10m"
                 else:
                     _log.debug("found ratings.dat but no tags, interpreting as 1M")
-                    ds = MLVersion.ML_1M
+                    version = "ml-1m"
+                ctor = MLMLoader
             elif (loc / "ratings.csv").exists():
                 _log.debug("found ratings.csv, interpreting as modern (20M and later)")
-                ds = MLVersion.ML_MODERN
+                version = "ml-modern"
+                ctor = MLModernLoader
             else:
                 _log.error("could not detect MovieLens data")
                 raise RuntimeError("invalid ML directory")
 
-        return MLData(ds, loc)
-
-
-def _load_ml_100k(ml: MLData) -> pd.DataFrame:
-    with ml.open_file("u.data") as data:
-        return pd.read_csv(
-            data,
-            sep="\t",
-            header=None,
-            names=["user_id", "item_id", "rating", "timestamp"],
-            dtype={
-                "user_id": np.int32,
-                "item_id": np.int32,
-                "rating": np.float32,
-                "timestamp": np.int32,
-            },
-        )
-
-
-def _load_ml_million(ml: MLData) -> pd.DataFrame:
-    with ml.open_file("ratings.dat") as data:
-        return pd.read_csv(
-            data,
-            sep=":",
-            header=None,
-            names=["user_id", "_ui", "item_id", "_ir", "rating", "_rt", "timestamp"],
-            usecols=[0, 2, 4, 6],
-            dtype={
-                "user_id": np.int32,
-                "item_id": np.int32,
-                "rating": np.float32,
-                "timestamp": np.int32,
-            },
-        )
-
-
-def _load_ml_modern(ml: MLData) -> pd.DataFrame:
-    with ml.open_file("ratings.csv") as data:
-        return pd.read_csv(
-            data,
-            dtype={
-                "userId": np.int32,
-                "movieId": np.int32,
-                "rating": np.float32,
-                "timestamp": np.int64,
-            },
-        ).rename(columns={"userId": "user_id", "movieId": "item_id"})
+        return ctor(version, loc)
--- lenskit/tests/movielens/test_load_movielens.py
+++ lenskit/tests/movielens/test_load_movielens.py
@@ -9,7 +9,7 @@ from pathlib import Path
 
 from pytest import mark
 
-from lenskit.data.movielens import load_movielens
+from lenskit.data.movielens import load_movielens, load_movielens_df
 
 ML_LATEST_DIR = Path("data/ml-latest-small")
 
@@ -38,6 +38,15 @@ def test_100k_zip():
     assert ds.interaction_count >= 100_000
 
 
+@mark.skipif(not ML_100K_ZIP.exists(), reason="ml-100k does not exist")
+def test_100k_df():
+    ds = load_movielens_df(ML_100K_ZIP)
+    assert ds["item_id"].nunique() >= 100
+    assert ds["user_id"].nunique() >= 100
+    assert ds["user_id"].nunique() < 1000
+    assert len(ds) >= 100_000
+
+
 @mark.skipif(not ML_1M_ZIP.exists(), reason="ml-1m does not exist")
 def test_1m_zip():
     ds = load_movielens(ML_1M_ZIP)
@@ -46,33 +55,73 @@ def test_1m_zip():
     assert ds.interaction_count >= 1_000_000
 
 
+@mark.skipif(not ML_1M_ZIP.exists(), reason="ml-1m does not exist")
+def test_1m_df():
+    ds = load_movielens_df(ML_1M_ZIP)
+    assert ds["item_id"].nunique() >= 500
+    assert ds["user_id"].nunique() >= 500
+    assert len(ds) >= 1_000_000
+
+
 @mark.skipif(not ML_10M_ZIP.exists(), reason="ml-10m does not exist")
 def test_10m_zip():
     ds = load_movielens(ML_10M_ZIP)
-    assert ds.item_count >= 100
-    assert ds.user_count >= 100
+    assert ds.item_count >= 10_000
+    assert ds.user_count >= 69_000
     assert ds.interaction_count >= 10_000_000
+
+
+@mark.skipif(not ML_10M_ZIP.exists(), reason="ml-10m does not exist")
+def test_10m_df():
+    ds = load_movielens_df(ML_10M_ZIP)
+    assert ds["item_id"].nunique() >= 10_000
+    assert ds["user_id"].nunique() >= 69_000
+    assert len(ds) >= 10_000_000
 
 
 @mark.skipif(not ML_20M_ZIP.exists(), reason="ml-20m does not exist")
 def test_20m_zip():
     ds = load_movielens(ML_20M_ZIP)
-    assert ds.item_count >= 100
-    assert ds.user_count >= 100
+    assert ds.item_count >= 25_000
+    assert ds.user_count >= 130_000
     assert ds.interaction_count >= 20_000_000
 
 
-@mark.skipif(not ML_25M_ZIP.exists(), reason="ml-20m does not exist")
+@mark.skipif(not ML_20M_ZIP.exists(), reason="ml-20m does not exist")
+def test_20m_df():
+    ds = load_movielens_df(ML_20M_ZIP)
+    assert ds["item_id"].nunique() >= 25_000
+    assert ds["user_id"].nunique() >= 130_000
+    assert len(ds) >= 20_000_000
+
+
+@mark.skipif(not ML_25M_ZIP.exists(), reason="ml-25m does not exist")
 def test_25m_zip():
     ds = load_movielens(ML_25M_ZIP)
-    assert ds.item_count >= 100
-    assert ds.user_count >= 100
-    assert ds.interaction_count >= 20_000_000
+    assert ds.item_count >= 50_000
+    assert ds.user_count >= 160_000
+    assert ds.interaction_count >= 25_000_000
 
 
-@mark.skipif(not ML_32M_ZIP.exists(), reason="ml-20m does not exist")
+@mark.skipif(not ML_25M_ZIP.exists(), reason="ml-25m does not exist")
+def test_25m_df():
+    ds = load_movielens_df(ML_25M_ZIP)
+    assert ds["item_id"].nunique() >= 50_000
+    assert ds["user_id"].nunique() >= 160_000
+    assert len(ds) >= 25_000_000
+
+
+@mark.skipif(not ML_32M_ZIP.exists(), reason="ml-32m does not exist")
 def test_32m_zip():
     ds = load_movielens(ML_32M_ZIP)
-    assert ds.item_count >= 100
-    assert ds.user_count >= 100
-    assert ds.interaction_count >= 20_000_000
+    assert ds.item_count >= 50_000
+    assert ds.user_count >= 200_000
+    assert ds.interaction_count >= 32_000_000
+
+
+@mark.skidf(not ML_32M_ZIP.exists(), reason="ml-32m does not exist")
+def test_32m_zip_df():
+    ds = load_movielens_df(ML_32M_ZIP)
+    assert ds["item_id"].nunique() >= 50_000
+    assert ds["user_id"].nunique() >= 200_000
+    assert len(ds) >= 32_000_000
