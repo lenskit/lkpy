f1f63448bb724943cde0bfe118345e7307a72b74
Michael Ekstrand <mdekstrand@drexel.edu>
2025-01-28 10:54:32 -0500

    add docstring for sklearn

---
+++ lenskit-sklearn/lenskit/sklearn/__init__.py
@@ -0,0 +1,11 @@
+"""
+Recommendation models using SciKit-Learn.
+
+The ``lenskit-sklearn`` package provides some recommendation models using
+SciKit-Learn's machine learning model implementations.
+
+.. note::
+
+    This package is not included in the base ``lenskit`` distribution.
+    Install ``lenskit-sklearn`` from PyPI or Conda to get these models.
+"""
