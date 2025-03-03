360cf761647f0987c8ee86db44b36778097c18b8
Michael Ekstrand <mdekstrand@drexel.edu>
2025-03-03 12:10:48 -0500

    add deprecated random export

--- src/lenskit/util.py
+++ src/lenskit/util.py
@@ -1,5 +1,9 @@
 from lenskit.logging import Stopwatch
+from lenskit.random import derivable_rng, random_generator, set_global_rng
 
 __all__ = [
     "Stopwatch",
+    "derivable_rng",
+    "random_generator",
+    "set_global_rng",
 ]
