9f8e769feb6b2ebc07b5cc3ce1c4b47da1581bb3
Michael Ekstrand <mdekstrand@drexel.edu>
2025-03-03 12:11:23 -0500

    deprecation warning on lenskit.util

--- src/lenskit/util.py
+++ src/lenskit/util.py
@@ -1,5 +1,9 @@
+import warnings
+
 from lenskit.logging import Stopwatch
 from lenskit.random import derivable_rng, random_generator, set_global_rng
+
+warnings.warn("lenskit.util is deprecated, import from original modules", DeprecationWarning)
 
 __all__ = [
     "Stopwatch",
