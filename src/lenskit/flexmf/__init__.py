b32502c9a488438ace1a823e219cc35054b81113
Michael Ekstrand <mdekstrand@drexel.edu>
2025-03-17 17:31:23 -0400

    package

---
+++ src/lenskit/flexmf/__init__.py
@@ -0,0 +1,9 @@
+"""
+Flexible PyTorch matrix factorization models for LensKit.
+
+The components in this package implement several matrix factorization models for
+LensKit, and also serve as an example for practical PyTorch recommender
+training.
+
+.. stability:: internal
+"""
