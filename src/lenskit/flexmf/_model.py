73ec985a7833bbbeb54ee2610ffe632e5c75fe9b
Michael Ekstrand <mdekstrand@drexel.edu>
2025-03-17 22:58:26 -0400

    update headers

--- src/lenskit/flexmf/_model.py
+++ src/lenskit/flexmf/_model.py
@@ -1,3 +1,9 @@
+# This file is part of LensKit.
+# Copyright (C) 2018-2023 Boise State University
+# Copyright (C) 2023-2025 Drexel University
+# Licensed under the MIT license, see LICENSE.md for details.
+# SPDX-License-Identifier: MIT
+
 import torch
 from torch import Tensor, nn
 from torch.linalg import norm, vecdot
