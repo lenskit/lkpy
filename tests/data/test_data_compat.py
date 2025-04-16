7171e8fef8656b52c92ef241df8c3a4dcac3ad26
Michael Ekstrand <mdekstrand@drexel.edu>
2025-04-16 16:57:29 -0400

    skip ML20M if not available

--- tests/data/test_data_compat.py
+++ tests/data/test_data_compat.py
@@ -39,6 +39,7 @@ def test_data_backwards_compat(version, tmpdir: Path):
 
 
 @mark.realdata
+@mark.skipif(not _ml_path.exists(), reason="ml-20m not available")
 @mark.parametrize("version", LK_VERSIONS)
 def test_data_backwards_ml20m(version, tmpdir: Path):
     "Test that we can load datasets prepared by old versions (ML20M)."
