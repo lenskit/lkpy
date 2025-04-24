cd9e2c5bf31731e6d38657bc5967618fda5a7871
Michael Ekstrand <mdekstrand@drexel.edu>
2025-04-24 17:50:54 -0400

    remove unused coordinate descent

--- src/accel/als/explicit.rs
+++ src/accel/als/explicit.rs
@@ -12,8 +12,6 @@ use log::*;
 
 use crate::{progress::ProgressHandle, sparse::CSRMatrix};
 
-const RR_EPOCHS: usize = 2;
-
 #[pyfunction]
 pub(super) fn train_explicit_matrix<'py>(
     py: Python<'py>,
@@ -51,41 +49,6 @@ pub(super) fn train_explicit_matrix<'py>(
     });
 
     Ok(frob.sqrt())
-}
-
-fn train_row_cd(
-    matrix: &CSRMatrix<i32>,
-    row_num: usize,
-    mut row_data: ArrayBase<ViewRepr<&mut f32>, Ix1>,
-    other: &ArrayView2<f32>,
-    reg: f32,
-) -> f32 {
-    let cols = matrix.row_cols(row_num);
-    let vals = matrix.row_vals(row_num);
-    let cols: Vec<_> = cols.iter().map(|c| *c as usize).collect();
-
-    let nd = row_data.len();
-
-    let o_picked = other.select(Axis(0), &cols);
-    let mut resid = Array1::from_iter(vals.iter().map(|f| *f));
-    let scores = o_picked.dot(&row_data);
-    resid -= &scores;
-
-    let mut deltas = Array1::zeros(nd);
-
-    for _e in 0..RR_EPOCHS {
-        for d in 0..nd {
-            let dvec = o_picked.column(d);
-            let num = dvec.dot(&resid) - reg * row_data[d];
-            let denom = dvec.dot(&dvec) + reg;
-            let dw = num / denom;
-            deltas[d] += dw;
-            row_data[d] += dw;
-            resid.scaled_add(-dw, &dvec);
-        }
-    }
-
-    deltas.dot(&deltas)
 }
 
 fn train_row_solve(
