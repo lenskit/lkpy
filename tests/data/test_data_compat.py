26cb045696e0d461726d5c6759674036126dbca3
Michael Ekstrand <mdekstrand@drexel.edu>
2025-04-28 15:10:53 -0400

    add headers to test files

--- tests/data/test_data_compat.py
+++ tests/data/test_data_compat.py
@@ -1,3 +1,9 @@
+# This file is part of LensKit.
+# Copyright (C) 2018-2023 Boise State University.
+# Copyright (C) 2023-2025 Drexel University.
+# Licensed under the MIT license, see LICENSE.md for details.
+# SPDX-License-Identifier: MIT
+
 import subprocess as sp
 from os import environ, fspath
 from pathlib import Path
