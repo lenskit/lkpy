2b8d08ff7fce356ec8499a726ba040a277be7096
Michael Ekstrand <mdekstrand@drexel.edu>
2025-04-28 09:55:19 -0400

    update lazy imports

--- src/lenskit/__init__.py
+++ src/lenskit/__init__.py
@@ -17,10 +17,36 @@ try:
 except PackageNotFoundError:  # pragma: nocover
     __version__ = "UNKNOWN"
 
+
+# lazy-load LensKit internal imports (per SPEC-1)
+# IMPORTANT: this must be kept in sync with __init__.pyi
 __getattr__, __dir__, __all__ = lazy.attach(
     __name__,
+    submodules=[
+        # modules
+        "batch",
+        "data",
+        "logging",
+        "metrics",
+        "pipeline",
+        "operations",
+        "splitting",
+        "stats",
+        "torch",
+        "training",
+        # component modules
+        "als",
+        "basic",
+        "flexmf",
+        "funksvd",
+        "hpf",
+        "knn",
+        "sklearn",
+    ],
     submod_attrs={
+        "data": ["Dataset", "DatasetBuilder"],
         "operations": ["predict", "recommend", "score"],
-        "pipeline": ["Pipeline", "RecPipelineBuilder", "topn_pipeline"],
+        "pipeline": ["Pipeline", "RecPipelineBuilder", "Component", "topn_pipeline"],
+        "splitting": ["TTSplit"],
     },
 )
--- src/lenskit/__init__.pyi
+++ src/lenskit/__init__.pyi
@@ -1,6 +1,61 @@
+from . import (
+    als,
+    basic,
+    batch,
+    data,
+    flexmf,
+    funksvd,
+    hpf,
+    knn,
+    logging,
+    metrics,
+    operations,
+    pipeline,
+    sklearn,
+    splitting,
+    stats,
+    torch,
+    training,
+)
+from .data import Dataset, DatasetBuilder
 from .operations import predict, recommend, score
-from .pipeline import Pipeline, RecPipelineBuilder, topn_pipeline
+from .pipeline import Component, Pipeline, RecPipelineBuilder, topn_pipeline
+from .splitting import TTSplit
 
-__all__ = ["predict", "recommend", "score", "Pipeline", "RecPipelineBuilder", "topn_pipeline"]
+__all__ = [
+    # modules
+    "batch",
+    "data",
+    "logging",
+    "metrics",
+    "pipeline",
+    "operations",
+    "splitting",
+    "stats",
+    "torch",
+    "training",
+    # component modules
+    "als",
+    "basic",
+    "flexmf",
+    "funksvd",
+    "hpf",
+    "knn",
+    "sklearn",
+    # operations
+    "predict",
+    "recommend",
+    "score",
+    # pipeline
+    "Pipeline",
+    "Component",
+    "RecPipelineBuilder",
+    "topn_pipeline",
+    # data
+    "Dataset",
+    "DatasetBuilder",
+    # splitting
+    "TTSplit",
+]
 
 __version__: str
