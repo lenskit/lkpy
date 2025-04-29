338f64e68404fb3047cdcd62d0a1c849f3f2ba5b
ALBIN BABU VARGHESE <albinbabuvarghese20@gmail.com>
2025-04-29 12:12:51 -0400

    added recs.ranks instead of computing it

--- src/lenskit/metrics/ranking/_map.py
+++ src/lenskit/metrics/ranking/_map.py
@@ -36,7 +36,7 @@ class AveragePrecision(ListMetric, RankingMetricBase):
         ap_sum = 0.0
         good = np.isin(items, test.ids())
         sum_good = np.cumsum(good)            # cumulative count of “good” up to each rank
-        ranks = np.arange(1, len(items) + 1)  # ranks starting from 1
+        ranks = recs.ranks()  # ranks starting from 1
         ap_sum = np.sum(sum_good[good] / ranks[good])
         
         denom = min(len(test.ids()), len(items))
