fix log weighting to be reiprocal

src/lenskit/metrics/ranking/_dcg.py
@@ -215,7 +215,7 @@ def array_dcg(scores: NDArray[np.number], weight: RankWeight = LogRankWeight()):
     """
     scores = np.nan_to_num(scores)
     ranks = np.arange(1, len(scores) + 1)
-    wvec = np.reciprocal(weight.weight(ranks))
+    wvec = weight.weight(ranks)
     return np.dot(scores, wvec)
 
 
@@ -226,5 +226,5 @@ def fixed_dcg(n: int, weight: RankWeight = LogRankWeight()):
     """
 
     ranks = np.arange(1, n + 1)
-    wvec = np.reciprocal(weight.weight(ranks))
+    wvec = weight.weight(ranks)
     return np.sum(wvec)

src/lenskit/metrics/ranking/_weighting.py
@@ -20,7 +20,9 @@ from lenskit.data.types import NPVector
 
 class RankWeight(ABC):
     """
-    Interface for rank weighting models.
+    Interface for rank weighting models.  This returns *multiplicative* weights,
+    so that scores should be multiplied by the weights in order to produce
+    weighted scores.
 
     Stability:
         caller
@@ -73,10 +75,10 @@ class GeometricRankWeight(RankWeight):
         self.patience = patience
 
     def weight(self, ranks) -> NPVector[np.float64]:
-        return np.power(self.patience, ranks)
+        return np.power(self.patience, ranks - 1)
 
     def log_weight(self, ranks) -> NPVector[np.float64]:
-        return self.patience * np.log(ranks)
+        return self.patience * np.log(ranks - 1)
 
     def series_sum(self) -> float:
         return 1 / (1 - self.patience)
@@ -112,6 +114,6 @@ class LogRankWeight(RankWeight):
 
     def weight(self, ranks):
         if self.offset > 0:
-            return np.log(ranks + self.offset) / np.log(self.base)
+            return np.reciprocal(np.log(ranks + self.offset) / np.log(self.base))
         else:
-            return np.log(np.maximum(ranks, 2)) / np.log(self.base)
+            return np.reciprocal(np.log(np.maximum(ranks, 2)) / np.log(self.base))