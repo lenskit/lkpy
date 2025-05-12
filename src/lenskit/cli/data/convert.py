95e9c1082f6d2f7d63dd1584779fd93dfe087326
Michael Ekstrand <mdekstrand@drexel.edu>
2025-05-12 12:44:35 -0400

    add support for converting item list collections

--- src/lenskit/cli/data/convert.py
+++ src/lenskit/cli/data/convert.py
@@ -9,6 +9,7 @@ from pathlib import Path
 import click
 
 from lenskit.data.amazon import load_amazon_ratings
+from lenskit.data.collection._list import ListILC
 from lenskit.data.movielens import load_movielens
 from lenskit.logging import get_logger
 
@@ -18,9 +19,12 @@ _log = get_logger(__name__)
 @click.command("convert")
 @click.option("--movielens", "format", flag_value="movielens", help="Convert MovieLens data.")
 @click.option("--amazon", "format", flag_value="amazon", help="Convert Amazon rating data.")
+@click.option(
+    "--item-lists", is_flag=True, help="Convert to an ItemListCollection instead of Dataset."
+)
 @click.argument("src", nargs=-1, required=True, type=Path)
 @click.argument("dst", type=Path)
-def convert(format: str | None, src: list[Path], dst: Path):
+def convert(format: str | None, src: list[Path], dst: Path, item_lists: bool = False):
     """
     Convert data into the LensKit native format.
     """
@@ -41,6 +45,17 @@ def convert(format: str | None, src: list[Path], dst: Path):
         case _:
             raise ValueError(f"unknown data format {format}")
 
-    log = _log.bind(dst=str(dst))
-    log.info("saving data in native format")
-    data.save(dst)
+    log = _log.bind(dst=str(dst), name=data.name)
+    if item_lists:
+        icls = data.default_interaction_class()
+        log.info("extracting %s data", icls)
+        rels = data.interactions(icls).matrix()
+        ilc: ListILC = ListILC.from_dict(
+            {(rels.row_vocabulary.id(i),): rels.row_items(number=i) for i in range(rels.n_rows)},  # type: ignore
+            f"{rels.row_type}_id",
+        )
+        log.info("saving to native Parquet format")
+        ilc.save_parquet(dst)
+    else:
+        log.info("saving data in native format")
+        data.save(dst)
