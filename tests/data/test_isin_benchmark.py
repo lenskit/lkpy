e5f4f09544e684f110acac8e8a92c24cfc289de2
Michael Ekstrand <mdekstrand@drexel.edu>
2025-05-14 14:23:49 -0400

    add benchmark for membership testing

---
+++ tests/data/test_isin_benchmark.py
@@ -0,0 +1,103 @@
+# This file is part of LensKit.
+# Copyright (C) 2018-2023 Boise State University.
+# Copyright (C) 2023-2025 Drexel University.
+# Licensed under the MIT license, see LICENSE.md for details.
+# SPDX-License-Identifier: MIT
+
+from uuid import uuid4
+
+import numpy as np
+import pandas as pd
+import pyarrow as pa
+import pyarrow.compute as pc
+
+from pytest import mark
+
+LEFT_SIZE = 50000
+RIGHT_SIZE = 100
+
+
+@mark.benchmark(group="integers")
+def test_numpy_integers(rng: np.random.Generator, benchmark):
+    left = np.arange(LEFT_SIZE)
+    right = rng.choice(LEFT_SIZE * 2, RIGHT_SIZE, replace=False)
+
+    def search():
+        _mask = np.isin(left, right)
+
+    benchmark(search)
+
+
+@mark.benchmark(group="strings")
+def test_numpy_strings(rng: np.random.Generator, benchmark):
+    left = np.array([str(uuid4()) for _i in range(LEFT_SIZE)])
+    right = rng.choice(left, RIGHT_SIZE, replace=False)
+
+    def search():
+        _mask = np.isin(left, right)
+
+    benchmark(search)
+
+
+@mark.benchmark(group="strings")
+def test_numpy_dstrings(rng: np.random.Generator, benchmark):
+    left = np.array([str(uuid4()) for _i in range(LEFT_SIZE)], dtype=np.dtypes.StringDType)
+    right = rng.choice(left, RIGHT_SIZE, replace=False)
+
+    def search():
+        _mask = np.isin(left, right)
+
+    benchmark(search)
+
+
+@mark.benchmark(group="integers")
+def test_arrow_integers(rng: np.random.Generator, benchmark):
+    left = np.arange(LEFT_SIZE)
+    right = rng.choice(LEFT_SIZE * 2, RIGHT_SIZE, replace=False)
+    left = pa.array(left)
+    right = pa.array(right)
+
+    def search():
+        _mask = pc.is_in(left, right)
+
+    benchmark(search)
+
+
+@mark.benchmark(group="strings")
+def test_arrow_strings(rng: np.random.Generator, benchmark):
+    left = np.array([str(uuid4()) for _i in range(LEFT_SIZE)])
+    right = rng.choice(left, RIGHT_SIZE, replace=False)
+
+    left = pa.array(left)
+    right = pa.array(right)
+
+    def search():
+        _mask = pc.is_in(left, right)
+
+    benchmark(search)
+
+
+@mark.benchmark(group="integers")
+def test_index_integers(rng: np.random.Generator, benchmark):
+    left = np.arange(LEFT_SIZE)
+    right = rng.choice(LEFT_SIZE * 2, RIGHT_SIZE, replace=False)
+
+    def search():
+        idx = pd.Index(right)
+        res = idx.get_indexer_for(left)
+        _mask = res >= 0
+
+    benchmark(search)
+
+
+@mark.benchmark(group="strings")
+def test_index_strings(rng: np.random.Generator, benchmark):
+    left = np.array([str(uuid4()) for _i in range(LEFT_SIZE)])
+    right = rng.choice(left, RIGHT_SIZE, replace=False)
+
+    def search():
+        idx = pd.Index(right)
+        res = idx.get_indexer_for(left)
+        _mask = res >= 0
+
+    benchmark(search)
