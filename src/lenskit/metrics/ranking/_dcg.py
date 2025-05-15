47b238bc614753d853f59affa71415a6213e1eba
Michael Ekstrand <mdekstrand@drexel.edu>
2025-05-15 10:05:05 -0400

    don't call IDs unnecessarily on dcg

--- src/lenskit/metrics/ranking/_dcg.py
+++ src/lenskit/metrics/ranking/_dcg.py
@@ -94,12 +94,12 @@ class NDCG(ListMetric, RankingMetricBase):
     @override
     def measure_list(self, recs: ItemList, test: ItemList) -> float:
         recs = self.truncate(recs)
-        items = recs.ids()
 
         if self.gain:
             gains = test.field(self.gain, "pandas", index="ids")
             if gains is None:
                 raise KeyError(f"test items have no field {self.gain}")
+            items = recs.ids()
             scores = gains.reindex(items, fill_value=0).values
             if self.k:
                 gains = gains.nlargest(n=self.k)
@@ -107,7 +107,7 @@ class NDCG(ListMetric, RankingMetricBase):
                 gains = gains.sort_values(ascending=False)
             ideal = array_dcg(np.require(gains.values, np.float32), self.weight)
         else:
-            scores = np.zeros_like(items, dtype=np.float32)
+            scores = np.zeros(len(recs), dtype=np.float32)
             scores[recs.isin(test)] = 1.0
             n = len(test)
             if self.k and self.k < n:
@@ -182,15 +182,15 @@ class DCG(ListMetric, RankingMetricBase):
     @override
     def measure_list(self, recs: ItemList, test: ItemList) -> float:
         recs = self.truncate(recs)
-        items = recs.ids()
 
         if self.gain:
             gains = test.field(self.gain, "pandas", index="ids")
             if gains is None:
                 raise KeyError(f"test items have no field {self.gain}")
+            items = recs.ids()
             scores = gains.reindex(items, fill_value=0).values
         else:
-            scores = np.zeros_like(items, dtype=np.float32)
+            scores = np.zeros(len(recs), dtype=np.float32)
             scores[recs.isin(test)] = 1.0
 
         return array_dcg(np.require(scores, np.float32), self.weight)
