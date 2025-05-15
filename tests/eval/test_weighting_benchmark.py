363dfc9a584c8cce9675d0345eec069fd47edb1a
Michael Ekstrand <mdekstrand@drexel.edu>
2025-05-15 18:41:54 -0400

    more weight benchmarking

--- tests/eval/test_weighting_benchmark.py
+++ tests/eval/test_weighting_benchmark.py
@@ -21,41 +21,94 @@ RIGHT_SIZE = 100
 TOTAL_SIZE = LEFT_SIZE * 2
 
 
-@mark.benchmark()
+@mark.benchmark(group="geometric")
 @mark.parametrize("size", [100, 1000, 50000])
-def test_numpy(benchmark, size: int):
+def test_numpy_geom(benchmark, size: int):
+    ranks = np.arange(1, size + 1)
+
     def search():
-        ranks = np.arange(1, size + 1)
         _w = np.power(0.85, ranks - 1)
 
     benchmark(search)
 
 
-@mark.benchmark()
+@mark.benchmark(group="geometric")
 @mark.parametrize("size", [100, 1000, 50000])
-def test_numpy_log(benchmark, size: int):
+def test_numpy_geom_log(benchmark, size: int):
+    ranks = np.arange(1, size + 1)
+
     def search():
-        ranks = np.arange(1, size + 1)
         _w = np.exp(np.log(0.85) * (ranks - 1))
 
     benchmark(search)
 
 
-@mark.benchmark()
+@mark.benchmark(group="geometric")
 @mark.parametrize("size", [100, 1000, 50000])
-def test_numexpr(benchmark, size: int):
+def test_numpy_geom_log_ip(benchmark, size: int):
+    ranks = np.arange(1, size + 1)
+
     def search():
-        ranks = np.arange(1, size + 1)
+        scores = np.require(ranks, dtype=np.float64)
+        scores -= 1
+        scores *= np.log(0.85)
+        np.exp(scores, out=scores)
+
+    benchmark(search)
+
+
+@mark.benchmark(group="geometric")
+@mark.parametrize("size", [100, 1000, 50000])
+def test_numexpr_geom(benchmark, size: int):
+    ranks = np.arange(1, size + 1)
+
+    def search():
         _w = numexpr.evaluate("patience ** (rank - 1)", {"patience": 0.85, "rank": ranks})
 
     benchmark(search)
 
 
-@mark.benchmark()
+@mark.benchmark(group="geometric")
+@mark.parametrize("size", [100, 1000, 50000])
+def test_numexpr_geom_log(benchmark, size: int):
+    ranks = np.arange(1, size + 1)
+
+    def search():
+        _w = numexpr.evaluate("exp(log(patience) * (rank - 1))", {"patience": 0.85, "rank": ranks})
+
+    benchmark(search)
+
+
+@mark.benchmark(group="logarithmic")
+@mark.parametrize("size", [100, 1000, 50000])
+def test_numpy_log_old(benchmark, size: int):
+    ranks = np.arange(1, size + 1)
+
+    def search():
+        _w = np.reciprocal(np.log(np.maximum(ranks, 2)) / np.log(2))
+
+    benchmark(search)
+
+
+@mark.benchmark(group="logarithmic")
+@mark.parametrize("size", [100, 1000, 50000])
+def test_numpy_log(benchmark, size: int):
+    ranks = np.arange(1, size + 1)
+
+    def search():
+        _w = np.log(2) / np.log(np.maximum(ranks, 2))
+
+    benchmark(search)
+
+
+@mark.benchmark(group="logarithmic")
 @mark.parametrize("size", [100, 1000, 50000])
 def test_numexpr_log(benchmark, size: int):
+    ranks = np.arange(1, size + 1)
+
     def search():
-        ranks = np.arange(1, size + 1)
-        _w = numexpr.evaluate("exp(log(patience) * (rank - 1))", {"patience": 0.85, "rank": ranks})
+        _w = numexpr.evaluate(
+            "log(base) / log(where(rank < 2, 2, rank))", {"base": 2, "rank": ranks}
+        )
 
     benchmark(search)
