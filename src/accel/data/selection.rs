6e8cbf648a36a3d61dae7ecf67e71818c5f8633f
Michael Ekstrand <mdekstrand@drexel.edu>
2025-05-19 11:12:15 -0400

    speed up negative mask with bulk add

--- src/accel/data/selection.rs
+++ src/accel/data/selection.rs
@@ -25,19 +25,30 @@ pub(super) fn negative_mask(
 
     let mut mask = BooleanBuilder::with_capacity(n);
     let mut j = 0;
+    let mut last_i = 0;
     for i in 0..n {
         let old_j = j;
         // loop, to handle duplicate indices
         while j < indices.len() && i == indices[j] as usize {
             j += 1;
         }
-        if old_j == j {
-            // no match
-            mask.append_value(true);
-        } else {
-            // matched, skip this position
+        // this is optimized for the common case: many fewer to remove
+        if old_j != j {
+            // this index matches to-remove. step carefully!
+
+            // append `true` for everything since the write position
+            if i > last_i {
+                mask.append_n(i - last_i, true);
+            }
+            // append `false` for this item
             mask.append_value(false);
+            // update the write position
+            last_i = i + 1;
         }
+    }
+    // fill in write position
+    if n > last_i {
+        mask.append_n(n - last_i, true);
     }
 
     let mask = mask.finish();
