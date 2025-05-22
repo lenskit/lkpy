4302a98c9b52661bb53b5ffa75748179e17b288f
Michael Ekstrand <mdekstrand@drexel.edu>
2025-05-22 18:25:27 -0400

    add the ability to run with changed versions

--- utils/version-tool.py
+++ utils/version-tool.py
@@ -10,20 +10,30 @@ Manipulate LensKit versions.
 
 Usage:
     version-tool.py [-v | -vv] [-q]
+    version-tool.py [-v | -vv] --run PROGRAM [ARGS...]
 
 Options:
     -v, --verbose
         Ouput verbose logging.
     -q, --quiet
         Print version only, without other text.
+    --run
+        Run a program with an updated version.
 """
 
 import logging
+import os
+import shutil
+import subprocess
 import sys
+from pathlib import Path
 
+import tomlkit
 from docopt import docopt
 
 from lenskit._version import lenskit_version
+
+_log = logging.getLogger("lenskit.version-tool")
 
 options = docopt(__doc__ or "")
 
@@ -39,6 +49,28 @@ logging.basicConfig(level=level, stream=sys.stderr)
 
 # get the version
 version = lenskit_version()
+
+if options["--run"]:
+    ppt_file = Path("pyproject.toml")
+    saved = ppt_file.with_suffix(".toml.saved")
+    shutil.copy(ppt_file, saved)
+    with ppt_file.open("rt") as tf:
+        meta = tomlkit.load(tf)
+
+    proj = meta["project"]
+    proj["dynamic"].remove("version")
+    proj["version"] = str(version)
+
+    try:
+        with ppt_file.open("wt") as tf:
+            _log.debug("writing updated pyproject.toml")
+            tomlkit.dump(meta, tf)
+
+        subprocess.check_call([options["PROGRAM"]] + options["ARGS"])
+    finally:
+        _log.info("restoring pyproject.toml")
+        os.replace(saved, ppt_file)
+
 
 # we only want to print the version
 if options["--quiet"]:
