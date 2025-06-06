45ba173473b224e6447d79348c0ff963562df996
Michael Ekstrand <mdekstrand@drexel.edu>
2025-06-06 11:45:19 -0400

    add separate single and batch recommend integration tests

--- tests/integration/test_pipeline_basics.py
+++ tests/integration/test_pipeline_basics.py
@@ -10,7 +10,7 @@ Whole-pipeline integration tests to check for composite bugs.
 
 import numpy as np
 
-from lenskit import batch
+from lenskit import batch, recommend
 from lenskit.basic import (
     RandomSelector,
     UnratedTrainingItemsCandidateSelector,
@@ -20,7 +20,30 @@ from lenskit.data import Dataset, QueryInput
 from lenskit.pipeline import PipelineBuilder
 
 
-def test_training_items_removed(rng: np.random.Generator, ml_ds: Dataset):
+def test_training_items_removed_solo(rng: np.random.Generator, ml_ds: Dataset):
+    pb = PipelineBuilder()
+    query = pb.create_input("query", QueryInput)
+    query = pb.add_component("lookup", UserTrainingHistoryLookup, query=query)
+    candidates = pb.add_component("candidate-selector", UnratedTrainingItemsCandidateSelector)
+    pb.add_component("recommender", RandomSelector, {"n": 100}, items=candidates)
+
+    pipe = pb.build()
+    pipe.train(ml_ds)
+
+    users = rng.choice(ml_ds.users.ids(), 200)
+
+    for uid in users:
+        items = recommend(pipe, uid)
+
+        train_items = ml_ds.user_row(uid)
+        assert train_items is not None
+
+        assert len(items) == 100
+        assert not np.any(np.isin(items.ids(), train_items.ids()))
+        assert not np.any(np.isin(items.numbers(), train_items.numbers()))
+
+
+def test_training_items_removed_batch(rng: np.random.Generator, ml_ds: Dataset):
     pb = PipelineBuilder()
     query = pb.create_input("query", QueryInput)
     query = pb.add_component("lookup", UserTrainingHistoryLookup, query=query)
