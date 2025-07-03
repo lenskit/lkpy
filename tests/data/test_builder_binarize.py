6ec0dfb4d232605f5672378965f41caaffa4b5a4
vibhor kumar <vibhork1105@gmail.com>
2025-07-03 21:18:06 +0530

    test corrected

--- tests/data/test_builder_binarize.py
+++ tests/data/test_builder_binarize.py
@@ -18,8 +18,9 @@ def test_binarize_zero(ml_ds: Dataset) -> None:
     """Test that binarize removes items."""
     ratings_df = ml_ds.interactions().pandas()
     dsb = DatasetBuilder()
-    ratings_df["user"] = ratings_df["user"].astype(int)
-    ratings_df["item"] = ratings_df["item"].astype(int)
+    ratings_df["user_num"] = ratings_df["user_num"].astype(int)
+    ratings_df["item_num"] = ratings_df["item_num"].astype(int)
+    ratings_df = ratings_df.rename(columns={"user_num": "user_id", "item_num": "item_id"})
     dsb.add_interactions(
         "rating", ratings_df, entities=["user", "item"], missing="insert", default=True
     )
@@ -35,8 +36,9 @@ def test_binarize_zero(ml_ds: Dataset) -> None:
 def test_binarize_remove(ml_ds: Dataset) -> None:
     ratings_df = ml_ds.interactions().pandas()
     dsb = DatasetBuilder()
-    ratings_df["user"] = ratings_df["user"].astype(int)
-    ratings_df["item"] = ratings_df["item"].astype(int)
+    ratings_df["user_num"] = ratings_df["user_num"].astype(int)
+    ratings_df["item_num"] = ratings_df["item_num"].astype(int)
+    ratings_df = ratings_df.rename(columns={"user_num": "user_id", "item_num": "item_id"})
     dsb.add_interactions(
         "rating", ratings_df, entities=["user", "item"], missing="insert", default=True
     )
@@ -51,8 +53,9 @@ def test_binarize_remove(ml_ds: Dataset) -> None:
 def test_binarize_error(ml_ds: Dataset) -> None:
     ratings_df = ml_ds.interactions().pandas()
     dsb = DatasetBuilder()
-    ratings_df["user"] = ratings_df["user"].astype(int)
-    ratings_df["item"] = ratings_df["item"].astype(int)
+    ratings_df["user_num"] = ratings_df["user_num"].astype(int)
+    ratings_df["item_num"] = ratings_df["item_num"].astype(int)
+    ratings_df = ratings_df.rename(columns={"user_num": "user_id", "item_num": "item_id"})
     dsb.add_interactions(
         "rating", ratings_df, entities=["user", "item"], missing="insert", default=True
     )
@@ -60,8 +63,8 @@ def test_binarize_error(ml_ds: Dataset) -> None:
     with raises(ValueError, match="method must be 'zero' or 'remove'"):
         dsb.binarize_ratings("rating", min_pos_rating=3.5, method="unknown")
 
-    with raises(ValueError, match="min_pos_rating 6.0 is outside the rating range"):
+    with raises(ValueError, match="min_pos_rating 6.0 is not in the range of ratings \[.*\]"):
         dsb.binarize_ratings("rating", min_pos_rating=6.0, method="zero")
 
-    with raises(ValueError, match="min_pos_rating -1.0 is outside the rating range"):
+    with raises(ValueError, match="min_pos_rating -1.0 is not in the range of ratings \[.*\]"):
         dsb.binarize_ratings("rating", min_pos_rating=-1.0, method="zero")
