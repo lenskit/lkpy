ab7e663fc9f5ac8b0f684036236a194a17395000
Michael Ekstrand <mdekstrand@drexel.edu>
2025-08-01 17:06:32 -0400

    data: first pass at Steam

--- src/lenskit/data/sources/steam.py
+++ src/lenskit/data/sources/steam.py
@@ -20,6 +20,12 @@ import pyarrow as pa
 from more_itertools import chunked
 from xopen import xopen
 
+from lenskit.data import Dataset, DatasetBuilder
+from lenskit.diagnostics import DataError
+from lenskit.logging import get_logger
+
+_log = get_logger(__name__)
+
 BATCH_SIZE = 10_000
 AU_USERS_ITEMS_SCHEMA = pa.schema(
     {
@@ -32,13 +38,86 @@ AU_USERS_ITEMS_SCHEMA = pa.schema(
                 [
                     pa.field("item_id", pa.string()),
                     pa.field("item_name", pa.string()),
-                    pa.field("playtime_2weeks", pa.string()),
-                    pa.field("playtime_forever", pa.string()),
+                    pa.field("playtime_2weeks", pa.float32()),
+                    pa.field("playtime_forever", pa.float32()),
                 ]
             )
         ),
     }
 )
+
+
+def load_steam(*files: Path, reviews: bool = False) -> Dataset:
+    """
+    Load a `Steam dataset`_ from Julian McAuley's group at UCSD.
+
+    .. _Steam dataset: https://cseweb.ucsd.edu/~jmcauley/datasets.html#steam_data
+
+    .. note::
+
+        This function uses filenames to detect which version of the data to
+        load, so the files should be named exactly as they are from McAuley's
+        download site, optionally recompressed.
+
+    Args:
+        files:
+            Input files to read, in any order.  Can only load one version of
+            the data at a time (Australian or full).
+        reviews:
+            Set to ``True`` to include review text in the loaded dataset.
+    """
+    au_interactions = None
+    au_reviews = None
+    all_games = None
+    all_reviews = None
+
+    # autodetect our file(s)
+    for file in files:
+        name = file.name
+        if name.startswith("australian_user_reviews.json"):
+            au_reviews = file
+        elif name.startswith("australian_users_items.json"):
+            au_interactions = file
+        elif name.startswith("steam_games.json"):
+            all_games = file
+        elif name.startswith("steam_reviews.json"):
+            all_reviews = file
+
+    if au_interactions is not None:
+        _log.debug("looking for Australian subset interactions")
+        if all_reviews is not None or all_games is not None:
+            _log.error("cannot specify both Australian and overall input files")
+            raise DataError("invalid combination of Steam input files")
+
+        return _load_au_steam(au_interactions, au_reviews if reviews else None)
+
+    elif all_reviews is not None:
+        _log.debug("looking for full-data inteactions")
+        if au_reviews is not None:
+            _log.error("cannot specify both Australian and overall input files")
+            raise DataError("invalid combination of Steam input files")
+
+        return _load_all_steam(all_games, all_reviews, include_reviews=reviews)
+    else:
+        _log.error("must supply one of australian_users_items or steam_reviews")
+        raise DataError("no Steam interactions provided")
+
+
+def _load_au_steam(interactions: Path, reviews: Path | None) -> Dataset:
+    _dsb = DatasetBuilder()
+
+    _ui_data = _read_table(interactions, AU_USERS_ITEMS_SCHEMA)
+
+
+def _load_all_steam(games: Path | None, reviews: Path, *, include_reviews: bool) -> Dataset:
+    raise NotImplementedError()
+
+
+def _read_table(path: Path, schema: pa.Schema | None = None) -> pa.Table:
+    _log.debug("reading table from loose JSON", file=str(path))
+    tbl = pa.Table.from_batches(_decode_chunks(path, schema))
+    _log.debug("finished reading table", rows=tbl.num_rows, file=str(path))
+    return tbl
 
 
 def _decode_chunks(path: Path, schema: pa.Schema | None = None) -> Generator[pa.RecordBatch]:
