fd4ea6154e476bace34bd72ba6bfc4c73281fcc6
Michael Ekstrand <mdekstrand@drexel.edu>
2025-08-15 19:41:15 -0400

    invoke the build runner

--- mise/tasks/test.sh
+++ mise/tasks/test.sh
@@ -2,17 +2,27 @@
 #MISE description="Run tests"
 #USAGE flag "--coverage" help="Run tests with coverage."
 #USAGE flag "--slow" help="Include slow tests."
+#USAGE flag "--release" help="Test the release-build accelerator."
 #USAGE arg "<arg>" var=#true default="tests" help="Arguments and paths to pytest"
 
 set -eo pipefail
 source "$MISE_PROJECT_ROOT/mise/task-functions.sh"
 
 declare -a test_args=()
+declare -a build_args=()
 
 if [[ $usage_coverage = true ]]; then
     msg "running tests with coverage"
     test_args+=(--cov=src/lenskit --cov-report=xml --cov-report=term)
+    export RUSTFLAGS="-C instrument-coverage"
 fi
+
+if [[ $usage_release = true ]]; then
+    build_args+=(-r)
+fi
+
+msg "building accelerator"
+mise run build:accel -- "${build_args[@]}"
 
 if [[ $usage_slow != true ]]; then
     test_args+=(-m 'not slow')
