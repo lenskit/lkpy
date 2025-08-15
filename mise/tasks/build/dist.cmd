cced8092197e9501af8f1b49133c54ca30ea6faa
Michael Ekstrand <mdekstrand@drexel.edu>
2025-08-15 19:05:25 -0400

    add usage

--- mise/tasks/build/dist.cmd
+++ mise/tasks/build/dist.cmd
@@ -1,3 +1,7 @@
 @echo off
+GOTO start
+#MISE description="Build distribution"
+#USAGE flag "-s --sdist" help="build source distribution only"
+:start
 bash %MISE_TASK_DIR%/dist.sh %*
 exit %ERRORLEVEL%
