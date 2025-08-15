4d56ae95e9bf1c18be92496554f75c55ac4fa27f
Michael Ekstrand <mdekstrand@drexel.edu>
2025-08-15 16:12:47 -0700

    usage for accelerator build

--- mise/tasks/build/accel.sh
+++ mise/tasks/build/accel.sh
@@ -1,30 +1,14 @@
 #!/usr/bin/env bash
 #MISE description="Build accelerator for in-place testing."
 #MISE depends=["init-dirs"]
+#USAGE flag "-r --release" help="build in release mode"
 
 set -eo pipefail
 
 . "$MISE_PROJECT_ROOT/mise/task-functions.sh"
 
-declare -A options
-
-while getopts rh arg; do
-    options[$arg]="${OPTARG:-1}"
-done
-
-if ((${options[h]})); then
-    cat <<EOD
-mise run build:accel [-h] [-P profile]
-
-Options:
-    -r      build in release mode
-    -h      print this help message
-EOD
-    exit
-fi
-
 declare -a build_args=()
-if ((${options[r]})); then
+if [[ $usage_release = true ]]; then
     build_args+='--profile=release'
 else
     build_args+='--profile=dev'
