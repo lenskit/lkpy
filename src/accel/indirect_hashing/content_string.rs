2a2e1e2934a7d9b7abc78815fddf415a0e2aba58
Michael Ekstrand <mdekstrand@drexel.edu>
2025-08-15 12:56:09 -0400

    support large strings in indirect hash table

--- src/accel/indirect_hashing/content_string.rs
+++ src/accel/indirect_hashing/content_string.rs
@@ -6,8 +6,10 @@
 
 use std::hash::{Hash, Hasher};
 
-use arrow::array::{make_array, Array, ArrayData, AsArray, StringArray, StringBuilder};
-use arrow::compute::cast;
+use arrow::array::{
+    make_array, Array, ArrayData, AsArray, GenericStringArray, OffsetSizeTrait, StringArray,
+    StringBuilder, StringViewArray,
+};
 use arrow::pyarrow::PyArrowType;
 use arrow_schema::DataType;
 use pyo3::exceptions::PyTypeError;
@@ -16,6 +18,12 @@ use rustc_hash::FxHasher;
 
 use crate::indirect_hashing::{IndirectHashContent, IndirectSearcher};
 
+/// Helper trait for accessing string arrays.
+trait StringAccess {
+    fn len(&self) -> usize;
+    fn value(&self, idx: usize) -> &str;
+}
+
 /// ID array implementation for string IDs.
 pub struct StringContentArray {
     array: StringArray,
@@ -23,7 +31,7 @@ pub struct StringContentArray {
 
 pub struct StringSearch<'a> {
     this: &'a StringArray,
-    other: StringArray,
+    other: Box<dyn StringAccess>,
 }
 
 impl StringContentArray {
@@ -44,7 +52,7 @@ impl IndirectHashContent for StringContentArray {
     }
 
     fn len(&self) -> usize {
-        self.array.len()
+        Array::len(&self.array)
     }
 
     fn create_searcher<'py, 'a>(
@@ -52,15 +60,23 @@ impl IndirectHashContent for StringContentArray {
         _py: pyo3::Python<'py>,
         val: pyo3::Bound<'py, pyo3::PyAny>,
     ) -> pyo3::PyResult<StringSearch<'a>> {
-        let arr = if let Ok(val) = val.extract::<&str>() {
+        let arr: Box<dyn StringAccess> = if let Ok(val) = val.extract::<&str>() {
             let mut ab = StringBuilder::with_capacity(1, val.len() + 1);
             ab.append_value(val);
-            ab.finish()
+            Box::new(ab.finish())
         } else if let Ok(PyArrowType(arr)) = val.extract::<PyArrowType<ArrayData>>() {
             let arr = make_array(arr);
-            let arr = cast(&arr, &DataType::Utf8)
-                .map_err(|e| PyTypeError::new_err(format!("error casting arrays: {}", e)))?;
-            arr.as_string().clone()
+            match arr.data_type() {
+                DataType::Utf8 => Box::new(arr.as_string::<i32>().clone()),
+                DataType::Utf8View => Box::new(arr.as_string_view().clone()),
+                DataType::LargeUtf8 => Box::new(arr.as_string::<i64>().clone()),
+                t => {
+                    return Err(PyTypeError::new_err(format!(
+                        "unsupported array type {:?}",
+                        t
+                    )))
+                }
+            }
         } else {
             return Err(PyTypeError::new_err(format!(
                 "invalid value type {}",
@@ -81,23 +97,56 @@ impl<'a> IndirectSearcher<'a> for StringSearch<'a> {
     }
 
     fn hash(&self, idx: usize) -> u64 {
-        hash_array_idx(&self.other, idx)
+        hash_array_idx(self.other.as_ref(), idx)
     }
 
     fn compare_with_entry(&self, search_idx: usize, table_idx: u32) -> bool {
-        compare_array_idx(self.this, table_idx as usize, &self.other, search_idx)
+        compare_array_idx(
+            self.this,
+            table_idx as usize,
+            self.other.as_ref(),
+            search_idx,
+        )
     }
 }
 
-fn hash_array_idx(arr: &StringArray, idx: usize) -> u64 {
+fn hash_array_idx(arr: &dyn StringAccess, idx: usize) -> u64 {
     let mut h = FxHasher::default();
     let v = arr.value(idx);
     v.hash(&mut h);
     h.finish()
 }
 
-fn compare_array_idx(arr1: &StringArray, idx1: usize, arr2: &StringArray, idx2: usize) -> bool {
+fn compare_array_idx(
+    arr1: &StringArray,
+    idx1: usize,
+    arr2: &dyn StringAccess,
+    idx2: usize,
+) -> bool {
     let v1 = arr1.value(idx1);
     let v2 = arr2.value(idx2);
     v1 == v2
+}
+
+impl<O> StringAccess for GenericStringArray<O>
+where
+    O: OffsetSizeTrait,
+{
+    fn len(&self) -> usize {
+        Array::len(self)
+    }
+
+    fn value(&self, idx: usize) -> &str {
+        GenericStringArray::value(self, idx)
+    }
+}
+
+impl StringAccess for StringViewArray {
+    fn len(&self) -> usize {
+        Array::len(self)
+    }
+
+    fn value(&self, idx: usize) -> &str {
+        StringViewArray::value(self, idx)
+    }
 }
