5e0d07ef6748ebaa4ce410d7183f7e911e0a4b2f
Michael Ekstrand <mdekstrand@drexel.edu>
2025-08-26 13:51:28 -0400

    initial test for loading a configuration

---
+++ tests/pipeline/test_human_config.py
@@ -0,0 +1,23 @@
+# This file is part of LensKit.
+# Copyright (C) 2018-2023 Boise State University.
+# Copyright (C) 2023-2025 Drexel University.
+# Licensed under the MIT license, see LICENSE.md for details.
+# SPDX-License-Identifier: MIT
+
+from pathlib import Path
+
+from lenskit.config import load_config_data
+from lenskit.pipeline.config import PipelineConfig
+
+config_dir = Path("pipelines")
+
+
+def test_load_config():
+    als_file = config_dir / "als-implicit.toml"
+    als_cfg = load_config_data(als_file, PipelineConfig)
+    assert isinstance(als_cfg, PipelineConfig)
+    assert als_cfg.meta.name == "als-implicit"
+
+
+def test_apply_scorer_config():
+    pass
