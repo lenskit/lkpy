a18765240e73c428f09580851155f83b7a78b40c
Michael Ekstrand <mdekstrand@drexel.edu>
2025-08-26 14:38:44 -0400

    working PyTorch geometric covnersion

--- src/lenskit/graphs/data.py
+++ src/lenskit/graphs/data.py
@@ -8,10 +8,43 @@
 Convert LensKit datasets to PyG graphs.
 """
 
+import torch
 from torch_geometric.data import HeteroData
 
 from lenskit.data import Dataset
+from lenskit.logging import get_logger
+
+_log = get_logger(__name__)
 
 
 def torch_graph(ds: Dataset) -> HeteroData:
-    pass
+    """
+    Convert a LensKit dataset to a PyG heterogeneous graph.
+
+    Stability:
+        Experimental
+    """
+    log = _log.bind(dataset=ds.name)
+    data = HeteroData()
+
+    for entity in ds.schema.entities:
+        log.debug("adding entities", type=entity)
+        data[entity].n_id = ds.entities(entity).numbers()
+
+    for rel in ds.schema.relationships:
+        log.debug("adding relationships", type=rel)
+        rset = ds.relationships(rel)
+        entities = rset.entities
+        if len(entities) != 2:
+            log.warn("ignoring non-binary relationship %s", rel)
+            continue
+
+        src, tgt = entities
+        tbl = rset.arrow(attributes=[])
+        print(tbl)
+
+        cols = [torch.tensor(c.to_numpy()) for c in tbl.columns]
+        edges = torch.stack(cols)
+        data[src, rel, tgt].edge_index = edges
+
+    return data
