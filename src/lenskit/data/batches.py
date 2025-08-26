8cede988052b40952b7a9f3974928d3b80051e1e
Michael Ekstrand <mdekstrand@drexel.edu>
2025-08-26 17:19:24 -0400

    implement LightGCN passing tests

---
+++ src/lenskit/data/batches.py
@@ -0,0 +1,44 @@
+# This file is part of LensKit.
+# Copyright (C) 2018-2023 Boise State University.
+# Copyright (C) 2023-2025 Drexel University.
+# Licensed under the MIT license, see LICENSE.md for details.
+# SPDX-License-Identifier: MIT
+
+"""
+Basic batching support.
+"""
+
+from __future__ import annotations
+
+import math
+from collections.abc import Iterator
+from dataclasses import dataclass
+
+from .types import Extent
+
+
+@dataclass
+class BatchIter:
+    """
+    Iterator over batches of a collection size.
+
+    The length of this collection is the number of batches,
+    and iterating over it yields batch extents.
+    """
+
+    n: int
+    "The total number of items."
+    size: int
+    "The batch size."
+
+    @property
+    def batch_count(self) -> int:
+        return math.ceil(self.n / self.size)
+
+    def __len__(self) -> int:
+        return self.batch_count
+
+    def __iter__(self) -> Iterator[Extent]:
+        for start in range(0, self.n, self.size):
+            end = min(start + self.size, self.n)
+            yield Extent(start, end)
