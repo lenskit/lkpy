15f2e495347b9a8afeb737f9c2ae849e07e5c4d6
Michael Ekstrand <mdekstrand@drexel.edu>
2025-08-28 13:14:12 -0400

    add and test config file to train CLI

---
+++ tests/cli/test_train.py
@@ -0,0 +1,48 @@
+# This file is part of LensKit.
+# Copyright (C) 2018-2023 Boise State University.
+# Copyright (C) 2023-2025 Drexel University.
+# Licensed under the MIT license, see LICENSE.md for details.
+# SPDX-License-Identifier: MIT
+
+import json
+import pickle
+from os import fspath
+from pathlib import Path
+
+from click.testing import CliRunner
+from xopen import xopen
+
+from lenskit.als import BiasedMFScorer
+from lenskit.cli import lenskit
+from lenskit.data import Dataset
+from lenskit.pipeline.nodes import ComponentInstanceNode
+
+
+def test_train_config(tmpdir: Path, ml_ds: Dataset):
+    ds_path = tmpdir / "ml-data"
+    ml_ds.save(ds_path)
+
+    out_file = tmpdir / "als.pkl.zst"
+
+    runner = CliRunner()
+    result = runner.invoke(
+        lenskit,
+        [
+            "train",
+            "--config",
+            "pipelines/als-explicit.toml",
+            "-o",
+            fspath(out_file),
+            fspath(ds_path),
+        ],
+    )
+
+    assert result.exit_code == 0
+    assert out_file.exists()
+
+    with xopen(out_file, "rb") as pf:
+        pipe = pickle.load(pf)
+
+    node = pipe.node("scorer")
+    assert isinstance(node, ComponentInstanceNode)
+    assert isinstance(node.component, BiasedMFScorer)
