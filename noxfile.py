4dc4a282b402f79e8b8909a6f20dac2617a13031
Michael Ekstrand <mdekstrand@drexel.edu>
2025-09-04 12:40:08 -0400

    initial nox support for torch variants

---
+++ noxfile.py
@@ -0,0 +1,17 @@
+# This file is part of LensKit.
+# Copyright (C) 2018-2023 Boise State University.
+# Copyright (C) 2023-2025 Drexel University.
+# Licensed under the MIT license, see LICENSE.md for details.
+# SPDX-License-Identifier: MIT
+
+import nox
+
+
+@nox.session(venv_backend="uv")
+@nox.parametrize("torch", ["2.4", "2.5", "2.6", "2.7", "2.8"])
+def test(session, torch):
+    session.install(f"torch ~={torch}.0", "-e", ".", "--group", "test")
+    opts = session.posargs
+    if not opts:
+        opts = ["-m", "not slow", "tests"]
+    session.run("pytest", *opts)
