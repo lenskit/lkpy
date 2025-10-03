7cae0a9bf28c4f4ec5a6263fd3f35851e0fba038
Michael Ekstrand <mdekstrand@drexel.edu>
2025-10-03 14:42:23 -0400

    add more tests for CLI

--- tests/cli/test_data_split.py
+++ tests/cli/test_data_split.py
@@ -64,3 +64,31 @@ def test_data_split_time(tmpdir: Path, ml_ds: Dataset):
     test_n = sum(len(il) for il in items.lists())
 
     assert trds.interaction_count + test_n == ml_ds.interaction_count
+
+
+def test_data_out_dir(tmpdir: Path, ml_ds: Dataset):
+    ds_path = tmpdir / "ml-data"
+    ml_ds.save(ds_path)
+
+    out_dir = tmpdir / "split"
+    train_path = out_dir / "train"
+    test_path = out_dir / "test.parquet"
+
+    runner = CliRunner()
+    result = runner.invoke(
+        lenskit,
+        ["data", "split", "--fraction", "0.2", "-d", fspath(out_dir), fspath(ds_path)],
+    )
+
+    assert result.exit_code == 0
+    assert train_path.exists()
+    assert test_path.exists()
+
+    trds = Dataset.load(train_path)
+    print(trds)
+    items = ItemListCollection.load_parquet(test_path)
+    test_n = sum(len(il) for il in items.lists())
+    tgt_n = int(ml_ds.interaction_count * 0.2)
+    assert tgt_n - 1 <= test_n <= tgt_n + 1
+
+    assert trds.interaction_count + test_n == ml_ds.interaction_count
