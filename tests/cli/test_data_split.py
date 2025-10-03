5bb067f6c6956ee65c4d91303672e6a59c5d87b7
Michael Ekstrand <mdekstrand@drexel.edu>
2025-10-03 13:29:56 -0400

    add CLI test

---
+++ tests/cli/test_data_split.py
@@ -0,0 +1,41 @@
+# This file is part of LensKit.
+# Copyright (C) 2018-2023 Boise State University.
+# Copyright (C) 2023-2025 Drexel University.
+# Licensed under the MIT license, see LICENSE.md for details.
+# SPDX-License-Identifier: MIT
+
+import json
+from os import fspath
+from pathlib import Path
+
+from click.testing import CliRunner
+
+from lenskit.cli import lenskit
+from lenskit.data import Dataset, ItemListCollection
+
+
+def test_data_split(tmpdir: Path, ml_ds: Dataset):
+    ds_path = tmpdir / "ml-data"
+    ml_ds.save(ds_path)
+
+    train_path = tmpdir / "ml-data.train"
+    test_path = tmpdir / "ml-data.test.parquet"
+
+    runner = CliRunner()
+    result = runner.invoke(
+        lenskit,
+        ["data", "split", "--fraction", "0.2", fspath(ds_path)],
+    )
+
+    assert result.exit_code == 0
+    assert train_path.exists()
+    assert test_path.exists()
+
+    trds = Dataset.load(train_path)
+    print(trds)
+    items = ItemListCollection.load_parquet(test_path)
+    test_n = sum(len(il) for il in items.lists())
+    tgt_n = int(ml_ds.interaction_count * 0.2)
+    assert tgt_n - 1 <= test_n <= tgt_n + 1
+
+    assert trds.interaction_count + test_n == ml_ds.interaction_count
