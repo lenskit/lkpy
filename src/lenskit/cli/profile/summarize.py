3a92e00d9f6dbbbcafcfeb97311b3e03bdf592ab
Michael Ekstrand <mdekstrand@drexel.edu>
2025-10-20 16:08:36 -0400

    improve summarization display

--- src/lenskit/cli/profile/summarize.py
+++ src/lenskit/cli/profile/summarize.py
@@ -8,6 +8,7 @@ from pathlib import Path
 
 import click
 import pandas as pd
+from rich.table import Table
 
 from lenskit.logging import stdout_console
 
@@ -24,6 +25,15 @@ def summarize_profile(file: Path):
     # convert to ms
     prof = prof / 1_000_000
     summary = prof.agg(["mean", "std", "median"]).T
+    summary["95%ile"] = prof.quantile(0.95)
+    summary.index.name = "Stage"
 
-    console.print("[bold]Stage timing (ms):[/bold]")
-    console.print(summary)
+    tbl = Table(title="Stage Timing (ms)")
+    tbl.add_column("Stage", justify="left")
+    for c in summary.columns:
+        tbl.add_column(c, justify="right")
+
+    for row in summary.itertuples():
+        tbl.add_row(row[0], *["{:.4f}".format(r) for r in row[1:]])
+
+    console.print(tbl)
