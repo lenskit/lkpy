c8f83c0c2f9c7cf864458d1ac576b5941317990a
Michael Ekstrand <mdekstrand@drexel.edu>
2025-10-27 12:11:32 -0400

    add support for test coverage build on accelerator

--- mise/tasks/build/accel.sh
+++ mise/tasks/build/accel.sh
@@ -2,6 +2,7 @@
 #MISE description="Build accelerator for in-place testing."
 #MISE depends=["init-dirs"]
 #USAGE flag "-r --release" help="build in release mode"
+#USAGE flag "--coverage" help="build for code coverage"
 
 set -eo pipefail
 
@@ -15,3 +16,8 @@ else
 fi
 
 echo-run maturin develop "${build_args[@]}"
+
+if [[ $usage_coverage = true ]]; then
+    msg "rebuilding final stage for coverage"
+    echo-run maturin develop "${build_args[@]}" -- -C instrument-coverage
+fi
