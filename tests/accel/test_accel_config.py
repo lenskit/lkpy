75aa8bb12a6da49f1fc8439a88366a6796e8655c
Michael Ekstrand <mdekstrand@drexel.edu>
2025-10-29 10:45:14 -0400

    move tests arround + add test for accel thread count

---
+++ tests/accel/test_accel_config.py
@@ -0,0 +1,16 @@
+# This file is part of LensKit.
+# Copyright (C) 2018-2023 Boise State University.
+# Copyright (C) 2023-2025 Drexel University.
+# Licensed under the MIT license, see LICENSE.md for details.
+# SPDX-License-Identifier: MIT
+
+from lenskit import _accel
+from lenskit.parallel import ensure_parallel_init, get_parallel_config
+
+
+def test_thread_count():
+    ensure_parallel_init()
+    pc = get_parallel_config()
+
+    tc = _accel.thread_count()
+    assert tc == pc.threads
