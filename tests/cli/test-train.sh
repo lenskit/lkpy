04b4de05185364ae57291236a9e83f439b588355
Michael Ekstrand <mdekstrand@drexel.edu>
2025-11-07 12:51:57 -0500

    fix verify train

--- tests/cli/test-train.sh
+++ tests/cli/test-train.sh
@@ -5,4 +5,4 @@ run-lenskit data convert --movielens "$ML_TEST_DIR" "$data"
 run-lenskit train --config pipelines/als-explicit.toml -o "$out" "$data"
 
 require -f "$out"
-run-python "$TEST_DIR/test-train-verify.py" "$out"
+run-python "$TEST_DIR/verify-train.py" "$out"
