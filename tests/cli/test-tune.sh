58ec483bd7d6f751ac58da8c0c0b726b42d6adc2
Michael Ekstrand <mdekstrand@drexel.edu>
2025-11-07 12:36:27 -0500

    use ML100K for tuning test

--- tests/cli/test-tune.sh
+++ tests/cli/test-tune.sh
@@ -1,3 +1,4 @@
+ML100K="data/ml-100k.zip"
 data="$TEST_WORK/ml-data"
 train="$TEST_WORK/ml-data.train"
 test="$TEST_WORK/ml-data.test.parquet"
@@ -8,8 +9,13 @@ if ! which ray >/dev/null; then
     exit 0
 fi
 
+if [[ ! -f $ML100K ]]; then
+    msg "ML-100K not available, skipping tune test"
+    exit 0
+fi
+
 begin-suite
-run-lenskit data convert --movielens "$ML_TEST_DIR" "$data"
+run-lenskit data convert --movielens "$ML100K" "$data"
 run-lenskit data split --fraction=0.2 --min-train-interactions=5 "$data"
 run-lenskit tune -T "$train" -V "$test" --save-pipeline "$TEST_WORK/pipeline.json" \
     --max-points=10 pipelines/bias-search.toml "$out"
