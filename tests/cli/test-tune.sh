00b1c8122074fe3a225e1103e2827931a1b51418
Michael Ekstrand <mdekstrand@drexel.edu>
2025-11-07 12:38:14 -0500

    add iterative test

--- tests/cli/test-tune.sh
+++ tests/cli/test-tune.sh
@@ -17,8 +17,18 @@ fi
 begin-suite
 run-lenskit data convert --movielens "$ML100K" "$data"
 run-lenskit data split --fraction=0.2 --min-train-interactions=5 "$data"
+tap_comment "testing non-iterative tuning"
 run-lenskit tune -T "$train" -V "$test" --save-pipeline "$TEST_WORK/pipeline.json" \
     --max-points=10 pipelines/bias-search.toml "$out"
+
+require -d "$out"
+require -f "$out/result.json"
+require -f "$TEST_WORK/pipeline.json"
+
+out="$TEST_WORK/als-tune"
+tap_comment "testing iterative tuning"
+run-lenskit tune -T "$train" -V "$test" --save-pipeline "$TEST_WORK/pipeline.json" \
+    --max-points=10 pipelines/als-implicit-search.toml "$out"
 
 require -d "$out"
 require -f "$out/result.json"
