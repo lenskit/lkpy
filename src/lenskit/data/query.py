e795a9265f08c1c6d2bfe5f815601f6f65f6daad
Michael Ekstrand <mdekstrand@drexel.edu>
2025-11-07 16:31:23 -0500

    initial support for session queries

--- src/lenskit/data/query.py
+++ src/lenskit/data/query.py
@@ -11,13 +11,15 @@ Recommendation queries.
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import TypeAlias
+from typing import Literal, TypeAlias
 
 import numpy as np
 
 from lenskit.data.items import ItemList
 
 from .types import ID
+
+QueryItemSource: TypeAlias = Literal["history", "session", "context"]
 
 
 @dataclass
@@ -42,14 +44,35 @@ class RecQuery:
 
     user_id: ID | None = None
     """
-    The user's identifier.
+    The user's identifier, if known.
     """
-    user_items: ItemList | None = None
+
+    history_items: ItemList | None = None
     """
     The items from the user's interaction history, with ratings if available.  This
     list is *deduplicated*, like :meth:`~lenskit.data.Dataset.interaction_matrix`,
     rather than a full interaction log.  As with :meth:`~lenskit.data.Dataset.user_row`,
     the rating is expected to be on a ``rating`` field, not ``score``.
+    """
+
+    session_items: ItemList | None = None
+    """
+    Items the user has interacted with in the current session.
+    """
+
+    context_items: ItemList | None = None
+    """
+    Items that form a context for the requested recommendations.  For example:
+
+    -   To recommend products related to a product the user is viewing, the
+        viewed product is a context item.
+    -   To recommend products to add to a shopping cart, the current cart
+        contents are the context items.
+    """
+
+    query_source: QueryItemSource | set[QueryItemSource] | None = None
+    """
+    The list of items to return from :meth:`query_items`.
     """
 
     @classmethod
@@ -71,13 +94,70 @@ class RecQuery:
         elif isinstance(data, RecQuery):
             return data
         elif isinstance(data, ItemList):
-            return cls(user_items=data)
+            return cls(history_items=data)
         elif isinstance(data, np.integer):
             return cls(user_id=data.item())
         elif isinstance(data, int | str | bytes):
             return cls(user_id=data)
         else:  # pragma: nocover
             raise TypeError(f"invalid query input (type {type(data)})")
+
+    @property
+    def user_items(self) -> ItemList | None:
+        """
+        Deprecated alias for :attr:`history_items`.
+
+        .. deprecated:: 2025.6
+
+            Use :attr:`history_items` instead.  This property will be removed in
+            LensKit 2026.1.
+        """
+        return self.history_items
+
+    @user_items.setter
+    def user_items(self, value: ItemList):
+        self.history_items = value
+
+    @property
+    def query_items(self) -> ItemList | None:
+        """
+        Return the items the recommender should treat as a “query”.
+
+        This property exists so that components can obtain a list of items to
+        use for generating recommendations, regardless of the precise shape of
+        recommendation problem being solved (e.g., user-personalized
+        recommendation or session-based recommendation).  This allows general
+        models such as :class:`~lenskit.knn.ItemKNNScorer` to operate across
+        recommendation tasks.
+
+        The :attr:`query_source` attribute determines which list(s) of items
+        in this query are considered the “query items”.  If unset, the first
+        of the following that is available are used as the query items:
+
+        1. :attr:`context_items`
+        2. :attr:`session_items`
+        3. :attr:`history_items`
+        """
+        if self.query_source is None:
+            if self.context_items is not None:
+                return self.context_items
+            if self.session_items is not None:
+                return self.session_items
+            if self.history_items is not None:
+                return self.history_items
+        elif isinstance(self.query_source, str):
+            return self._items(self.query_source)
+
+    def _items(self, source: QueryItemSource | None):
+        match source:
+            case "context":
+                return self.context_items
+            case "session":
+                return self.session_items
+            case "history":
+                return self.history_items
+            case _:  # pragma: nocover
+                raise ValueError(f"unsupported item source {source}")
 
 
 QueryInput: TypeAlias = RecQuery | ID | ItemList | None
