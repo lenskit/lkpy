c46abac453b1a02180197b3a3dd4d9cd6a09eddc
Michael Ekstrand <mdekstrand@drexel.edu>
2025-11-12 13:45:22 -0500

    add (failing) KNN session test

---
+++ tests/knn/test_knn_session.py
@@ -0,0 +1,24 @@
+# This file is part of LensKit.
+# Copyright (C) 2018-2023 Boise State University.
+# Copyright (C) 2023-2025 Drexel University.
+# Licensed under the MIT license, see LICENSE.md for details.
+# SPDX-License-Identifier: MIT
+
+from pathlib import Path
+
+from pytest import fixture, skip
+
+from lenskit import batch
+from lenskit.knn.item import ItemKNNScorer
+from lenskit.pipeline import topn_pipeline
+from lenskit.splitting import TTSplit
+from lenskit.testing import msweb
+
+
+def test_knn_batch_msweb(msweb: TTSplit):
+    pipe = topn_pipeline(ItemKNNScorer(feedback="implicit"))
+    pipe.train(msweb.train)
+
+    recs = batch.recommend(pipe, msweb.test, 10)
+    assert recs.key_fields == ["session_id"]
+    assert len(recs) == len(msweb.test)
