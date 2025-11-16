97886921fa2704e108bd0910b6d91db8b5b05271
Michael Ekstrand <mdekstrand@drexel.edu>
2025-11-16 11:45:11 -0500

    add test to evaluate session recs

--- tests/knn/test_knn_session.py
+++ tests/knn/test_knn_session.py
@@ -9,8 +9,10 @@ from pathlib import Path
 from pytest import fixture, skip
 
 from lenskit import batch
-from lenskit.data import RecQuery
+from lenskit.data import ItemListCollection, RecQuery
 from lenskit.knn.item import ItemKNNScorer
+from lenskit.metrics.bulk import RunAnalysis
+from lenskit.metrics.ranking import RBP, RecipRank
 from lenskit.pipeline import topn_pipeline
 from lenskit.splitting import TTSplit
 from lenskit.testing import msweb
@@ -25,8 +27,18 @@ def test_knn_batch_msweb(msweb: TTSplit):
         for (k, il) in msweb.test.items()
         if len(il) > 5
     ]
-    _test = {k: il[5:] for (k, il) in msweb.test.items() if len(il) > 5}
+    test = {k: il[5:] for (k, il) in msweb.test.items() if len(il) > 5}
 
     recs = batch.recommend(pipe, queries, 10)
     assert recs.key_fields == ("query_id",)
     assert len(recs) == len(queries)
+
+    test = ItemListCollection.from_dict(test, "query_id")
+    ra = RunAnalysis()
+    ra.add_metric(RBP(n=10))
+    ra.add_metric(RecipRank())
+
+    out = ra.measure(recs, test)
+    assert out is not None
+    ls = out.list_summary()
+    assert ls.loc["RBP@10", "mean"] > 0.01
