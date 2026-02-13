d31cf1ef7bef05f7fbc1b7722a5ac8166dcc39d2
Michael Ekstrand <mdekstrand@drexel.edu>
2026-02-13 10:19:06 -0500

    expand testing of ML COOC

--- tests/data/test_rel_cooc.py
+++ tests/data/test_rel_cooc.py
@@ -25,7 +25,7 @@ def test_basic_cooc(ml_ds: Dataset, rng: np.random.Generator):
     item_cooc = item_cooc.tocsr()
 
     item_tbl = ml_ds.interactions().pandas()
-    for i in rng.choice(ml_ds.item_count, 50):
+    for i in rng.choice(ml_ds.item_count, 100):
         l1 = item_tbl[item_tbl["item_num"] == i]
         lim = item_tbl[item_tbl["user_num"].isin(l1["user_num"])]
         lim = lim[lim["item_num"] != i]
@@ -47,7 +47,7 @@ def test_ordered_cooc(ml_ratings: pd.DataFrame, rng: np.random.Generator):
     item_cooc = item_cooc.tocsr()
 
     item_tbl = ml_ds.interactions().pandas()
-    for i in rng.choice(ml_ds.item_count, 50):
+    for i in rng.choice(ml_ds.item_count, 100):
         l1 = item_tbl[item_tbl["item_num"] == i]
         lim = pd.merge(item_tbl, l1, on="user_num", suffixes=[".t", ".u"])
         lim = lim[lim["timestamp.t"] > lim["timestamp.u"]]
