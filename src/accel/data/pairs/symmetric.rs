d80b8795af973ac09874c960c54227c12dbcbf32
Michael Ekstrand <mdekstrand@drexel.edu>
2026-02-13 17:24:34 -0500

    use arrays to count co-occurances

--- src/accel/data/pairs/symmetric.rs
+++ src/accel/data/pairs/symmetric.rs
@@ -4,11 +4,8 @@
 // Licensed under the MIT license, see LICENSE.md for details.
 // SPDX-License-Identifier: MIT
 
-use std::collections::HashMap;
-
 use arrow::datatypes::Int32Type;
 use log::*;
-use rustc_hash::FxBuildHasher;
 
 use crate::{
     data::pairs::PairCounter,
@@ -17,57 +14,90 @@ use crate::{
 
 /// Accumulate symmetric pair counts.
 pub struct SymmetricPairCounter {
-    rows: Vec<HashMap<i32, i32, FxBuildHasher>>,
-    total: usize,
+    n_items: usize,
+    data: Vec<i32>,
+    nnz: usize,
 }
 
 impl PairCounter for SymmetricPairCounter {
     fn create(n: usize) -> SymmetricPairCounter {
+        let cap = arith_tot(n);
         SymmetricPairCounter {
-            rows: vec![HashMap::with_hasher(FxBuildHasher); n],
-            total: 0,
+            n_items: n,
+            data: vec![0; cap],
+            nnz: 0,
         }
     }
 
     fn record(&mut self, row: i32, col: i32) {
-        if row > col {
-            self.record(col, row);
-        } else {
-            if row < 0 {
-                panic!("negative row index")
-            }
-            if col < 0 {
-                panic!("negative column index")
-            }
-
-            let row = &mut self.rows[row as usize];
-            *row.entry(col).or_default() += 1;
-            self.total += 1;
+        let (row, col) = order_coords(row, col);
+        let idx = self.compute_index(row, col);
+        let val = &mut self.data[idx];
+        if *val == 0 {
+            self.nnz += 1;
         }
+        *val += 1;
     }
 
     fn nnz(&self) -> usize {
-        self.rows.iter().map(|r| r.len()).sum()
+        self.nnz
     }
 
     fn finish(self) -> COOMatrix<Int32Type, Int32Type> {
-        // compute the # of rows
-        let size = self.nnz() * 2;
-        debug!("creating matrix with {} co-occurrance counts", size);
-        let mut bld = COOMatrixBuilder::with_capacity(size);
-        for (i, row) in self.rows.into_iter().enumerate() {
-            let ri = i as i32;
-            for (ci, count) in row.into_iter() {
-                bld.add_entry(ri, ci, count);
+        debug!("creating matrix with {} co-occurrance counts", self.nnz);
+        let mut bld = COOMatrixBuilder::with_capacity(self.nnz * 2);
+        let mut idx = 0;
+        for i in 0..self.n_items {
+            let row = i as i32;
+            for j in i..self.n_items {
+                let col = j as i32;
+                let val = self.data[idx];
+                if val > 0 {
+                    bld.add_entry(row, col, val);
+                    bld.add_entry(col, row, val);
+                }
+                idx += 1;
             }
         }
         bld.finish()
     }
 }
 
+impl SymmetricPairCounter {
+    fn compute_index(&self, row: i32, col: i32) -> usize {
+        debug_assert!(row >= 0);
+        debug_assert!(col >= row);
+        let row = row as usize;
+        let col = col as usize;
+
+        // total array size
+        let total = self.data.len();
+        // array capacity used by current & later rows
+        let remaining = arith_tot(self.n_items - row);
+        // difference is the array capacity used by earlier rows
+        let base = total - remaining;
+        base + col - row
+    }
+}
+
+fn order_coords(row: i32, col: i32) -> (i32, i32) {
+    if row < 0 {
+        panic!("negative row index");
+    }
+    if col < 0 {
+        panic!("negative column index");
+    }
+    if row <= col {
+        (row, col)
+    } else {
+        (col, row)
+    }
+}
+
 /// Compute the total of an arithmetic series.
 fn arith_tot(n: usize) -> usize {
-    n * (n + 1) / 2
+    let num = n * (n + 1);
+    num >> 1
 }
 
 #[test]
@@ -83,4 +113,60 @@ fn test_arith_one() {
 #[test]
 fn test_arith_two() {
     assert_eq!(arith_tot(2), 3);
+}
+
+#[test]
+fn test_arith_big() {
+    let n = 250;
+    let tot = (1..=n).sum::<usize>();
+    assert_eq!(arith_tot(n), tot);
+}
+
+#[test]
+fn test_first_row() {
+    let acc = SymmetricPairCounter::create(1000);
+    let i = acc.compute_index(0, 0);
+    assert_eq!(i, 0);
+    let i = acc.compute_index(0, 999);
+    assert_eq!(i, 999);
+}
+
+#[test]
+fn test_second_row() {
+    let acc = SymmetricPairCounter::create(1000);
+    let i = acc.compute_index(1, 1);
+    assert_eq!(i, 1000);
+    let i = acc.compute_index(1, 999);
+    assert_eq!(i, 1998);
+}
+
+#[test]
+fn test_last_element() {
+    let acc = SymmetricPairCounter::create(1000);
+    let i = acc.compute_index(999, 999);
+    assert_eq!(i, acc.data.len() - 1);
+}
+
+#[test]
+fn test_diagonal() {
+    let acc = SymmetricPairCounter::create(1000);
+    let n = 1000;
+    let mut pos = 0;
+    for row in 0..n {
+        let i = acc.compute_index(row as i32, row as i32);
+        assert_eq!(i, pos);
+        pos += n - row;
+    }
+}
+
+#[test]
+fn test_last_col() {
+    let acc = SymmetricPairCounter::create(1000);
+    let n = 1000;
+    let mut pos = 0;
+    for row in 0..n {
+        let i = acc.compute_index(row as i32, 999);
+        assert_eq!(i, pos + 999 - row);
+        pos += n - row;
+    }
 }
