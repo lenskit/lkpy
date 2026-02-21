7329dbe174994345dd8a48eb127e5c6250717228
Michael Ekstrand <mdekstrand@drexel.edu>
2026-02-21 17:56:20 -0500

    initial pass at implementing EASE

--- src/lenskit/knn/ease.py
+++ src/lenskit/knn/ease.py
@@ -8,11 +8,19 @@
 EASE scoring model.
 """
 
-from pydantic import BaseModel
+import numpy as np
+import scipy.linalg as spla
+from humanize import metric
+from pydantic import BaseModel, PositiveFloat
 
-from lenskit.data import Dataset, ItemList, RecQuery
+from lenskit.data import Dataset, ItemList, RecQuery, Vocabulary
+from lenskit.data.matrix import COOStructure, fast_col_cooc
+from lenskit.data.types import NPMatrix
+from lenskit.logging import Stopwatch, get_logger, item_progress
 from lenskit.pipeline import Component
 from lenskit.training import Trainable, TrainingOptions
+
+_log = get_logger(__name__)
 
 
 class EASEConfig(BaseModel):
@@ -20,7 +28,7 @@ class EASEConfig(BaseModel):
     Configuration for :class:`EASEScorer`.
     """
 
-    regularization: float = 0.1
+    regularization: PositiveFloat = 0.1
     """
     Regularization term for EASE.
     """
@@ -36,8 +44,77 @@ class EASEScorer(Component[ItemList], Trainable):
     EASE configuration.
     """
 
-    def train(self, data: Dataset, options: TrainingOptions):
-        pass
+    items: Vocabulary
+    """
+    Items known at training time.
+    """
+
+    weights: NPMatrix[np.float32]
+    """
+    Item interpolation weight matrix.
+    """
+
+    def train(self, data: Dataset, options: TrainingOptions | None = None):
+        if options is None:
+            options = TrainingOptions()
+
+        if hasattr(self, "weights") and not options.retrain:
+            return
+
+        n_items = data.item_count
+        n_users = data.user_count
+        rates = data.interactions().matrix(row_entity="item", col_entity="user")
+        log = _log.bind(n_items=n_items)
+        log.info("training EASE model")
+
+        # trim out single-user items
+        log.debug("finding recommendable items")
+        rate_csr = rates.csr_structure()
+        item_nnz = rate_csr.row_nnzs
+        ok_items = item_nnz > 1
+        n_ok = int(np.sum(ok_items))
+        log.debug("keeping %d items", n_ok)
+        iid_map = np.zeros(n_items, dtype=np.int32)
+        iid_map[ok_items] = np.arange(n_ok, dtype=np.int32)
+
+        log.debug("filtering interaction matrix")
+        rate_coo = rates.coo_structure()
+
+        kept_ents = ok_items[rate_coo.row_numbers]
+        kept_inos = rate_coo.row_numbers[kept_ents]
+        kept_unos = rate_coo.col_numbers[kept_ents]
+        kept_coo = COOStructure(kept_unos, iid_map[kept_inos], shape=(n_users, n_ok))
+        del kept_ents, rate_csr, rate_coo, rates
+
+        log.debug("computing co-occurrance matrix")
+        timer = Stopwatch()
+        with item_progress("Counting co-occurrances", len(kept_inos)) as pb:
+            cooc = fast_col_cooc(kept_coo, progress=pb)
+
+        log.info("computed %s co-occurances in %s", metric(cooc.nnz), timer)
+
+        cooc = cooc.astype(np.float32).toarray()
+        log.debug("made dense matrix")
+
+        log.debug("adding regularization term")
+        di = np.diag_indices(n_ok)
+        cooc[di] += self.config.regularization
+
+        log.debug("inverting Gram-matrix")
+        timer = Stopwatch()
+        mat = spla.inv(cooc, assume_a="pos")
+        log.info("inverted co-occurrance matrix in %s", timer)
+
+        mat /= np.diag(mat)
+        mat[di] = 0
+
+        log.debug("assembling trained item vocabulary")
+        items = data.items.id_array()
+        items = items.filter(ok_items)
+        self.items = Vocabulary(items, name="item", reorder=False)
+
+        log.info("finished training EASE for %d items", len(self.items))
+        self.weights = mat
 
     def __call__(self, query: RecQuery, items: ItemList):
         pass
