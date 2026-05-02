tune: fix off-by-one in Ray iteration limits

src/lenskit/tuning/_ray/iterative.py
@@ -132,7 +132,8 @@ class IterativeEval(ray.tune.Trainable):
             metrics["epoch_measure_s"] = m_task.duration
             elog.debug("epoch measurement finished", duration=m_task.friendly_duration)
 
-        if epoch >= epoch_limit:
+        if epoch >= epoch_limit - 1:
+            elog.debug("epoch limit hit, terminating")
             metrics[ray.tune.result.DONE] = True
 
         send_task(self.task)
@@ -144,7 +145,7 @@ class IterativeEval(ray.tune.Trainable):
 
     def save_checkpoint(self, checkpoint_dir: str):
         cpdir = Path(checkpoint_dir)
-        log = self.log.bind(epochs=self.iteration)
+        log = self.log.bind(epoch=self.iteration)
         if isinstance(self.trainer, ParameterContainer):
             log.info("saving checkpoint")
             torch.save(
@@ -160,7 +161,7 @@ class IterativeEval(ray.tune.Trainable):
     def load_checkpoint(self, checkpoint_dir: str):
         cpdir = Path(checkpoint_dir)
 
-        self.log.info("resuming from checkpoint", epochs=self.iteration)
+        self.log.info("resuming from checkpoint", epoch=self.iteration)
 
         ptf = cpdir / "model.pt"
         if ptf.exists():