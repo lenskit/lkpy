4ac4f9c85fdfda7b4ad53cd7e55f2cd08715474d
Michael Ekstrand <mdekstrand@drexel.edu>
2026-05-04 11:25:00 -0400

    util: add a Latch class

---
+++ src/lenskit/util/__init__.py
@@ -0,0 +1,14 @@
+# This file is part of LensKit.
+# Copyright (C) 2018-2023 Boise State University.
+# Copyright (C) 2023-2026 Drexel University.
+# Licensed under the MIT license, see LICENSE.md for details.
+# SPDX-License-Identifier: MIT
+
+"""
+Small utility functions or classes that don't fit elsewhere, mostly used inside
+LensKit implementations.
+"""
+
+from ._latch import Latch
+
+__all__ = ["Latch"]
