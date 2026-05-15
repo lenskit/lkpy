4fa1c73b1ffa132894a97495248bbf76f0214d10
Michael Ekstrand <md@ekstrandom.net>
2026-05-15 10:50:25 -0400

    cli: add lenskit pipeline expand to show full configs

---
+++ src/lenskit/cli/pipeline/__init__.py
@@ -0,0 +1,12 @@
+# This file is part of LensKit.
+# Copyright (C) 2018-2023 Boise State University.
+# Copyright (C) 2023-2026 Drexel University.
+# Licensed under the MIT license, see LICENSE.md for details.
+# SPDX-License-Identifier: MIT
+
+from . import expand  # noqa: E402, F401
+from ._group import pipeline
+
+__all__ = [
+    "pipeline",
+]
---
+++ src/lenskit/cli/pipeline/expand.py
@@ -0,0 +1,76 @@
+# This file is part of LensKit.
+# Copyright (C) 2018-2023 Boise State University.
+# Copyright (C) 2023-2026 Drexel University.
+# Licensed under the MIT license, see LICENSE.md for details.
+# SPDX-License-Identifier: MIT
+
+from pathlib import Path
+
+import click
+
+from lenskit.logging import get_logger, stdout_console
+from lenskit.pipeline import Component, Pipeline, topn_pipeline
+from lenskit.pipeline._types import import_path_string
+
+from ._group import pipeline
+
+_log = get_logger(__name__)
+
+
+@pipeline.command
+@click.option(
+    "-C", "--scorer-class", metavar="CLS", help="Create a default pipeline with scorer CLS"
+)
+@click.option(
+    "-c", "--config", type=Path, metavar="FILE", help="Load pipeline configuration from FILE."
+)
+@click.option(
+    "--rating-predictor",
+    is_flag=True,
+    help="Include rating prediction in the pipeline capabilities.",
+)
+@click.option("-n", "--list-length", type=int, help="Default list length for pipeline ranker.")
+@click.option(
+    "-o",
+    "--output",
+    "out_file",
+    type=Path,
+    metavar="FILE",
+    help="Save expanded pipeline configuration to FILE.",
+)
+def expand(
+    scorer_class: str | None,
+    config: Path | None,
+    out_file: Path,
+    list_length: int | None,
+    rating_predictor: bool,
+):
+    """
+    Expand a pipeline configuration into fully-formed pipeline and save or print
+    the resulting configuration.
+    """
+    console = stdout_console()
+    if config is not None:
+        if scorer_class is not None:
+            _log.error("cannot specify both scorer class and configuration file")
+            raise SystemExit(3)
+
+        _log.info("loading pipeline from %s", config)
+        pipe = Pipeline.load_config(config)
+
+    elif scorer_class is not None:
+        _log.info("creating pipeline for %s", config)
+        scorer = import_path_string(scorer_class)
+        assert issubclass(scorer, Component)
+        pipe = topn_pipeline(scorer, predicts_ratings=rating_predictor, n=list_length)
+
+    else:
+        _log.error("no scorer specified")
+        raise SystemExit(5)
+
+    if out_file is not None:
+        with out_file.open("wt") as jf:
+            jf.write(pipe.config.model_dump_json())
+            jf.write("\n")
+    else:
+        console.print_json(pipe.config.model_dump_json())
