e05b40acbf89ad6c6aaa138c37c6b5b9bafa582f
Michael Ekstrand <mdekstrand@drexel.edu>
2026-06-29 10:55:59 -0400

    tuning: fix ray args for tune test

--- tests/cli/test-tune.sh
+++ tests/cli/test-tune.sh
@@ -7,7 +7,7 @@ out="$TEST_WORK/bias-tune"
 declare -a tune_args=()
 if [[ $LK_USE_RAY_TUNE ]]; then
     msg "using ray tune"
-    tune_args+=(--use-ray)
+    tune_args+=(--ray)
 fi
 
 if [[ ! -f $ML100K ]]; then
@@ -31,7 +31,7 @@ require -f "$TEST_WORK/pipeline.json"
 
 out="$TEST_WORK/als-tune"
 tap_comment "testing iterative tuning"
-run-lenskit tune -T "$train" -V "$test" --save-pipeline "$TEST_WORK/pipeline.json" \
+run-lenskit tune "${tune_args[@]}" -T "$train" -V "$test" --save-pipeline "$TEST_WORK/pipeline.json" \
     --max-points=10 pipelines/als-implicit-search.toml "$out"
 
 require -d "$out"
