30c3f4e41e2693753216cf8f48177a2d2f62c8a8
Michael Ekstrand <mdekstrand@drexel.edu>
2026-06-30 13:41:22 -0400

    accel: add support for nested accelerator pools

--- src/accel/lib.rs
+++ src/accel/lib.rs
@@ -35,6 +35,7 @@ fn _accel<'py>(py: Python<'py>, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<logging::AccelLogListener>()?;
     m.add_class::<funksvd::FunkSVDTrainer>()?;
     m.add_class::<util::atomic::AtomicInt>()?;
+    m.add_class::<parallel::NestedAccelPool>()?;
     m.add_function(wrap_pyfunction!(parallel::init_accel_pool, m)?)?;
     m.add_function(wrap_pyfunction!(parallel::thread_count, m)?)?;
     m.add_function(wrap_pyfunction!(sparse::sparse_row_debug_type, m)?)?;
