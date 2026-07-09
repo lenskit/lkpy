09a424e44f08b53c6b6c9271cd5529864d84152e
Michael Ekstrand <mdekstrand@drexel.edu>
2026-07-09 15:56:13 -0400

    tuning: add test for pow2 space

---
+++ tests/tuning/test_optuna_space.py
@@ -0,0 +1,29 @@
+# This file is part of LensKit.
+# Copyright (C) 2018-2023 Boise State University.
+# Copyright (C) 2023-2026 Drexel University.
+# Licensed under the MIT license, see LICENSE.md for details.
+# SPDX-License-Identifier: MIT
+
+import optuna
+
+from pytest import importorskip
+
+importorskip("optuna")
+from lenskit.tuning._optuna import _ask_space
+from lenskit.tuning.spec import SearchParam
+
+NTRIALS = 1000
+
+
+def test_log2_space():
+    space = {"n": SearchParam(type="int", scale="pow2", min=8, max=1024)}
+    study = optuna.create_study()
+
+    for _t in range(NTRIALS):
+        trial = study.ask()
+        for p in range(10):
+            pt = _ask_space(trial, space)
+            x = pt["n"]
+            assert isinstance(x, int)
+            assert x >= 8
+            assert x <= 1024
