9c23857fd26bdb4f2c76932d52415d00d554faee
Michael Ekstrand <mdekstrand@drexel.edu>
2026-08-11 17:03:15 -0400

    cli: add test for ms-web data import

--- tests/cli/test-data-convert.sh
+++ tests/cli/test-data-convert.sh
@@ -12,3 +12,11 @@ if [[ -f data/australian_users_items.json.gz ]]; then
 else
     skip 3
 fi
+
+if [[ -f data/anonymous-msweb.data.gz ]]; then
+    run-lenskit data convert --ms-web data/anonymous-msweb.data.gz "$TEST_WORK/msweb"
+    require -d "$TEST_WORK/msweb"
+    require -f "$TEST_WORK/msweb/schema.json"
+else
+    skip 3
+fi
