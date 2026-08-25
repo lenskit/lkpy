0128c673dbf96f22426410abb6c640f6b1b4fbe3
Michael Ekstrand <mdekstrand@drexel.edu>
2026-08-25 16:45:33 -0400

    docs: add JSON schemas to doc site

--- mise/tasks/build/docs.sh
+++ mise/tasks/build/docs.sh
@@ -9,4 +9,10 @@ if [[ $usage_preview ]]; then
     cmd=(sphinx-autobuild)
 fi
 
-echo-run "${cmd[@]}" docs build/site || exit 2
+echo-run "${cmd[@]}" docs build/site || die "doc build failed"
+
+msg "building schemas"
+mkdir -p build/site/schemas
+echo-run python -m lenskit.schemas -o build/site/schemas/config.json --config
+echo-run python -m lenskit.schemas -o build/site/schemas/pipeline.json --pipeline
+echo-run python -m lenskit.schemas -o build/site/schemas/tuner.json --tuner
