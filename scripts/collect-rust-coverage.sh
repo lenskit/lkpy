62f7b7b1740074fbe647f26e01b6f298ad210d32
Michael Ekstrand <mdekstrand@drexel.edu>
2026-09-18 18:33:49 -0400

    build: add shell scripts to run tests

---
+++ scripts/collect-rust-coverage.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+. "$(dirname "$0")/lib/init.sh" || exit 2
+
+set -eo pipefail
+
+tdir="target/debug"
+
+OS="$(uname)"
+if [[ $OS = Darwin ]]; then
+    target="$tdir/liblenskit_accel.dylib"
+else
+    target="$tdir/liblenskit_accel.so"
+fi
+
+msg "merging coverage data"
+cargo profdata -- merge -sparse .coverage-prof/lenskit-test-*.profraw -o .coverage-prof/lenskit-test.profdata
+msg "exporting lcov.info"
+cargo cov -- export --instr-profile=.coverage-prof/lenskit-test.profdata "$target" --format=lcov >lcov.info
