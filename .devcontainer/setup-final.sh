b677bdd1668fad27786b59a7d1a8210b4559e1c7
Michael Ekstrand <mdekstrand@drexel.edu>
2026-09-22 14:49:18 -0400

    build: update devcontainer to just uv

--- .devcontainer/setup-final.sh
+++ .devcontainer/setup-final.sh
@@ -2,5 +2,5 @@
 set -xeo pipefail
 
 # sync again to build LensKit post-attach
-mise x -- uv sync --all-extras --group=cpu
+uv sync --all-extras --group=cpu
 "$VIRTUAL_ENV/bin/prek" install --prepare-hooks
