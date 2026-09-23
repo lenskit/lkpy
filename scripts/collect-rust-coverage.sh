94d171a5f276e7bb1c72f599dacb792f0713c2a2
Michael Ekstrand <md@ekstrandom.net>
2026-09-23 08:31:25 -0400

    ci: directly call llvm tools

--- scripts/collect-rust-coverage.sh
+++ scripts/collect-rust-coverage.sh
@@ -2,7 +2,7 @@
 
 . "$(dirname "$0")/lib/init.sh" || exit 2
 
-set -eo pipefail
+set -o pipefail
 
 tdir="target/debug"
 
@@ -12,8 +12,14 @@ if [[ $OS = Darwin ]]; then
 else
     target="$tdir/liblenskit_accel.so"
 fi
+sysroot="$(rustc --print sysroot)"
+if (($?)); then die "cannot find rust sysroot"; fi
+tuple="$(rustc --print host-tuple)"
+if (($?)); then die "cannot find rust host tuple"; fi
+
+PATH="$sysroot/lib/rustlib/$tuple/bin:$PATH"
 
 msg "merging coverage data"
-run-cmd -check cargo profdata -- merge --sparse .coverage-prof/lenskit-test-*.profraw -o .coverage-prof/lenskit-test.profdata
+run-cmd -check llvm-profdata merge --sparse .coverage-prof/lenskit-test-*.profraw -o .coverage-prof/lenskit-test.profdata
 msg "exporting lcov.info"
-cargo cov -- export --instr-profile=.coverage-prof/lenskit-test.profdata "$target" --format=lcov >lcov.info
+run-cmd -check llvm-cov export --instr-profile=.coverage-prof/lenskit-test.profdata "$target" --format=lcov >lcov.info
